9d73c2d22d2e224a64707c36d5d57e56b08ce03a
Robert Osfield <robert@openscenegraph.com>
2001-10-23 22:09:21 +0000

    Adding in new .cpp versions of FT font code.

---
+++ src/osgText/FTGLBitmapFont.cpp
@@ -0,0 +1,70 @@
+#include	"GL/gl.h"
+
+#include	"FTGLBitmapFont.h"
+#include	"FTGlyphContainer.h"
+#include	"FTBitmapGlyph.h"
+
+
+FTGLBitmapFont::FTGLBitmapFont()
+:	tempGlyph(0)
+{}
+
+
+FTGLBitmapFont::~FTGLBitmapFont()
+{}
+
+
+// OPSignature: bool FTGlyphContainer:MakeGlyphList() 
+bool FTGLBitmapFont::MakeGlyphList()
+{
+//	if( preCache)
+	for( unsigned int c = 0; c < numGlyphs; ++c)
+	{
+		FT_Glyph* ftGlyph = face.Glyph( c, FT_LOAD_DEFAULT);
+//		FT_HAS_VERTICAL(face)
+
+		if( ftGlyph)
+		{
+			tempGlyph = new FTBitmapGlyph( *ftGlyph);
+			glyphList->Add( tempGlyph);
+		}
+		else
+		{
+			err = face.Error();
+		}
+	}
+	
+	return !err;
+}
+
+
+void FTGLBitmapFont::render( const char* string)
+{	
+	glPushClientAttrib( GL_CLIENT_PIXEL_STORE_BIT);
+	
+	// doing this every frame is a bad?
+	glPixelStorei( GL_UNPACK_LSB_FIRST, GL_FALSE);
+	glPixelStorei( GL_UNPACK_ROW_LENGTH, 0);
+	glPixelStorei( GL_UNPACK_ALIGNMENT, 1);
+
+	FTFont::render( string);
+
+	glPopClientAttrib();
+
+}
+
+
+void FTGLBitmapFont::render( const wchar_t* string)
+{	
+	glPushClientAttrib( GL_CLIENT_PIXEL_STORE_BIT);
+	
+	// doing this every frame is a bad?
+	glPixelStorei( GL_UNPACK_LSB_FIRST, GL_FALSE);
+	glPixelStorei( GL_UNPACK_ROW_LENGTH, 0);
+	glPixelStorei( GL_UNPACK_ALIGNMENT, 1);
+
+	FTFont::render( string);
+
+	glPopClientAttrib();
+
+}
---
+++ src/osgText/FTGLOutlineFont.cpp
@@ -0,0 +1,68 @@
+#include	"GL/gl.h"
+
+#include	"FTGLOutlineFont.h"
+#include	"FTGlyphContainer.h"
+#include	"FTGL.h"
+#include	"FTOutlineGlyph.h"
+
+
+FTGLOutlineFont::FTGLOutlineFont()
+:	tempGlyph(0)
+{}
+
+
+FTGLOutlineFont::~FTGLOutlineFont()
+{}
+
+
+bool FTGLOutlineFont::MakeGlyphList()
+{
+	for( unsigned int n = 0; n < numGlyphs; ++n)
+	{
+		FT_Glyph* ftGlyph = face.Glyph( n, FT_LOAD_NO_HINTING | FT_LOAD_NO_BITMAP);
+		
+		if( ftGlyph)
+		{
+			tempGlyph = new FTOutlineGlyph( *ftGlyph);
+			glyphList->Add( tempGlyph);
+		}
+		else
+		{
+			err = face.Error();
+		}
+	}
+	
+	return !err;
+}
+
+
+void FTGLOutlineFont::render( const char* string)
+{	
+	glPushAttrib( GL_ENABLE_BIT | GL_HINT_BIT | GL_LINE_BIT | GL_PIXEL_MODE_BIT);
+	
+	glEnable( GL_LINE_SMOOTH);
+	glHint( GL_LINE_SMOOTH_HINT, GL_DONT_CARE);
+	glEnable(GL_BLEND);
+ 	glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); // GL_ONE
+
+	FTFont::render( string);
+
+	glPopAttrib();
+
+}
+
+
+void FTGLOutlineFont::render( const wchar_t* string)
+{	
+	glPushAttrib( GL_ENABLE_BIT | GL_HINT_BIT | GL_LINE_BIT | GL_PIXEL_MODE_BIT);
+	
+	glEnable( GL_LINE_SMOOTH);
+	glHint( GL_LINE_SMOOTH_HINT, GL_DONT_CARE);
+	glEnable(GL_BLEND);
+ 	glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); // GL_ONE
+
+	FTFont::render( string);
+
+	glPopAttrib();
+
+}

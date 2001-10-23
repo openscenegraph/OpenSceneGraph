e2c03d686a5774e4d2c2e157c4b9ba2fee560f82
Robert OSFIELD <robert.osfield@gmail.com>
2001-10-23 22:09:21 +0000

    Adding in new .cpp versions of FT font code.

---
+++ src/osgText/FTGlyph.cpp
@@ -0,0 +1,14 @@
+#include	"FTGlyph.h"
+
+
+FTGlyph::FTGlyph()
+:	advance(0),
+	err(0)	
+{
+	pos.x = 0;
+	pos.y = 0;
+}
+
+
+FTGlyph::~FTGlyph()
+{}
---
+++ src/osgText/FTLibrary.cpp
@@ -0,0 +1,65 @@
+#include	"FTLibrary.h"
+#include	"FTGL.h"
+
+
+FTLibrary&	FTLibrary::Instance()
+{
+	static FTLibrary ftlib;
+	return ftlib;
+}
+
+
+FTLibrary::~FTLibrary()
+{
+	if( lib != 0)
+	{
+		FT_Done_FreeType( *lib);
+
+		delete lib;
+		lib= 0;
+	}
+
+//	if( manager != 0)
+//	{
+//		FTC_Manager_Done( manager );
+//
+//		delete manager;
+//		manager= 0;
+//	}
+}
+
+
+FTLibrary::FTLibrary()
+:	lib(0),
+	err(0)
+{
+	Init();
+}
+
+
+bool FTLibrary::Init()
+{
+	if( lib != 0 )
+		return true;
+
+	lib = new FT_Library;
+	
+	err = FT_Init_FreeType( lib);
+	if( err)
+	{
+		delete lib;
+		lib = 0;
+		return false;
+	}
+	
+// 	FTC_Manager* manager;
+// 	
+// 	if( FTC_Manager_New( lib, 0, 0, 0, my_face_requester, 0, manager )
+// 	{
+// 		delete manager;
+// 		manager= 0;
+// 		return false;
+// 	}
+
+	return true;
+}

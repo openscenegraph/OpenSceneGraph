09f5d37e7d928a636dc6c52086f3aefb3db603ce
Robert OSFIELD <robert.osfield@gmail.com>
2001-10-29 12:19:14 +0000

    Added missing src/osgText/Version.cpp file.

---
+++ src/osgText/Version.cpp
@@ -0,0 +1,12 @@
+#include <osgText/Version>
+
+const char* osgTextGetVersion()
+{
+    return "0.8.42";
+}
+
+
+const char* osgTextGetLibraryName()
+{
+    return "Open Scene Graph Text Library";
+}

876b5ea3a184e499a6413c04ad3b66d371c3f1d1
Robert OSFIELD <robert.osfield@gmail.com>
2001-10-31 22:35:44 +0000

    Added missing config.h file to src/osgPlugins/lib3ds.

---
+++ src/osgPlugins/lib3ds/config.h
@@ -0,0 +1,12 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "lib3ds"
+
+/* Version number of package */
+#define VERSION "1.1.0"
+
+/* Define if using the dmalloc debugging malloc package */
+/* #undef WITH_DMALLOC */
+

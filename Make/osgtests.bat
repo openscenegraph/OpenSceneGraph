200e93ee77ab7ffd36cc3e353320e3f2346e8028
Robert Osfield <robert@openscenegraph.com>
2002-04-22 09:50:32 +0000

    Added test script which runs all the demos in the OSG to make sure they run ok.  User has to verify they are correct visually.

---
+++ Make/osgtests.bat
@@ -0,0 +1,27 @@
+sgv cow.osg
+sgv -stereo cessna.osg
+sgv Town.osg
+
+osghangglide
+
+osgbillboard lz.rgb
+
+osgcube
+
+osghud glider.osg
+
+osgimpostor Town.osg
+
+osgreflect cow.osg
+
+osgscribe cow.osg
+
+osgstereoimage dog_left_eye.jpg dog_right_eye.jpg
+
+osgtext
+
+osgtexture reflect.rgb lz.rgb
+
+osgversion
+
+osgviews cow.osg

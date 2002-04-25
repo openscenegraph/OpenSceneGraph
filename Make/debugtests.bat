ff531133d21f2dff56c08d69e62cf1cd20af6c54
Robert OSFIELD <robert.osfield@gmail.com>
2002-04-25 10:46:11 +0000

    A more verbose version of the test script, using each and more memleaks.log to print out progress and report any memory leaks which have been logged. The later will only occur with a debug build.  The use of echo and more makes this version osgtest.bat not portable to windows unfortuntately.

---
+++ Make/debugtests.bat
@@ -0,0 +1,59 @@
+echo sgv cow.osg
+sgv cow.osg
+more memleaks.log
+
+echo sgv -stereo cessna.osg
+sgv -stereo cessna.osg
+more memleaks.log
+
+echo sgv Town.osg
+sgv Town.osg
+more memleaks.log
+
+echo osghangglide
+osghangglide
+more memleaks.log
+
+echo osgbillboard lz.rgb
+osgbillboard lz.rgb
+more memleaks.log
+
+echo osgcube
+osgcube
+more memleaks.log
+
+echo osghud glider.osg
+osghud glider.osg
+more memleaks.log
+
+echo osgimpostor Town.osg
+osgimpostor Town.osg
+more memleaks.log
+
+echo osgreflect cow.osg
+osgreflect cow.osg
+more memleaks.log
+
+echo osgscribe cow.osg
+osgscribe cow.osg
+more memleaks.log
+
+echo osgstereoimage dog_left_eye.jpg dog_right_eye.jpg
+osgstereoimage dog_left_eye.jpg dog_right_eye.jpg
+more memleaks.log
+
+echo osgtext
+osgtext
+more memleaks.log
+
+echo osgtexture reflect.rgb lz.rgb
+osgtexture reflect.rgb lz.rgb
+more memleaks.log
+
+echo osgversion
+osgversion
+more memleaks.log
+
+echo osgviews cow.osg
+osgviews cow.osg
+more memleaks.log

f67be7c07f8673e393048b5bd42c7e40aa43c13b
Robert Osfield <robert@openscenegraph.com>
2002-06-04 15:21:24 +0000

    Further work on moving culling functionality from CullVisitor into CullingSet, in preperation for occlusion culling.

--- src/osg/CullingSet.cpp
+++ src/osg/CullingSet.cpp
@@ -5,6 +5,8 @@ using namespace osg;
 CullingSet::CullingSet()
 {
     _mask = ALL_CULLING;
+    _pixelSizeVector.set(0,0,0,1);
+    _smallFeatureCullingPixelSize=1.0f;
 }
 
 CullingSet::~CullingSet()

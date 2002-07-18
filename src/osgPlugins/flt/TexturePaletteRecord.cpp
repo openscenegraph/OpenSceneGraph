979fc70a9490f03bafa8d63c8657bdec99e92044
Robert Osfield <robert@openscenegraph.com>
2002-07-18 15:25:02 +0000

    From Brede Johansen, fix the TexturePaletteRecord::endian() to handle old flt versions (11, 12 & 13).

--- src/osgPlugins/flt/TexturePaletteRecord.cpp
+++ src/osgPlugins/flt/TexturePaletteRecord.cpp
@@ -28,9 +28,23 @@ TexturePaletteRecord::~TexturePaletteRecord()
 // virtual
 void TexturePaletteRecord::endian()
 {
-    STexturePalette *pSTexture = (STexturePalette*)getData();
 
-    ENDIAN( pSTexture->diIndex );
-    ENDIAN( pSTexture->diX );
-    ENDIAN( pSTexture->diY );
+    int flightVersion = getFlightVersion();
+
+    if (flightVersion > 13)
+    {
+        STexturePalette *pSTexture = (STexturePalette*)getData();
+
+        ENDIAN( pSTexture->diIndex );
+        ENDIAN( pSTexture->diX );
+        ENDIAN( pSTexture->diY );
+    }
+    else    // version 11, 12 & 13
+    {
+        SOldTexturePalette *pSOldTexture = (SOldTexturePalette*)getData();
+
+        ENDIAN( pSOldTexture->diIndex );
+        ENDIAN( pSOldTexture->diX );
+        ENDIAN( pSOldTexture->diY );
+    }
 }

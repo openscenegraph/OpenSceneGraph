9b39744ead7e72ee64bffd35d258ef114c484c11
Robert OSFIELD <robert.osfield@gmail.com>
2002-08-26 10:24:01 +0000

    Updated Texture3D to use extension checking to get the relevant 3d texturing extensions.

--- src/osg/GLExtensions.cpp
+++ src/osg/GLExtensions.cpp
@@ -1,5 +1,6 @@
 
 #include <osg/GL>
+#include <osg/GLU>
 #include <osg/GLExtensions>
 #include <osg/Notify>
 
@@ -48,6 +49,46 @@ const bool osg::isGLExtensionSupported(const char *extension)
 
     if (result) osg::notify(INFO)<<"OpenGL extension '"<<extension<<"' is supported."<<std::endl;
     else osg::notify(INFO)<<"OpenGL extension '"<<extension<<"' is not supported."<<std::endl;
+
+    return result;
+}
+
+const bool osg::isGLUExtensionSupported(const char *extension)
+{
+    typedef std::set<std::string>  ExtensionSet;
+    static ExtensionSet s_extensionSet;
+    static const char* s_extensions = NULL;
+    if (s_extensions==NULL)
+    {
+        // get the extension list from OpenGL.
+        s_extensions = (const char*)gluGetString(GL_EXTENSIONS);
+        if (s_extensions==NULL) return false;
+
+        // insert the ' ' delimiated extensions words into the extensionSet.
+        const char *startOfWord = s_extensions;
+        const char *endOfWord;
+        while ((endOfWord = strchr(startOfWord,' '))!=NULL)
+        {
+            s_extensionSet.insert(std::string(startOfWord,endOfWord));
+            startOfWord = endOfWord+1;
+        }
+        if (*startOfWord!=0) s_extensionSet.insert(std::string(startOfWord));
+        
+        osg::notify(INFO)<<"OpenGL extensions supported by installed OpenGL drivers are:"<<std::endl;
+        for(ExtensionSet::iterator itr=s_extensionSet.begin();
+            itr!=s_extensionSet.end();
+            ++itr)
+        {
+            osg::notify(INFO)<<"    "<<*itr<<std::endl;
+        }
+            
+    }
+
+    // true if extension found in extensionSet.
+    bool result = s_extensionSet.find(extension)!=s_extensionSet.end();
+
+    if (result) osg::notify(INFO)<<"OpenGL utility library extension '"<<extension<<"' is supported."<<std::endl;
+    else osg::notify(INFO)<<"OpenGL utility library extension '"<<extension<<"' is not supported."<<std::endl;
 
     return result;
 }

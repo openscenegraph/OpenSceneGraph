fb629ae87da96ee1919abb02a7ac975f73fb6d70
Robert Osfield <robert@openscenegraph.com>
2002-09-20 15:55:50 +0000

    From Gideon, support for tracking the number of objects constructed and deleted, only compiled in when unit tests are compiled in.

--- src/osg/Object.cpp
+++ src/osg/Object.cpp
@@ -4,6 +4,11 @@
 
 using namespace osg;
 
+#ifdef OSG_COMPILE_UNIT_TESTS
+int Referenced::_createdCount = 0;
+int Referenced::_deletedCount = 0;
+#endif
+
 Referenced::~Referenced()
 {
     if (_refCount>0)
@@ -11,6 +16,9 @@ Referenced::~Referenced()
         notify(WARN)<<"Warning: deleting still referenced object "<<this<<" of type '"<<typeid(this).name()<<"'"<<std::endl;
         notify(WARN)<<"         the final reference count was "<<_refCount<<", memory corruption possible."<<std::endl;
     }
+#ifdef OSG_COMPILE_UNIT_TESTS
+    _deletedCount ++;
+#endif
 }
 
 

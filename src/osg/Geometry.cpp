31e8323f176005fbe28ef67f8e1bc7ebeb72f22a
Robert Osfield <robert@openscenegraph.com>
2002-10-18 12:26:23 +0000

    Added default colour and normal definition to the Geometry draw to prevent state inheritance of these attributes from causing problems of Geometry which arn't fully defined.

--- src/osg/Geometry.cpp
+++ src/osg/Geometry.cpp
@@ -412,6 +412,10 @@ void Geometry::drawImmediateMode(State& state)
     unsigned int secondaryColorIndex = 0;
     unsigned int fogCoordIndex = 0;
 
+    // if no values are defined for normal and color provide some defaults...
+    if (_normalBinding==BIND_OFF) glNormal3f(0.0f,0.0f,1.0f);
+    if (_colorBinding==BIND_OFF) glColor4f(1.0f,1.0f,1.0f,1.0f);
+
     if (areFastPathsUsed())
     {
     

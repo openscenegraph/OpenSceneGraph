15773d920a81aa9418cfb98a8f062a730df603c0
Robert Osfield <robert@openscenegraph.com>
2002-11-07 08:48:30 +0000

    Fix for build under Linux.

---
+++ src/osgPlugins/osg/ShapeDrawable.cpp
@@ -0,0 +1,41 @@
+#include <osg/ShapeDrawable>
+
+#include <osgDB/Registry>
+#include <osgDB/Input>
+#include <osgDB/Output>
+
+using namespace osg;
+using namespace osgDB;
+
+// forward declare functions to use later.
+bool ShapeDrawable_readLocalData(Object& obj, Input& fr);
+bool ShapeDrawable_writeLocalData(const Object& obj, Output& fw);
+
+//register the read and write functions with the osgDB::Registry.
+RegisterDotOsgWrapperProxy g_ShapeDrawableFuncProxy
+(
+    osgNew osg::ShapeDrawable,
+    "ShapeDrawable",
+    "Object Drawable ShapeDrawable",
+    &ShapeDrawable_readLocalData,
+    &ShapeDrawable_writeLocalData,
+    DotOsgWrapper::READ_AND_WRITE
+);
+
+bool ShapeDrawable_readLocalData(Object& obj, Input& fr)
+{
+    bool iteratorAdvanced = false;
+
+    ShapeDrawable& geom = static_cast<ShapeDrawable&>(obj);
+
+    bool matchedFirst = false;
+
+    return iteratorAdvanced;
+}
+
+bool ShapeDrawable_writeLocalData(const Object& obj, Output& fw)
+{
+    const ShapeDrawable& geom = static_cast<const ShapeDrawable&>(obj);
+
+    return true;
+}

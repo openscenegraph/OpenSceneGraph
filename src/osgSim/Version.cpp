d73f6b048f5e8cbeb197dc7710a049504f7dce4d
Robert OSFIELD <robert.osfield@gmail.com>
2002-11-19 12:23:45 +0000

    Added visual workspace files for osgSim nodekit and osglightpoint demo.

---
+++ src/osgSim/Version.cpp
@@ -0,0 +1,12 @@
+#include <osgSim/Version>
+
+const char* osgSimGetVersion()
+{
+    return "0.9.2";
+}
+
+
+const char* osgSimGetLibraryName()
+{
+    return "Open Scene Graph Visual Simulation Library";
+}

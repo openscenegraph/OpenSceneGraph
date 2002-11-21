39d66d0688f12c069041d3e1b2f1b1d82221c7e8
Robert OSFIELD <robert.osfield@gmail.com>
2002-11-21 07:53:48 +0000

    Fix for compile problems under IRIX.

--- src/osgSim/BlinkSequence.cpp
+++ src/osgSim/BlinkSequence.cpp
@@ -13,28 +13,30 @@
 using namespace osgSim;
 
 BlinkSequence::BlinkSequence():
-    Referenced(),
     _pulsePeriod(0.0),           
     _phaseShift(0.0),
     _pulseData(),
-    _sequenceGroup(0) {}
+    _sequenceGroup(0)
+{
+}
 
 BlinkSequence::BlinkSequence(const BlinkSequence& bs):
-    Referenced(),
     _pulsePeriod(bs._pulsePeriod),
     _phaseShift(bs._phaseShift),
     _pulseData(bs._pulseData),
-    _sequenceGroup(bs._sequenceGroup) {}
+    _sequenceGroup(bs._sequenceGroup)
+{
+}
 
 
-BlinkSequence::SequenceGroup::SequenceGroup():
-    Referenced()
+BlinkSequence::SequenceGroup::SequenceGroup()
 {
     // set a random base time between 0 and 1000.0
     _baseTime = ((double)rand()/(double)RAND_MAX)*1000.0;
 }
 
 BlinkSequence::SequenceGroup::SequenceGroup(double baseTime):
-    Referenced(),
-    _baseTime(baseTime) {}
+    _baseTime(baseTime)
+{
+}
     

5ad9602673553c7374ff3371d63ab5a8a2cf92a2
Robert Osfield <robert@openscenegraph.com>
2003-01-15 19:23:16 +0000

    Updates to the osgproducer demo.

--- src/Demos/osgproducer/osgproducer.cpp
+++ src/Demos/osgproducer/osgproducer.cpp
@@ -8,6 +8,7 @@
 // graphics drawing with Producer::RenderSurface
 
 #include <Producer/Camera>
+#include <Producer/CameraConfig>
 #include <Producer/OsgCameraGroup>
 #include <Producer/OsgSceneHandler>
 #include <Producer/InputArea>
@@ -19,6 +20,42 @@
 #include <osgDB/ReadFile>
 
 #include "MyKeyboardMouseCallback"
+
+static Producer::CameraConfig *BuildConfig(void)
+{
+    Producer::RenderSurface *rs1 = new Producer::RenderSurface;
+    rs1->setScreenNum(0);
+//     rs1->useBorder(false);
+//     rs1->setWindowRect(0,0,640,480);
+    rs1->setWindowRect(10,10,620,480);
+
+    Producer::Camera *camera1 = new Producer::Camera;
+    camera1->setRenderSurface(rs1);
+    camera1->setOffset( 1.0, 0.0 );
+
+
+    Producer::RenderSurface *rs2 = new Producer::RenderSurface;
+    rs2->setScreenNum(0);
+//     rs2->useBorder(false);
+//     rs2->setWindowRect(640,0,640,480);
+    rs2->setWindowRect(650,10,620,480);
+
+    Producer::Camera *camera2 = new Producer::Camera;
+    camera2->setRenderSurface(rs2);
+    camera2->setOffset( -1.0, 0.0 );
+
+    Producer::CameraConfig *cfg = new Producer::CameraConfig;
+    cfg->addCamera("Camera 1",camera1);
+    cfg->addCamera("Camera 2", camera2);
+
+    Producer::InputArea *ia = new Producer::InputArea;
+    ia->addInputRectangle( rs1, Producer::InputRectangle(0.0,0.5,0.0,1.0));
+    ia->addInputRectangle( rs2, Producer::InputRectangle(0.5,1.0,0.0,1.0));
+
+    cfg->setInputArea(ia);
+
+    return cfg;
+}
 
 int main( int argc, char **argv )
 {
@@ -33,31 +70,37 @@ int main( int argc, char **argv )
     osgUtil::Optimizer optimizer;
     optimizer.optimize(scene.get());
 
+#define USE_BUILD_CONFIG
 
-    Producer::Camera camera1;
-    Producer::RenderSurface *rs1 = camera1.getRenderSurface();
-    rs1->setWindowRect(10,10,620,480);
-    camera1.setOffset( 1.0, 0.0 );
-    camera1.setSceneHandler(new Producer::OsgSceneHandler);
+#ifdef USE_BUILD_CONFIG
 
-    Producer::Camera camera2;
-    Producer::RenderSurface *rs2 = camera2.getRenderSurface();
-    rs2->setWindowRect(650,10,620,480);
-    camera2.setOffset( -1.0, 0.0 );
-    camera2.setSceneHandler(new Producer::OsgSceneHandler);
+    Producer::CameraConfig *cfg = BuildConfig();
+    Producer::OsgCameraGroup *cg = new Producer::OsgCameraGroup(cfg);
     
-    Producer::InputArea *ia = new Producer::InputArea;
-    ia->addInputRectangle( rs1, Producer::InputRectangle(0.0,0.5,0.0,1.0));
-    ia->addInputRectangle( rs2, Producer::InputRectangle(0.5,1.0,0.0,1.0));
+#elif USE_PARSE_CONFIG
 
-    Producer::CameraConfig cfg;
-    cfg.addCamera("Camera 1", &camera1);
-    cfg.addCamera("Camera 2", &camera2);
+    Producer::CameraConfig *cfg = new Producer::CameraConfig;
+    cfg->parseFile("twoWindows.cfg");
 
-    Producer::OsgCameraGroup cg(&cfg);
-        
-    cg.setSceneData(scene.get());
+    Producer::OsgCameraGroup *cg = new Producer::OsgCameraGroup(cfg);
+    
+#else
 
+    Producer::OsgCameraGroup *cg = new Producer::OsgCameraGroup();
+    // ackk we need a CameraConfig to get the InputArea from, as CameraGroup
+    // doesn't have its own InputArea...  perhaps there should be a 
+    // getOrCreateInputArea on the CameraGroup & CameraConfig classes to help
+    // out setting up the input area appropriatly.
+    
+#endif
+
+
+    // set up a scene handler for each camera.
+    for( int i = 0; i < cg->getNumberOfCameras(); i++ )
+        cg->getCamera(i)->setSceneHandler( new Producer::OsgSceneHandler );
+
+
+    Producer::InputArea *ia = cfg->getInputArea();
     Producer::KeyboardMouse kbm(ia);
 
     bool done = false;
@@ -68,16 +111,19 @@ int main( int argc, char **argv )
     Producer::Trackball tb;
     tb.setOrientation( Producer::Trackball::Y_UP );
 
+
+    cg->setSceneData(scene.get());
+
 //    cg.realize(Producer::CameraGroup::ThreadPerCamera);
-    cg.realize(Producer::CameraGroup::SingleThreaded);
+    cg->realize(Producer::CameraGroup::SingleThreaded);
 
     while( !done )
     {
         tb.input( kbmcb.mx(), kbmcb.my(), kbmcb.mbutton() );
 
-        cg.setView(tb.getMatrix().ptr());
+        cg->setView(tb.getMatrix().ptr());
                 
-        cg.frame();
+        cg->frame();
     }
     return 0;
 }

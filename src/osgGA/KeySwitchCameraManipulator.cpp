c2c38955c66624ad5cf5e91c85469c8eec302ecc
Robert Osfield <robert@openscenegraph.com>
2003-01-17 14:12:25 +0000

    Added new getCurrentCamaeraManipulator(), and removed and debugging message.

--- src/osgGA/KeySwitchCameraManipulator.cpp
+++ src/osgGA/KeySwitchCameraManipulator.cpp
@@ -27,7 +27,6 @@ bool KeySwitchCameraManipulator::handle(const GUIEventAdapter& ea,GUIActionAdapt
         KeyManipMap::iterator it=_manips.find(ea.getKey());
         if(it != _manips.end()){
             osg::notify(osg::INFO)<<"Switching to manipulator: "<<(*it).second.first<<std::endl;
-            std::cout<<"***Switching to manipulator: "<<(*it).second.first<<std::endl;
             it->second.second->setNode(_current->getNode());
             it->second.second->setCamera(_current->getCamera());
             it->second.second->init(ea,aa);

3e03de1a73d32c5c352916c1dd3eb5c80834e550
Robert OSFIELD <robert.osfield@gmail.com>
2003-01-24 09:35:05 +0000

    Fixed the formating of the Matrix output.

--- src/osgPlugins/osg/Matrix.cpp
+++ src/osgPlugins/osg/Matrix.cpp
@@ -36,14 +36,14 @@ bool readMatrix(osg::Matrix& matrix, osgDB::Input& fr)
 
 bool writeMatrix(const osg::Matrix& matrix, osgDB::Output& fw)
 {
-    fw.indent() << "Matrix {";
+    fw.indent() << "Matrix {" << std::endl;
     fw.moveIn();
     fw.indent() << matrix(0,0) << " " << matrix(0,1) << " " << matrix(0,2) << " " << matrix(0,3) << std::endl;
     fw.indent() << matrix(1,0) << " " << matrix(1,1) << " " << matrix(1,2) << " " << matrix(1,3) << std::endl;
     fw.indent() << matrix(2,0) << " " << matrix(2,1) << " " << matrix(2,2) << " " << matrix(2,3) << std::endl;
     fw.indent() << matrix(3,0) << " " << matrix(3,1) << " " << matrix(3,2) << " " << matrix(3,3) << std::endl;
     fw.moveOut();
-    fw.indent() << "}";
+    fw.indent() << "}"<< std::endl;
     return true;
 }
 

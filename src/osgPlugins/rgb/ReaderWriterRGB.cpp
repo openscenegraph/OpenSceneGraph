8d95f5162b3d70e1408a47c355b418dd1659a7f3
Robert OSFIELD <robert.osfield@gmail.com>
2003-02-27 08:38:53 +0000

    Changed the title of the ReaderWriter, removing the "Default" word as its no longer appropriate.

--- src/osgPlugins/rgb/ReaderWriterRGB.cpp
+++ src/osgPlugins/rgb/ReaderWriterRGB.cpp
@@ -313,7 +313,7 @@ static void RawImageGetData(rawImageRec *raw, unsigned char **data )
 class ReaderWriterRGB : public osgDB::ReaderWriter
 {
     public:
-        virtual const char* className() { return "Default RGB Image Reader/Writer"; }
+        virtual const char* className() { return "RGB Image Reader/Writer"; }
         
         virtual bool acceptsExtension(const std::string& extension)
         {

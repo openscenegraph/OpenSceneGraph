b02917e7a2f9282e20685b01245e6daa5916ebb7
Robert Osfield <robert@openscenegraph.com>
2003-04-09 08:20:08 +0000

    Fixed type of osg::ApplicationUsage::set/getApplictionName(), updated demos accordingly.
    Made OsgCameraGroup set the _applicationUsage to the
    osg::ApplicationUsage::instance() when no ArgumentParser is supplied.
    
    Added export to osgProducer::ViewerEventAdapter.

--- examples/osgviewer/osgviewer.cpp
+++ examples/osgviewer/osgviewer.cpp
@@ -20,7 +20,7 @@ int main( int argc, char **argv )
     osg::ArgumentParser arguments(&argc,argv);
     
     // set up the usage document, in case we need to print out how to use this program.
-    arguments.getApplicationUsage()->setApplicatonName(arguments.getApplicationName());
+    arguments.getApplicationUsage()->setApplicationName(arguments.getApplicationName());
     arguments.getApplicationUsage()->setDescription(arguments.getApplicationName()+" is the standard OpenSceneGraph example which loads and visualises 3d models.");
     arguments.getApplicationUsage()->setCommandLineUsage(arguments.getApplicationName()+" [options] filename ...");
     arguments.getApplicationUsage()->addCommandLineOption("-h or --help","Display this information");

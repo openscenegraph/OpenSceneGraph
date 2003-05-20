a1304e1e2d9b375123f49edb7c2768a3bce121d5
Robert OSFIELD <robert.osfield@gmail.com>
2003-05-20 08:57:02 +0000

    Removed redundent write_usage function.

--- examples/osgclip/osgclip.cpp
+++ examples/osgclip/osgclip.cpp
@@ -21,37 +21,6 @@
 
 #include <osgUtil/Optimizer>
 
-void write_usage(std::ostream& out,const std::string& name)
-{
-    out << std::endl;
-    out <<"usage:"<< std::endl;
-    out <<"    "<<name<<" [options] infile1 [infile2 ...]"<< std::endl;
-    out << std::endl;
-    out <<"options:"<< std::endl;
-    out <<"    -l libraryName      - load plugin of name libraryName"<< std::endl;
-    out <<"                          i.e. -l osgdb_pfb"<< std::endl;
-    out <<"                          Useful for loading reader/writers which can load"<< std::endl;
-    out <<"                          other file formats in addition to its extension."<< std::endl;
-    out <<"    -e extensionName    - load reader/wrter plugin for file extension"<< std::endl;
-    out <<"                          i.e. -e pfb"<< std::endl;
-    out <<"                          Useful short hand for specifying full library name as"<< std::endl;
-    out <<"                          done with -l above, as it automatically expands to"<< std::endl;
-    out <<"                          the full library name appropriate for each platform."<< std::endl;
-    out <<std::endl;
-    out <<"    -stereo             - switch on stereo rendering, using the default of,"<< std::endl;
-    out <<"                          ANAGLYPHIC or the value set in the OSG_STEREO_MODE "<< std::endl;
-    out <<"                          environmental variable. See doc/stereo.html for "<< std::endl;
-    out <<"                          further details on setting up accurate stereo "<< std::endl;
-    out <<"                          for your system. "<< std::endl;
-    out <<"    -stereo ANAGLYPHIC  - switch on anaglyphic(red/cyan) stereo rendering."<< std::endl;
-    out <<"    -stereo QUAD_BUFFER - switch on quad buffered stereo rendering."<< std::endl;
-    out <<std::endl;
-    out <<"    -stencil            - use a visual with stencil buffer enabled, this "<< std::endl;
-    out <<"                          also allows the depth complexity statistics mode"<< std::endl;
-    out <<"                          to be used (press 'p' three times to cycle to it)."<< std::endl;
-    out << std::endl;
-}
-
 
 osg::Node* decorate_with_clip_node(osg::Node* subgraph)
 {
@@ -171,7 +140,6 @@ int main( int argc, char **argv )
     osg::Node* loadedModel = osgDB::readNodeFiles(arguments);
     if (!loadedModel)
     {
-        write_usage(osg::notify(osg::NOTICE),argv[0]);
         return 1;
     }
   

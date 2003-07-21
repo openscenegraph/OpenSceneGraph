808fb74c393ada1dbe955b5c84bfbad66f8911e7
Robert OSFIELD <robert.osfield@gmail.com>
2003-07-21 15:51:43 +0000

    Added yield call to database paging main loop.

--- src/osgDB/DatabasePager.cpp
+++ src/osgDB/DatabasePager.cpp
@@ -260,12 +260,15 @@ void DatabasePager::run()
 
         }
         
-        // hack hack hack... sleep for 1ms so we give other threads a chance..
-        #ifdef WIN32
-            Sleep(1);
-        #else
-            usleep(1000);
-        #endif
+//         // hack hack hack... sleep for 1ms so we give other threads a chance..
+//         #ifdef WIN32
+//             Sleep(1);
+//         #else
+//             usleep(1000);
+//         #endif
+
+        yield();
+
     }
     
     cancel();

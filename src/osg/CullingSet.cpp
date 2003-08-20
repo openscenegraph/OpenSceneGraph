ea973c635b9a16fa2e6d2591e061e8e8afe827ac
Robert OSFIELD <robert.osfield@gmail.com>
2003-08-20 12:50:54 +0000

    Standardised the culling flags between CullingSet and CullStack, and made ENABLE_ALL_CULLING enable all culling including the near and far plane. DEFAULT_CULLING is now used for the default as uses the same original values as ENABLE_ALL_CULLING once did - view frustum culling with near and far culling. SceneView now uses DEFAULT_CULLING.

--- src/osg/CullingSet.cpp
+++ src/osg/CullingSet.cpp
@@ -16,7 +16,7 @@ using namespace osg;
 
 CullingSet::CullingSet()
 {
-    _mask = ALL_CULLING;
+    _mask = ENABLE_ALL_CULLING;
     _pixelSizeVector.set(0,0,0,1);
     _smallFeatureCullingPixelSize=1.0f;
 }

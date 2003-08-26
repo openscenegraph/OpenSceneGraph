f7d7a746f3269aa398cef938c39085766c463ca5
Robert Osfield <robert@openscenegraph.com>
2003-08-26 22:09:15 +0000

    Added missing osgFX files.

---
+++ src/osgPlugins/osgFX/IO_AnisotropicLighting.cpp
@@ -0,0 +1,59 @@
+#include <osgFX/AnisotropicLighting>
+
+#include <osgDB/Registry>
+#include <osgDB/Input>
+#include <osgDB/Output>
+
+bool AnisotropicLighting_readLocalData(osg::Object &obj, osgDB::Input &fr);
+bool AnisotropicLighting_writeLocalData(const osg::Object &obj, osgDB::Output &fw);
+
+osgDB::RegisterDotOsgWrapperProxy AnisotropicLighting_Proxy
+(
+	new osgFX::AnisotropicLighting,
+	"osgFX::AnisotropicLighting",
+	"Object Node osgFX::Effect osgFX::AnisotropicLighting",
+    AnisotropicLighting_readLocalData,
+    AnisotropicLighting_writeLocalData
+);
+
+bool AnisotropicLighting_readLocalData(osg::Object &obj, osgDB::Input &fr)
+{
+	osgFX::AnisotropicLighting &myobj = static_cast<osgFX::AnisotropicLighting &>(obj);
+	bool itAdvanced = false;
+
+	if (fr[0].matchWord("lightNumber")) {
+		int n;
+		if (fr[1].getInt(n)) {
+			myobj.setLightNumber(n);
+			fr += 2;
+			itAdvanced = true;
+		}
+	}
+
+	if (fr[0].matchWord("lightingMapFileName") && fr[1].isString()) {
+		osg::Image *lmap = fr.readImage(fr[1].getStr());
+		if (lmap) {
+			myobj.setLightingMap(lmap);
+		}
+		fr += 2;
+		itAdvanced = true;
+	}
+
+	return itAdvanced;
+}
+
+bool AnisotropicLighting_writeLocalData(const osg::Object &obj, osgDB::Output &fw)
+{
+	const osgFX::AnisotropicLighting &myobj = static_cast<const osgFX::AnisotropicLighting &>(obj);
+
+	fw.indent() << "lightNumber " << myobj.getLightNumber() << "\n";
+	
+	const osg::Image *lmap = myobj.getLightingMap();
+	if (lmap) {
+		if (!lmap->getFileName().empty()) {
+			fw.indent() << "lightingMapFileName \"" << lmap->getFileName() << "\"\n";
+		}
+	}
+
+    return true;
+}
---
+++ src/osgPlugins/osgFX/IO_Effect.cpp
@@ -0,0 +1,71 @@
+#include <osgFX/Effect>
+
+#include <osgDB/Registry>
+#include <osgDB/Input>
+#include <osgDB/Output>
+
+bool Effect_readLocalData(osg::Object &obj, osgDB::Input &fr);
+bool Effect_writeLocalData(const osg::Object &obj, osgDB::Output &fw);
+
+osgDB::RegisterDotOsgWrapperProxy Effect_Proxy
+(
+	0,
+    "osgFX::Effect",
+	"Object Node osgFX::Effect",
+    Effect_readLocalData,
+    Effect_writeLocalData
+);
+
+bool Effect_readLocalData(osg::Object &obj, osgDB::Input &fr)
+{
+	osgFX::Effect &myobj = static_cast<osgFX::Effect &>(obj);
+	bool itAdvanced = false;
+
+	if (fr[0].matchWord("enabled")) {
+		if (fr[1].matchWord("FALSE")) {
+			myobj.setEnabled(false);
+		} else {
+			myobj.setEnabled(true);
+		}
+		fr += 2;
+		itAdvanced = true;
+	}
+
+	if (fr[0].matchWord("selectedTechnique")) {
+		if (fr[1].matchWord("AUTO_DETECT")) {
+			myobj.selectTechnique(osgFX::Effect::AUTO_DETECT);
+			fr += 2;
+			itAdvanced = true;
+		} else {
+			int i;
+			if (fr[1].getInt(i)) {
+				myobj.selectTechnique(i);
+				fr += 2;
+				itAdvanced = true;
+			}
+		}
+	}
+
+	osg::ref_ptr<osg::Node> node = static_cast<osg::Node *>(fr.readObjectOfType(osgDB::type_wrapper<osg::Node>()));
+	if (node.valid()) {
+		myobj.setChild(node.get());
+	}
+
+	return itAdvanced;
+}
+
+bool Effect_writeLocalData(const osg::Object &obj, osgDB::Output &fw)
+{
+	const osgFX::Effect &myobj = static_cast<const osgFX::Effect &>(obj);
+
+	fw.indent() << "enabled " << (myobj.getEnabled() ? "TRUE" : "FALSE") << "\n";
+	fw.indent() << "selectedTechnique ";
+	if (myobj.getSelectedTechnique() == osgFX::Effect::AUTO_DETECT) {
+		fw << "AUTO_DETECT\n";
+	} else {
+		fw << myobj.getSelectedTechnique() << "\n";
+	}
+	fw.writeObject(*myobj.getChild());
+
+    return true;
+}

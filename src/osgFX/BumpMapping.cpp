2509c3339da50366d346b09cb3cdf59ba0362652
Robert Osfield <robert@openscenegraph.com>
2003-09-30 19:09:31 +0000

    Changed std::vector<>::at to [].

--- src/osgFX/BumpMapping.cpp
+++ src/osgFX/BumpMapping.cpp
@@ -82,14 +82,14 @@ namespace
             for (unsigned i=0; i<vx->getNumElements(); ++i) {
 
                 osg::Vec3 P;
-                if (v2a) P.set(v2a->at(i).x(), v2a->at(i).y(), 0);
-                if (v3a) P.set(v3a->at(i).x(), v3a->at(i).y(), v3a->at(i).z());
-                if (v4a) P.set(v4a->at(i).x(), v4a->at(i).y(), v4a->at(i).z());
+                if (v2a) P.set((*v2a)[i].x(), (*v2a)[i].y(), 0);
+                if (v3a) P.set((*v3a)[i].x(), (*v3a)[i].y(), (*v3a)[i].z());
+                if (v4a) P.set((*v4a)[i].x(), (*v4a)[i].y(), (*v4a)[i].z());
 
                 osg::Vec3 N(0, 0, 1);
-                if (n2a) N.set(n2a->at(i).x(), n2a->at(i).y(), 0);
-                if (n3a) N.set(n3a->at(i).x(), n3a->at(i).y(), n3a->at(i).z());
-                if (n4a) N.set(n4a->at(i).x(), n4a->at(i).y(), n4a->at(i).z());
+                if (n2a) N.set((*n2a)[i].x(), (*n2a)[i].y(), 0);
+                if (n3a) N.set((*n3a)[i].x(), (*n3a)[i].y(), (*n3a)[i].z());
+                if (n4a) N.set((*n4a)[i].x(), (*n4a)[i].y(), (*n4a)[i].z());
 
                 int axis = 0;
                 if (N.y() > N.x() && N.y() > N.z()) axis = 1;

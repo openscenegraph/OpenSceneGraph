5235c7cad1f5251668ce5c58598a60376f043685
Robert OSFIELD <robert.osfield@gmail.com>
2003-10-09 14:55:56 +0000

    Added DrawElementsUByte to ive plugin.

---
+++ src/osgPlugins/ive/DrawElementsUByte.h
@@ -0,0 +1,15 @@
+#ifndef IVE_DRAWELEMENTSUBYTE
+#define IVE_DRAWELEMENTSUBYTE 1
+
+#include <osg/PrimitiveSet>
+#include "ReadWrite.h"
+
+namespace ive{
+class DrawElementsUByte : public osg::DrawElementsUByte, public ReadWrite {
+public:
+	void write(DataOutputStream* out);
+	void read(DataInputStream* in);
+};
+}
+
+#endif

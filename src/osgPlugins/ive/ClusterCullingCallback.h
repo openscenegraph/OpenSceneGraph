b9423da386ad092952bebc737d5fc9ffdc9f922b
Robert Osfield <robert@openscenegraph.com>
2003-10-10 12:56:35 +0000

    Added ClusterCullingCallback to ive.

---
+++ src/osgPlugins/ive/ClusterCullingCallback.h
@@ -0,0 +1,15 @@
+#ifndef IVE_CLUSTERCULLINGCALLBACK
+#define IVE_CLUSTERCULLINGCALLBACK 1
+
+#include <osg/Drawable>
+#include "ReadWrite.h"
+
+namespace ive{
+class ClusterCullingCallback : public osg::ClusterCullingCallback, public ReadWrite {
+public:
+	void write(DataOutputStream* out);
+	void read(DataInputStream* in);
+};
+}
+
+#endif

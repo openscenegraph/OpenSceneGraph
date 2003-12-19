21307730d853f39dd53f18aafcb53f9a15610a47
Robert OSFIELD <robert.osfield@gmail.com>
2003-12-19 22:21:57 +0000

    From Corbin Holtz, support for endian testing and bytes swapping.  With mods from Robert to use include/osg/Endian

---
+++ src/osgPlugins/ive/IveVersion.h
@@ -0,0 +1,24 @@
+
+#ifndef IVE_VERSION
+#define IVE_VERSION 1
+
+
+/* The VERSION tag should be updated any time the
+   IVE format changes in order to support backward
+   compatibility (if implemented).  VERSION is 
+   stored in the 2nd 4 bytes of the file */
+
+#define VERSION 0x00000002
+
+
+/* The BYTE_SEX tag is used to check the endian
+   of the IVE file being read in.  The IVE format
+   is always written in the native endian of the
+   machine to provide optimum reading of the file.
+   BYTE_SEX is stored in the first 4 bytes of the 
+   file */
+#define ENDIAN_TYPE 0x01020304
+#define OPPOSITE_ENDIAN_TYPE 0x04030201
+
+
+#endif

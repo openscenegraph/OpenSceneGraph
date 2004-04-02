c22841baf3fc250885f24afd3fb287cf050c2262
Robert OSFIELD <robert.osfield@gmail.com>
2004-04-02 15:45:34 +0000

    From Bob Kuehne, reworked code to use a marco instead of duplicated code, done as measure for reducing the risk of copy and paste errors.

--- src/osg/CopyOp.cpp
+++ src/osg/CopyOp.cpp
@@ -1,4 +1,4 @@
-/* -*-c++-*- OpenSceneGraph - Copyright (C) 1998-2003 Robert Osfield 
+/* -*-c++-*- OpenSceneGraph - Copyright (C) 1998-2004 Robert Osfield 
  *
  * This library is open source and may be redistributed and/or modified under  
  * the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or 
@@ -21,40 +21,28 @@
 
 using namespace osg;
 
+#define COPY_OP( TYPE, FLAG ) \
+TYPE* CopyOp::operator() (const TYPE* obj) const \
+{ \
+    if (obj && _flags&FLAG) \
+        return dynamic_cast<TYPE*>( obj->clone(*this) ); \
+    else \
+        return const_cast<TYPE*>(obj); \
+} 
+
+COPY_OP( Object,         DEEP_COPY_OBJECTS );
+COPY_OP( Node,           DEEP_COPY_NODES );
+COPY_OP( Drawable,       DEEP_COPY_DRAWABLES );
+COPY_OP( StateSet,       DEEP_COPY_STATESETS );
+COPY_OP( Texture,        DEEP_COPY_TEXTURES );
+COPY_OP( Image,          DEEP_COPY_IMAGES );
+COPY_OP( Array,          DEEP_COPY_DRAWABLES );
+COPY_OP( PrimitiveSet,   DEEP_COPY_PRIMITIVES );
+COPY_OP( Shape,          DEEP_COPY_SHAPES );
+
 Referenced* CopyOp::operator() (const Referenced* ref) const
 {
     return const_cast<Referenced*>(ref);
-}
-
-Object* CopyOp::operator() (const Object* obj) const
-{
-    if (obj && _flags&DEEP_COPY_OBJECTS)
-        return obj->clone(*this);
-    else return const_cast<Object*>(obj);
-}
-
-Node* CopyOp::operator() (const Node* node) const
-{
-    if (node && _flags&DEEP_COPY_NODES)
-        return dynamic_cast<Node*>(node->clone(*this));
-    else
-        return const_cast<Node*>(node);
-}
-
-Drawable* CopyOp::operator() (const Drawable* drawable) const
-{
-    if (drawable && _flags&DEEP_COPY_DRAWABLES)
-        return dynamic_cast<Drawable*>(drawable->clone(*this));
-    else
-        return const_cast<Drawable*>(drawable);
-}
-
-StateSet* CopyOp::operator() (const StateSet* stateset) const
-{
-    if (stateset && _flags&DEEP_COPY_STATESETS)
-        return dynamic_cast<StateSet*>(stateset->clone(*this));
-    else
-        return const_cast<StateSet*>(stateset);
 }
 
 StateAttribute* CopyOp::operator() (const StateAttribute* attr) const
@@ -75,42 +63,4 @@ StateAttribute* CopyOp::operator() (const StateAttribute* attr) const
         return const_cast<StateAttribute*>(attr);
 }
 
-Texture* CopyOp::operator() (const Texture* text) const
-{
-    if (text && _flags&DEEP_COPY_TEXTURES)
-        return dynamic_cast<Texture*>(text->clone(*this));
-    else
-        return const_cast<Texture*>(text);
-}
-
-Image* CopyOp::operator() (const Image* image) const
-{
-    if (image && _flags&DEEP_COPY_IMAGES)
-        return dynamic_cast<Image*>(image->clone(*this));
-    else return const_cast<Image*>(image);
-}
-
-Array* CopyOp::operator() (const Array* array) const
-{
-    if (array && _flags&DEEP_COPY_ARRAYS)
-        return dynamic_cast<Array*>(array->clone(*this));
-    else
-        return const_cast<Array*>(array);
-}
-
-PrimitiveSet* CopyOp::operator() (const PrimitiveSet* primitive) const
-{
-    if (primitive && _flags&DEEP_COPY_PRIMITIVES)
-        return dynamic_cast<PrimitiveSet*>(primitive->clone(*this));
-    else
-        return const_cast<PrimitiveSet*>(primitive);
-}
-
-Shape* CopyOp::operator() (const Shape* shape) const
-{
-    if (shape && _flags&DEEP_COPY_SHAPES)
-        return dynamic_cast<Shape*>(shape->clone(*this));
-    else
-        return const_cast<Shape*>(shape);
-}
 

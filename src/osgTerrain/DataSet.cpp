8d47cedd1a53c6cc5f233cffe05c0a7be986f852
Robert OSFIELD <robert.osfield@gmail.com>
2004-04-12 21:30:23 +0000

    Commented out call to osgUtil::Simplifier as this utility is not yet functional

--- src/osgTerrain/DataSet.cpp
+++ src/osgTerrain/DataSet.cpp
@@ -2184,7 +2184,7 @@ osg::Node* DataSet::DestinationTile::createPolygonal()
         geometry->setColorBinding(osg::Geometry::BIND_OVERALL);
     }
     
-#if 0    
+#if 0
     osgUtil::Simplifier simplifier;
     simplifier.simplify(*geometry,0.5f);  // this will replace the normal vector with a new one
 #endif

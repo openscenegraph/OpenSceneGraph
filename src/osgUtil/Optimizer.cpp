3f7e7ffdc3c41c622c2d68f025501d5bfbd18e16
Robert OSFIELD <robert.osfield@gmail.com>
2004-04-12 21:29:31 +0000

    Further work on the new osgUtil::Simplifier.

--- src/osgUtil/Optimizer.cpp
+++ src/osgUtil/Optimizer.cpp
@@ -28,6 +28,7 @@
 #include <osgUtil/TransformAttributeFunctor>
 #include <osgUtil/TriStripVisitor>
 #include <osgUtil/Tesselator>
+#include <osgUtil/Simplifier>
 
 #include <typeinfo>
 #include <algorithm>
@@ -215,7 +216,11 @@ void Optimizer::optimize(osg::Node* node, unsigned int options)
         node->accept(tsv);
         tsv.stripify();
     }
-
+#if 0
+    // simplify geometry
+    osgUtil::Simplifier simplifier;
+    node->accept(simplifier);
+#endif
 }
 
 

dc133ff2028e0610b0fa566a585759659a9a07ba
Robert OSFIELD <robert.osfield@gmail.com>
2004-06-30 19:07:05 +0000

    Added support for using pixel size control of LOD levels.

--- examples/osgsimulation/osgsimulation.cpp
+++ examples/osgsimulation/osgsimulation.cpp
@@ -5,8 +5,11 @@
 #include <osg/ShapeDrawable>
 #include <osg/Texture2D>
 #include <osg/PositionAttitudeTransform>
+#include <osg/MatrixTransform>
 
 #include <osgDB/ReadFile>
+
+#include <osgText/Text>
 
 #include <osgSim/SphereSegment>
 
@@ -17,6 +20,108 @@
 
 // for the grid data..
 #include "../osghangglide/terrain_coords.h"
+
+osg::AnimationPath* createAnimationPath(const osg::Vec3& center,float radius,double looptime)
+{
+    // set up the animation path 
+    osg::AnimationPath* animationPath = new osg::AnimationPath;
+    animationPath->setLoopMode(osg::AnimationPath::LOOP);
+    
+    int numSamples = 40;
+    float yaw = 0.0f;
+    float yaw_delta = 2.0f*osg::PI/((float)numSamples-1.0f);
+    float roll = osg::inDegrees(30.0f);
+    
+    double time=0.0f;
+    double time_delta = looptime/(double)numSamples;
+    for(int i=0;i<numSamples;++i)
+    {
+        osg::Vec3 position(center+osg::Vec3(sinf(yaw)*radius,cosf(yaw)*radius,0.0f));
+        osg::Quat rotation(osg::Quat(roll,osg::Vec3(0.0,1.0,0.0))*osg::Quat(-(yaw+osg::inDegrees(90.0f)),osg::Vec3(0.0,0.0,1.0)));
+        
+        animationPath->insert(time,osg::AnimationPath::ControlPoint(position,rotation));
+
+        yaw += yaw_delta;
+        time += time_delta;
+
+    }
+    return animationPath;    
+}
+
+osg::Node* createMovingModel(const osg::Vec3& center, float radius)
+{
+    float animationLength = 10.0f;
+
+    osg::AnimationPath* animationPath = createAnimationPath(center,radius,animationLength);
+
+    osg::Group* model = new osg::Group;
+
+    osg::Node* glider = osgDB::readNodeFile("glider.osg");
+    if (glider)
+    {
+        const osg::BoundingSphere& bs = glider->getBound();
+
+        float size = radius/bs.radius()*0.3f;
+        osg::MatrixTransform* positioned = new osg::MatrixTransform;
+        positioned->setDataVariance(osg::Object::STATIC);
+        positioned->setMatrix(osg::Matrix::translate(-bs.center())*
+                                     osg::Matrix::scale(size,size,size)*
+                                     osg::Matrix::rotate(osg::inDegrees(-90.0f),0.0f,0.0f,1.0f));
+    
+        positioned->addChild(glider);
+    
+        osg::PositionAttitudeTransform* xform = new osg::PositionAttitudeTransform;    
+        xform->getOrCreateStateSet()->setMode(GL_NORMALIZE, osg::StateAttribute::ON);
+        xform->setUpdateCallback(new osg::AnimationPathCallback(animationPath,0.0,1.0));
+        xform->addChild(positioned);
+
+        model->addChild(xform);
+    }
+ 
+    osg::Node* cessna = osgDB::readNodeFile("cessna.osg");
+    if (cessna)
+    {
+        const osg::BoundingSphere& bs = cessna->getBound();
+
+        osgText::Text* text = new osgText::Text;
+        float size = radius/bs.radius()*0.3f;
+
+        text->setPosition(bs.center());
+        text->setText("Cessna");
+        text->setAlignment(osgText::Text::CENTER_CENTER);
+        text->setAxisAlignment(osgText::Text::SCREEN);
+        text->setCharacterSize(40.0f);
+        text->setCharacterSizeMode(osgText::Text::SCREEN_COORDS);
+        
+        osg::Geode* geode = new osg::Geode;
+        geode->addDrawable(text);
+    
+        osg::LOD* lod = new osg::LOD;
+        lod->setRangeMode(osg::LOD::PIXEL_SIZE_ON_SCREEN);
+        lod->addChild(geode,0.0f,100.0f);
+        lod->addChild(cessna,100.0f,10000.0f);
+
+
+        osg::MatrixTransform* positioned = new osg::MatrixTransform;
+        positioned->getOrCreateStateSet()->setMode(GL_NORMALIZE, osg::StateAttribute::ON);
+        positioned->setDataVariance(osg::Object::STATIC);
+        positioned->setMatrix(osg::Matrix::translate(-bs.center())*
+                                     osg::Matrix::scale(size,size,size)*
+                                     osg::Matrix::rotate(osg::inDegrees(180.0f),0.0f,0.0f,1.0f));
+    
+        //positioned->addChild(cessna);
+        positioned->addChild(lod);
+    
+        osg::MatrixTransform* xform = new osg::MatrixTransform;
+        xform->setUpdateCallback(new osg::AnimationPathCallback(animationPath,0.0f,2.0));
+        xform->addChild(positioned);
+
+        model->addChild(xform);
+    }
+    
+    return model;
+}
+
 
 osg::Vec3 computeTerrainIntersection(osg::Node* subgraph,float x,float y)
 {
@@ -131,6 +236,12 @@ void build_world(osg::Group *root)
 
         // add the updater node to the scene graph
         root->addChild(psu);
+    }
+    
+    
+    // create the moving models.
+    {
+        root->addChild(createMovingModel(osg::Vec3(500.0f,500.0f,500.0f),100.0f));
     }
 }
 

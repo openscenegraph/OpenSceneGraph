bec5a2444b6f5d8732bdffb480583a62afbe71be
Robert OSFIELD <robert.osfield@gmail.com>
2004-07-19 18:47:40 +0000

    From Mike Weiblen, "When launched, it creates a commandline shell that is fully configured to support running OSG apps from the source tree filesystems."

---
+++ VisualStudio/osgShell.bat
@@ -0,0 +1,23 @@
+@echo off
+REM This script provides a commandline shell properly configured to run OSG
+REM executables directly from the OpenThreads/Producer/OpenSceneGraph source
+REM trees.
+REM mew 2004-07-16
+
+cd ..\..
+set PATH=%CD%\OpenThreads\bin\win32;%CD%\Producer\bin;%CD%\OpenSceneGraph\bin;%CD%\3rdParty\bin;%PATH%
+set OSG_FILE_PATH=%CD%\OpenSceneGraph-Data;%CD%\OpenSceneGraph-Data\Images;%CD%\OpenSceneGraph-Data\fonts
+REM set PRODUCER_CAMERA_CONFIG_FILE=%CD%\Producer-Data\1win.cfg
+
+REM uncomment one of these for your desired notify level...
+rem set OSG_NOTIFY_LEVEL=ALWAYS
+rem set OSG_NOTIFY_LEVEL=FATAL
+rem set OSG_NOTIFY_LEVEL=WARN
+rem set OSG_NOTIFY_LEVEL=NOTICE
+rem set OSG_NOTIFY_LEVEL=DEBUG
+rem set OSG_NOTIFY_LEVEL=DEBUG_FP
+rem set OSG_NOTIFY_LEVEL=INFO
+
+TITLE osgShell
+%COMSPEC% /K
+ 

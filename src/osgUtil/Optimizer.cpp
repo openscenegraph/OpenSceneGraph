6f605d031f2bd0baf91b145a47d893666f995412
Robert Osfield <robert@openscenegraph.com>
2004-07-22 18:34:38 +0000

    Added support for OSG_OPTIMIZER="OFF"

--- src/osgUtil/Optimizer.cpp
+++ src/osgUtil/Optimizer.cpp
@@ -57,7 +57,7 @@ void Optimizer::reset()
     _permissableOptimizationsMap.clear();
 }
 
-static osg::ApplicationUsageProxy Optimizer_e0(osg::ApplicationUsage::ENVIRONMENTAL_VARIABLE,"OSG_OPTIMIZER \"<type> [<type>]\"","DEFAULT | FLATTEN_STATIC_TRANSFORMS | REMOVE_REDUNDANT_NODES | COMBINE_ADJACENT_LODS | SHARE_DUPLICATE_STATE | MERGE_GEOMETRY | SPATIALIZE_GROUPS  | COPY_SHARED_NODES  | TRISTRIP_GEOMETRY | OPTIMIZE_TEXTURE_SETTINGS");
+static osg::ApplicationUsageProxy Optimizer_e0(osg::ApplicationUsage::ENVIRONMENTAL_VARIABLE,"OSG_OPTIMIZER \"<type> [<type>]\"","OFF | DEFAULT | FLATTEN_STATIC_TRANSFORMS | REMOVE_REDUNDANT_NODES | COMBINE_ADJACENT_LODS | SHARE_DUPLICATE_STATE | MERGE_GEOMETRY | SPATIALIZE_GROUPS  | COPY_SHARED_NODES  | TRISTRIP_GEOMETRY | OPTIMIZE_TEXTURE_SETTINGS");
 
 void Optimizer::optimize(osg::Node* node)
 {
@@ -68,6 +68,8 @@ void Optimizer::optimize(osg::Node* node)
     {
         std::string str(env);
 
+
+        if(str.find("OFF")!=std::string::npos) options = 0;
 
         if(str.find("~DEFAULT")!=std::string::npos) options ^= DEFAULT_OPTIMIZATIONS;
         else if(str.find("DEFAULT")!=std::string::npos) options |= DEFAULT_OPTIMIZATIONS;

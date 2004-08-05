26ff1eccb3290f52fc7aa40fd9ab59b4ea45c26b
Robert OSFIELD <robert.osfield@gmail.com>
2004-08-05 15:15:51 +0000

    From Jay Zuckerman, fixes to AutoTransform so that it includes checking of previous position value.

--- src/osg/AutoTransform.cpp
+++ src/osg/AutoTransform.cpp
@@ -1,33 +1,33 @@
 /* -*-c++-*- OpenSceneGraph - Copyright (C) 1998-2003 Robert Osfield 
- *
- * This library is open source and may be redistributed and/or modified under  
- * the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or 
- * (at your option) any later version.  The full license is in LICENSE file
- * included with this distribution, and on the openscenegraph.org website.
  * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- * OpenSceneGraph Public License for more details.
-*/
-#include <osg/AutoTransform>
-#include <osg/CullStack>
+ * This library is open source and may be redistributed and/or modified under 
+ * the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or 
+ * (at your option) any later version. The full license is in LICENSE file 
+ * included with this distribution, and on the openscenegraph.org website. 
+ * 
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * OpenSceneGraph Public License for more details. 
+*/ 
+#include <osg/AutoTransform> 
+#include <osg/CullStack> 
 
-using namespace osg;
+using namespace osg; 
 
-AutoTransform::AutoTransform():
-    _autoUpdateEyeMovementTolerance(0.0f),
-    _autoRotateToScreen(false),
-    _autoScaleToScreen(false),
-    _scale(1.0f,1.0f,1.0f),
-    _firstTimeToInitEyePoint(true),
-    _matrixDirty(true)
-{
-//    setNumChildrenRequiringUpdateTraversal(1);
-}
+AutoTransform::AutoTransform(): 
+    _autoUpdateEyeMovementTolerance(0.0f), 
+    _autoRotateToScreen(false), 
+    _autoScaleToScreen(false), 
+    _scale(1.0f,1.0f,1.0f), 
+    _firstTimeToInitEyePoint(true), 
+    _matrixDirty(true) 
+{ 
+// setNumChildrenRequiringUpdateTraversal(1); 
+} 
 
-AutoTransform::AutoTransform(const AutoTransform& pat,const CopyOp& copyop):
-    Transform(pat,copyop),
+AutoTransform::AutoTransform(const AutoTransform& pat,const CopyOp& copyop): 
+	Transform(pat,copyop),
     _position(pat._position),
     _pivotPoint(pat._pivotPoint),
     _autoUpdateEyeMovementTolerance(pat._autoUpdateEyeMovementTolerance),
@@ -37,131 +37,137 @@ AutoTransform::AutoTransform(const AutoTransform& pat,const CopyOp& copyop):
     _scale(pat._scale),
     _firstTimeToInitEyePoint(true),
     _matrixDirty(true)
-{
-//    setNumChildrenRequiringUpdateTraversal(getNumChildrenRequiringUpdateTraversal()+1);            
-}
+{ 
+// setNumChildrenRequiringUpdateTraversal(getNumChildrenRequiringUpdateTraversal()+1); 
+} 
 
-bool AutoTransform::computeLocalToWorldMatrix(Matrix& matrix,NodeVisitor*) const
-{
-    if (_matrixDirty) computeMatrix();
-    
-    if (_referenceFrame==RELATIVE_TO_PARENTS)
-    {
-        matrix.preMult(_cachedMatrix);
-    }
-    else // absolute
-    {
-        matrix = _cachedMatrix;
-    }
-    return true;
-}
+bool AutoTransform::computeLocalToWorldMatrix(Matrix& matrix,NodeVisitor*) const 
+{ 
+    if (_matrixDirty) computeMatrix(); 
+     
+    if (_referenceFrame==RELATIVE_TO_PARENTS) 
+    { 
+        matrix.preMult(_cachedMatrix); 
+    } 
+    else // absolute 
+    { 
+        matrix = _cachedMatrix; 
+    } 
+    return true; 
+} 
 
 
-bool AutoTransform::computeWorldToLocalMatrix(Matrix& matrix,NodeVisitor*) const
-{
-    if (_referenceFrame==RELATIVE_TO_PARENTS)
-    {
-        matrix.postMult(osg::Matrix::translate(-_position)*
-                        osg::Matrix::rotate(_rotation.inverse())*
-                        osg::Matrix::scale(1.0f/_scale.x(),1.0f/_scale.y(),1.0f/_scale.z())*
-                        osg::Matrix::translate(_pivotPoint));
-    }
-    else // absolute
-    {
-        matrix = osg::Matrix::translate(-_position)*
-                 osg::Matrix::rotate(_rotation.inverse())*
-                 osg::Matrix::scale(1.0f/_scale.x(),1.0f/_scale.y(),1.0f/_scale.z())*
-                 osg::Matrix::translate(_pivotPoint);
-    }
-    return true;
-}
+bool AutoTransform::computeWorldToLocalMatrix(Matrix& matrix,NodeVisitor*) const 
+{ 
+    if (_referenceFrame==RELATIVE_TO_PARENTS) 
+    { 
+        matrix.postMult(osg::Matrix::translate(-_position)* 
+                        osg::Matrix::rotate(_rotation.inverse())* 
+                        osg::Matrix::scale(1.0f/_scale.x(),1.0f/_scale.y(),1.0f/_scale.z())* 
+                        osg::Matrix::translate(_pivotPoint)); 
+    } 
+    else // absolute 
+    { 
+        matrix = osg::Matrix::translate(-_position)* 
+                 osg::Matrix::rotate(_rotation.inverse())* 
+                 osg::Matrix::scale(1.0f/_scale.x(),1.0f/_scale.y(),1.0f/_scale.z())* 
+                 osg::Matrix::translate(_pivotPoint); 
+    } 
+    return true; 
+} 
 
-void AutoTransform::computeMatrix() const
-{
-    if (!_matrixDirty) return;
-    
-    _cachedMatrix.set(osg::Matrix::translate(-_pivotPoint)*
-                      osg::Matrix::scale(_scale)*
-                      osg::Matrix::rotate(_rotation)*
-                      osg::Matrix::translate(_position));
-    
-    _matrixDirty = false;
-}
+void AutoTransform::computeMatrix() const 
+{ 
+    if (!_matrixDirty) return; 
+     
+    _cachedMatrix.set(osg::Matrix::translate(-_pivotPoint)* 
+                      osg::Matrix::scale(_scale)* 
+                      osg::Matrix::rotate(_rotation)* 
+                      osg::Matrix::translate(_position)); 
+     
+    _matrixDirty = false; 
+} 
 
-void AutoTransform::accept(NodeVisitor& nv)
-{
-    // if app traversal update the frame count.
-    if (nv.getVisitorType()==NodeVisitor::UPDATE_VISITOR)
-    {
-    }
-    else
-    if (nv.getVisitorType()==NodeVisitor::CULL_VISITOR)
-    {
+void AutoTransform::accept(NodeVisitor& nv) 
+{ 
+    // if app traversal update the frame count. 
+    if (nv.getVisitorType()==NodeVisitor::UPDATE_VISITOR) 
+    { 
+    } 
+    else 
+    if (nv.getVisitorType()==NodeVisitor::CULL_VISITOR) 
+    { 
 
-        CullStack* cs = dynamic_cast<CullStack*>(&nv);
-        if (cs)
-        {
+        CullStack* cs = dynamic_cast<CullStack*>(&nv); 
+        if (cs) 
+        { 
 
-            int width = _previousWidth;
-            int height = _previousHeight;
+            int width = _previousWidth; 
+            int height = _previousHeight; 
 
-            osg::Viewport* viewport = cs->getViewport();
-            if (viewport)
-            {
-                width = viewport->width();
-                height = viewport->height();
-            }
+            osg::Viewport* viewport = cs->getViewport(); 
+            if (viewport) 
+            { 
+                width = viewport->width(); 
+                height = viewport->height(); 
+            } 
 
-            const osg::Vec3& eyePoint = cs->getEyeLocal();    
+            osg::Vec3 eyePoint = cs->getEyeLocal(); 
+			osg::Vec3 position = getPosition();
 
             const osg::Matrix& projection = cs->getProjectionMatrix();
 
-            bool doUpdate = _firstTimeToInitEyePoint;
-            if (!_firstTimeToInitEyePoint)
-            {
-                osg::Vec3 dv = _previousEyePoint-eyePoint;
-                if (dv.length2()>getAutoUpdateEyeMovementTolerance()*(eyePoint-getPosition()).length2())
-                {
-                    doUpdate = true;
-                }
-                else if (width!=_previousWidth || height!=_previousHeight)
-                {
-                    doUpdate = true;
-                }
+            bool doUpdate = _firstTimeToInitEyePoint; 
+            if (!_firstTimeToInitEyePoint) 
+            { 
+                osg::Vec3 dv = _previousEyePoint-eyePoint; 
+                if (dv.length2()>getAutoUpdateEyeMovementTolerance()*(eyePoint-getPosition()).length2()) 
+                { 
+                    doUpdate = true; 
+                } 
+                else if (width!=_previousWidth || height!=_previousHeight) 
+                { 
+                    doUpdate = true; 
+                } 
                 else if (projection != _previousProjection) 
-                {
-                    doUpdate = true;
-                }                
-            }
-            _firstTimeToInitEyePoint = false;
+                { 
+                    doUpdate = true; 
+                } 
+                else if (position != _previousPosition) 
+                { 
+                    doUpdate = true; 
+                } 
+            } 
+            _firstTimeToInitEyePoint = false; 
 
-            if (doUpdate)
-            {            
+            if (doUpdate) 
+            { 
 
-                if (getAutoScaleToScreen())
-                {
-                    float size = 1.0f/cs->pixelSize(getPosition(),1.0f);
-                    setScale(size);
-                }
+                if (getAutoScaleToScreen()) 
+                { 
+                    float size = 1.0f/cs->pixelSize(getPosition(),1.0f); 
+                    setScale(size); 
+                } 
 
-                if (getAutoRotateToScreen())
-                {
-                    osg::Quat rotation;
-                    cs->getModelViewMatrix().get(rotation);            
-                    setRotation(rotation.inverse());
-                }
+                if (getAutoRotateToScreen()) 
+                { 
+                    osg::Quat rotation; 
+                    cs->getModelViewMatrix().get(rotation); 
+                    setRotation(rotation.inverse()); 
+                } 
 
-                _previousEyePoint = eyePoint;
-                _previousWidth = width;
-                _previousHeight = height;
-                _previousProjection = projection;
+                _previousEyePoint = eyePoint; 
+                _previousWidth = width; 
+                _previousHeight = height; 
+                _previousProjection = projection; 
+				_previousPosition = position;
 
-                _matrixDirty = true;
-            }
+                _matrixDirty = true; 
+            } 
 
-        }
-    }
-    
-    // now do the proper accept
-    Transform::accept(nv);
-}
+        } 
+    } 
+     
+    // now do the proper accept 
+    Transform::accept(nv); 
+} 

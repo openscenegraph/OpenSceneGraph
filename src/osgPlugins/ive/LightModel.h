99332ef11a15733956fa686dca0757614f133a72
Robert Osfield <robert@openscenegraph.com>
2004-09-10 17:49:50 +0000

    Added missing LightModel.h and .cpp from Stansilav.

---
+++ src/osgPlugins/ive/LightModel.h
@@ -0,0 +1,15 @@
+#ifndef IVE_LIGHTMODEL
+#define IVE_LIGHTMODEL 1
+
+#include <osg/LightModel>
+#include "ReadWrite.h"
+
+namespace ive{
+  class LightModel : public osg::LightModel, public ReadWrite {
+  public:
+    void write(DataOutputStream* out);
+    void read(DataInputStream* in);
+  };
+}
+
+#endif

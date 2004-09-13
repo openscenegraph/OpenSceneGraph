1dcd91d24bfec2e4cbcc5fbe44cc8314599bd886
Robert OSFIELD <robert.osfield@gmail.com>
2004-09-13 13:53:45 +0000

    Improved the handling of building/writing generated databases, and the fixed transition distances on the lower levels of geocentric databases.

--- examples/osgsimulation/osgsimulation.cpp
+++ examples/osgsimulation/osgsimulation.cpp
@@ -6,250 +6,80 @@
 #include <osg/Texture2D>
 #include <osg/PositionAttitudeTransform>
 #include <osg/MatrixTransform>
+#include <osg/CoordinateSystemNode>
 
+#include <osgDB/FileUtils>
 #include <osgDB/ReadFile>
 
 #include <osgText/Text>
 
-#include <osgSim/SphereSegment>
-
-#include <osgParticle/ExplosionEffect>
-#include <osgParticle/SmokeEffect>
-#include <osgParticle/FireEffect>
-#include <osgParticle/ParticleSystemUpdater>
+#include <osgTerrain/DataSet>
 
 #include <osgGA/NodeTrackerManipulator>
 
-// for the grid data..
-#include "../osghangglide/terrain_coords.h"
-
-osg::AnimationPath* createAnimationPath(const osg::Vec3& center,float radius,double looptime)
-{
-    // set up the animation path 
-    osg::AnimationPath* animationPath = new osg::AnimationPath;
-    animationPath->setLoopMode(osg::AnimationPath::LOOP);
-    
-    int numSamples = 40;
-    float yaw = 0.0f;
-    float yaw_delta = 2.0f*osg::PI/((float)numSamples-1.0f);
-    float roll = osg::inDegrees(30.0f);
-    
-    double time=0.0f;
-    double time_delta = looptime/(double)numSamples;
-    for(int i=0;i<numSamples;++i)
-    {
-        osg::Vec3 position(center+osg::Vec3(sinf(yaw)*radius,cosf(yaw)*radius,0.0f));
-        osg::Quat rotation(osg::Quat(roll,osg::Vec3(0.0,1.0,0.0))*osg::Quat(-(yaw+osg::inDegrees(90.0f)),osg::Vec3(0.0,0.0,1.0)));
-        
-        animationPath->insert(time,osg::AnimationPath::ControlPoint(position,rotation));
-
-        yaw += yaw_delta;
-        time += time_delta;
-
-    }
-    return animationPath;    
-}
-
-osg::Node* createMovingModel(const osg::Vec3& center, float radius)
-{
-    float animationLength = 10.0f;
-
-    osg::AnimationPath* animationPath = createAnimationPath(center,radius,animationLength);
-
-    osg::Group* model = new osg::Group;
-
-    osg::Node* glider = osgDB::readNodeFile("glider.osg");
-    if (glider)
-    {
-        glider->setName("glider");
-
-        const osg::BoundingSphere& bs = glider->getBound();
-
-        float size = radius/bs.radius()*0.3f;
-        osg::MatrixTransform* positioned = new osg::MatrixTransform;
-        positioned->setDataVariance(osg::Object::STATIC);
-        positioned->setMatrix(osg::Matrix::translate(-bs.center())*
-                                     osg::Matrix::scale(size,size,size)*
-                                     osg::Matrix::rotate(osg::inDegrees(-90.0f),0.0f,0.0f,1.0f));
-    
-        positioned->addChild(glider);
-    
-        osg::PositionAttitudeTransform* xform = new osg::PositionAttitudeTransform;    
-        xform->getOrCreateStateSet()->setMode(GL_NORMALIZE, osg::StateAttribute::ON);
-        xform->setUpdateCallback(new osg::AnimationPathCallback(animationPath,0.0,1.0));
-        xform->addChild(positioned);
-
-        model->addChild(xform);
-    }
- 
-    osg::Node* cessna = osgDB::readNodeFile("cessna.osg");
-    if (cessna)
-    {
-        cessna->setName("cessna");
-    
-        const osg::BoundingSphere& bs = cessna->getBound();
-
-        osgText::Text* text = new osgText::Text;
-        float size = radius/bs.radius()*0.3f;
-
-        text->setPosition(bs.center());
-        text->setText("Cessna");
-        text->setAlignment(osgText::Text::CENTER_CENTER);
-        text->setAxisAlignment(osgText::Text::SCREEN);
-        text->setCharacterSize(40.0f);
-        text->setCharacterSizeMode(osgText::Text::SCREEN_COORDS);
-        
-        osg::Geode* geode = new osg::Geode;
-        geode->addDrawable(text);
-    
-        osg::LOD* lod = new osg::LOD;
-        lod->setRangeMode(osg::LOD::PIXEL_SIZE_ON_SCREEN);
-        lod->addChild(geode,0.0f,100.0f);
-        lod->addChild(cessna,100.0f,10000.0f);
-
-
-        osg::MatrixTransform* positioned = new osg::MatrixTransform;
-        positioned->getOrCreateStateSet()->setMode(GL_NORMALIZE, osg::StateAttribute::ON);
-        positioned->setDataVariance(osg::Object::STATIC);
-        positioned->setMatrix(osg::Matrix::translate(-bs.center())*
-                                     osg::Matrix::scale(size,size,size)*
-                                     osg::Matrix::rotate(osg::inDegrees(180.0f),0.0f,0.0f,1.0f));
-    
-        //positioned->addChild(cessna);
-        positioned->addChild(lod);
-    
-        osg::MatrixTransform* xform = new osg::MatrixTransform;
-        xform->setUpdateCallback(new osg::AnimationPathCallback(animationPath,0.0f,2.0));
-        xform->addChild(positioned);
-
-        model->addChild(xform);
-    }
-    
-    return model;
-}
-
-
-osg::Vec3 computeTerrainIntersection(osg::Node* subgraph,float x,float y)
-{
-    osgUtil::IntersectVisitor iv;
-    osg::ref_ptr<osg::LineSegment> segDown = new osg::LineSegment;
-
-    const osg::BoundingSphere& bs = subgraph->getBound();
-    float zMax = bs.center().z()+bs.radius();
-    float zMin = bs.center().z()-bs.radius();
-    
-    segDown->set(osg::Vec3(x,y,zMin),osg::Vec3(x,y,zMax));
-    iv.addLineSegment(segDown.get());
-
-    subgraph->accept(iv);
-
-    if (iv.hits())
-    {
-        osgUtil::IntersectVisitor::HitList& hitList = iv.getHitList(segDown.get());
-        if (!hitList.empty())
+class GraphicsContext {
+    public:
+        GraphicsContext()
         {
-            osg::Vec3 ip = hitList.front().getWorldIntersectPoint();
-            return  ip;
-        }
-    }
-
-    return osg::Vec3(x,y,0.0f);
-}
-
-
-//////////////////////////////////////////////////////////////////////////////
-// MAIN SCENE GRAPH BUILDING FUNCTION
-//////////////////////////////////////////////////////////////////////////////
-
-void build_world(osg::Group *root)
-{
-
-    osg::Geode* terrainGeode = new osg::Geode;
-    // create terrain
-    {
-        osg::StateSet* stateset = new osg::StateSet();
-        osg::Image* image = osgDB::readImageFile("Images/lz.rgb");
-        if (image)
-        {
-	    osg::Texture2D* texture = new osg::Texture2D;
-	    texture->setImage(image);
-	    stateset->setTextureAttributeAndModes(0,texture,osg::StateAttribute::ON);
+            rs = new Producer::RenderSurface;
+            rs->setWindowRectangle(0,0,1,1);
+            rs->useBorder(false);
+            rs->useConfigEventThread(false);
+            rs->realize();
+            std::cout<<"Realized window"<<std::endl;
         }
 
-        terrainGeode->setStateSet( stateset );
-
-        float size = 1000; // 10km;
-        float scale = size/39.0f; // 10km;
-        float z_scale = scale*3.0f;
-
-        osg::HeightField* grid = new osg::HeightField;
-        grid->allocateGrid(38,39);
-        grid->setXInterval(scale);
-        grid->setYInterval(scale);
-
-        for(unsigned int r=0;r<39;++r)
+        virtual ~GraphicsContext()
         {
-	    for(unsigned int c=0;c<38;++c)
-	    {
-	        grid->setHeight(c,r,z_scale*vertex[r+c*39][2]);
-	    }
         }
-        terrainGeode->addDrawable(new osg::ShapeDrawable(grid));
         
-        root->addChild(terrainGeode);
-    }    
+    private:
+        Producer::ref_ptr<Producer::RenderSurface> rs;
+};
 
-    // create sphere segment
-    {
-        osgSim::SphereSegment* ss = new osgSim::SphereSegment(
-                        computeTerrainIntersection(terrainGeode,550.0f,780.0f), // center
-                        500.0f, // radius
-                        osg::DegreesToRadians(135.0f),
-                        osg::DegreesToRadians(245.0f),
-                        osg::DegreesToRadians(-10.0f),
-                        osg::DegreesToRadians(30.0f),
-                        60);
-        ss->setAllColors(osg::Vec4(1.0f,1.0f,1.0f,0.5f));
-        ss->setSideColor(osg::Vec4(0.0f,1.0f,1.0f,0.1f));
-
-        root->addChild(ss);        
-    }
-
-
-    // create particle effects
-    {    
-        osg::PositionAttitudeTransform* positionEffects = new osg::PositionAttitudeTransform;
-        positionEffects->setPosition(computeTerrainIntersection(terrainGeode,100.0f,100.0f));
-        root->addChild(positionEffects);
-
-        osgParticle::ExplosionEffect* explosion = new osgParticle::ExplosionEffect;
-        osgParticle::SmokeEffect* smoke = new osgParticle::SmokeEffect;
-        osgParticle::FireEffect* fire = new osgParticle::FireEffect;
-
-        osg::Geode* geode = new osg::Geode;
-        geode->addDrawable(new osg::ShapeDrawable(new osg::Sphere(osg::Vec3(0.0f,0.0f,0.0f),10.0f)));
-        positionEffects->addChild(geode);
-
-        positionEffects->addChild(explosion);
-        positionEffects->addChild(smoke);
-        positionEffects->addChild(fire);
-
-        osgParticle::ParticleSystemUpdater *psu = new osgParticle::ParticleSystemUpdater;
-
-        psu->addParticleSystem(explosion->getParticleSystem());
-        psu->addParticleSystem(smoke->getParticleSystem());
-        psu->addParticleSystem(fire->getParticleSystem());
-
-        // add the updater node to the scene graph
-        root->addChild(psu);
-    }
+osg::Node* createEarth()
+{
+    osg::ref_ptr<osg::Node> scene;
     
-    
-    // create the moving models.
     {
-        root->addChild(createMovingModel(osg::Vec3(500.0f,500.0f,500.0f),100.0f));
+        std::string filename = osgDB::findDataFile("Images/land_shallow_topo_2048.jpg");
+
+        osg::ref_ptr<osgTerrain::DataSet> dataSet = new osgTerrain::DataSet;
+
+        // register the source imagery
+        {
+            osgTerrain::DataSet::Source* source = new osgTerrain::DataSet::Source(osgTerrain::DataSet::Source::IMAGE, filename);
+
+	    source->setCoordinateSystemPolicy(osgTerrain::DataSet::Source::PREFER_CONFIG_SETTINGS);
+            source->setCoordinateSystem(osgTerrain::DataSet::coordinateSystemStringToWTK("WGS84"));
+
+ 	    source->setGeoTransformPolicy(osgTerrain::DataSet::Source::PREFER_CONFIG_SETTINGS_BUT_SCALE_BY_FILE_RESOLUTION);
+            source->setGeoTransformFromRange(-180.0, 180.0, -90.0, 90.0);
+
+            dataSet->addSource(source);
+        }
+
+        // set up destination database paramters.
+        dataSet->setDatabaseType(osgTerrain::DataSet::LOD_DATABASE);
+        dataSet->setConvertFromGeographicToGeocentric(true);
+        dataSet->setDestinationName("test.osg");
+
+        // load the source data and record sizes.
+        dataSet->loadSources();
+
+        GraphicsContext context;
+        dataSet->createDestination(30);
+
+        if (dataSet->getDatabaseType()==osgTerrain::DataSet::LOD_DATABASE) dataSet->buildDestination();
+        else dataSet->writeDestination();
+        
+        scene = dataSet->getDestinationRootNode();
     }
+        
+    return scene.release();
+    
 }
+
 
 class FindNamedNodeVisitor : public osg::NodeVisitor
 {
@@ -273,11 +103,6 @@ public:
     NodeList _foundNodes;
 };
 
-
-
-//////////////////////////////////////////////////////////////////////////////
-// main()
-//////////////////////////////////////////////////////////////////////////////
 
 
 int main(int argc, char **argv)
@@ -319,8 +144,9 @@ int main(int argc, char **argv)
         return 1;
     }
     
-    osg::Group *root = new osg::Group;
-    build_world(root);
+    osg::Node *root = createEarth();
+
+    if (!root) return 0;
    
     // add a viewport to the viewer and attach the scene graph.
     viewer.setSceneData(root);

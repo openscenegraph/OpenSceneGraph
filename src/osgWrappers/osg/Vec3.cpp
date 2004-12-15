5eddf851b5a8499b32afe222bab42346bc75216f
Robert OSFIELD <robert.osfield@gmail.com>
2004-12-15 04:49:05 +0000

    Added missing files

---
+++ src/osgWrappers/osg/Vec3.cpp
@@ -0,0 +1,10 @@
+#include <osgIntrospection/ReflectionMacros>
+#include <osgIntrospection/TypedMethodInfo>
+#include <osgIntrospection/Attributes>
+
+#include <osg/Vec3>
+
+using namespace osgIntrospection;
+
+// simple reflectors for types that are considered 'atomic'
+STD_VALUE_REFLECTOR(osg::Vec3)

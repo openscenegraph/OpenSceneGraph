901ca9ee5ce6f026c0aac20b442b8f9f1dee8864
Robert Osfield <robert@openscenegraph.com>
2004-12-15 04:49:05 +0000

    Added missing files

---
+++ src/osgWrappers/osg/Vec4.cpp
@@ -0,0 +1,10 @@
+#include <osgIntrospection/ReflectionMacros>
+#include <osgIntrospection/TypedMethodInfo>
+#include <osgIntrospection/Attributes>
+
+#include <osg/Vec4>
+
+using namespace osgIntrospection;
+
+// simple reflectors for types that are considered 'atomic'
+STD_VALUE_REFLECTOR(osg::Vec4)

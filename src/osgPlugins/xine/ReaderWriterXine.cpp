0ab57fea6a3acfe28f92f58199d638f88184f459
Robert OSFIELD <robert.osfield@gmail.com>
2005-02-07 15:00:02 +0000

    Temporarily added xine video_out_rgb xine-plugin inline to the OSG's xine plugin

--- src/osgPlugins/xine/ReaderWriterXine.cpp
+++ src/osgPlugins/xine/ReaderWriterXine.cpp
@@ -5,6 +5,7 @@
 #include <osg/Notify>
 #include <osg/Geode>
 #include <osg/GL>
+#include <osg/Timer>
 
 #include <osgDB/Registry>
 #include <osgDB/FileNameUtils>
@@ -14,58 +15,7 @@
 #include <xine/xineutils.h>
 #include <xine/video_out.h>
 
-
-typedef enum
-{
-/*	PX_RGB_PLANAR = 1, ? */
-	PX_ARGB      = 2,  /* 32 bits [a:8@24, r:8@26, g:8@8, b:8@0] */
-	PX_ARGB1555  = 3,  /* 16 bits [a:1@15, r:5@10, g:5@5, b:5@0] */
-	PX_RGB32     = 4,  /* 32 bits [r:8@16, g:8@8, b:8@0] */
-	PX_RGB24     = 5,  /* 24 bits [r:8@16, g:8@8, b:8@0] */
-	PX_RGB16     = 6,  /* 16 bits [r:5@11, g:6@5, b:5@0] */
-	PX_BGRA      = 7,  /* 32 bits [a:8@0, r:8@8, g:8@16, b:8@24] */
-	PX_BGRA5551  = 8,  /* 16 bits [a:1@0, r:5@1, g:5@6, b:5@11] */
-	PX_BGR32     = 9,  /* 32 bits [r:8@0, g:8@8, b:8@16] */
-	PX_BGR24     = 10, /* 24 bits [r:8@0, g:8@8, b:8@16] */
-	PX_BGR16     = 11  /* 16 bits [r:5@0, g:6@5, b:5@11] */
-
-} rgb_pixel_format_t;
-
-
-typedef enum
-{
-	PXLEVEL_NONE = 0,
-	PXLEVEL_R    = (1 << 0),
-	PXLEVEL_G    = (1 << 1),
-	PXLEVEL_B    = (1 << 2),
-	PXLEVEL_ALL  = 7 /* PX_LEVEL_R | PX_LEVEL_G | PX_LEVEL_B */
-
-} rgb_pixel_levels_t;
-
-
-
-/*
- * Applications that want to use this driver must provide a
- * callabck function (eg. for rendering frames);
- * RGBout will pass it a buffer containing pixels in the format
- * specified by "format" (generally you have only to BLIT
- * the buffer if you want to display the frame).
- * "levels" selects which RGB level is visible (if you dont't
- * need this feature, set it to PXLEVEL_ALL).
- *
- * N.B.: DO NOT FREE THE BUFFER
- *
- */
-
-typedef struct
-{
-	rgb_pixel_format_t format;
-	rgb_pixel_levels_t levels;
-        void* user_data;
-	void (*callback) (uint32_t width, uint32_t height, void* imageData, void* userData);
-
-} rgbout_visual_info_t;
-
+#include "video_out_rgb.h"
 
 static int ready = 0;
 
@@ -73,15 +23,30 @@ static void my_render_frame(uint32_t width, uint32_t height, void* data, void* u
 {
     osg::Image* imageStream = (osg::Image*) userData;
     
-    GLenum pixelFormat = GL_RGB;
+    GLenum pixelFormat = GL_BGR;
     
+    if (!ready)
+    {
+        imageStream->allocateImage(width,height,1,pixelFormat,GL_UNSIGNED_BYTE,1);
+        imageStream->setInternalTextureFormat(GL_RGBA);
+    }
+
+    osg::Timer_t start_tick = osg::Timer::instance()->tick();
+    
+    memcpy(imageStream->data(),data,imageStream->getTotalSizeInBytes());
+    
+    osg::notify(osg::NOTICE)<<"image memcpy size="<<imageStream->getTotalSizeInBytes()<<" time="<<osg::Timer::instance()->delta_m(start_tick,osg::Timer::instance()->tick())<<"ms"<<std::endl;
+
+
+    imageStream->dirty();
+/*
     imageStream->setImage(width,height,1,
                       GL_RGB,
                       pixelFormat,GL_UNSIGNED_BYTE,
                       (unsigned char *)data,
                       osg::Image::NO_DELETE,
                       1);
-
+*/
     ready = 1;
 }
 
@@ -104,7 +69,9 @@ class ReaderWriterXine : public osgDB::ReaderWriter
             }
 
             xine_init(_xine);
-        
+            
+          register_plugin(_xine, "/usr/local/lib/osgPlugins", "osgdb_xine.so");      
+
         }
      
         virtual ~ReaderWriterXine()
@@ -137,17 +104,23 @@ class ReaderWriterXine : public osgDB::ReaderWriter
             // create visual
             rgbout_visual_info_t* visual = new rgbout_visual_info_t;
 	    visual->levels = PXLEVEL_ALL;
-            //visual->format = PX_BGRA;
             visual->format = PX_BGR24;
             visual->user_data = imageStream;
 	    visual->callback = my_render_frame;
 
             // set up drivers
-            xine_video_port_t* vo = xine_open_video_driver(_xine, "rgb", XINE_VISUAL_TYPE_FB, (void*)visual);
-            xine_audio_port_t* ao = xine_open_audio_driver(_xine, "none", NULL);
+            xine_video_port_t* vo = xine_open_video_driver(_xine, "rgb", XINE_VISUAL_TYPE_RGBOUT, (void*)visual);
+            xine_audio_port_t* ao = xine_open_audio_driver(_xine, "oss", NULL);
+
+            if (!vo)
+            {
+                osg::notify(osg::NOTICE)<<"Failed to create video driver"<<std::endl;
+                exit(0);
+            }
+            
 
             // set up stream
-            xine_stream_t* stream = xine_stream_new(_xine, ao, vo);
+            xine_stream_t* stream = xine_stream_new(_xine, 0, vo);
 
             // set up queue
             // xine_event_queue_t* queue = xine_event_new_queue(stream);
@@ -162,7 +135,7 @@ class ReaderWriterXine : public osgDB::ReaderWriter
             while (!ready)
             {
                 osg::notify(osg::NOTICE)<<"waiting..."<<std::endl;
-                usleep(1000);
+                usleep(10000);
             }
 
             return imageStream;

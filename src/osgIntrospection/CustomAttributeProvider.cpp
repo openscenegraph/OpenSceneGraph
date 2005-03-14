cd1dec50f4bdf378a951bb1b534a25b958432467
Robert OSFIELD <robert.osfield@gmail.com>
2005-03-14 09:28:31 +0000

    Changed tabs to four spaces

--- src/osgIntrospection/CustomAttributeProvider.cpp
+++ src/osgIntrospection/CustomAttributeProvider.cpp
@@ -5,39 +5,39 @@ using namespace osgIntrospection;
 
 bool CustomAttributeProvider::isDefined(const Type &type, bool inherit) const
 {
-	for (CustomAttributeList::const_iterator i=attribs_.begin(); i!=attribs_.end(); ++i)
-		if (typeid(**i) == type.getStdTypeInfo()) return true;
+    for (CustomAttributeList::const_iterator i=attribs_.begin(); i!=attribs_.end(); ++i)
+        if (typeid(**i) == type.getStdTypeInfo()) return true;
 
-	if (inherit)
-	{
-		CustomAttributeProviderList providers;
-		getInheritedProviders(providers);
+    if (inherit)
+    {
+        CustomAttributeProviderList providers;
+        getInheritedProviders(providers);
 
-		for (CustomAttributeProviderList::const_iterator i=providers.begin(); i!=providers.end(); ++i)
-		{
-			if ((*i)->isDefined(type, true)) return true;
-		}
-	}
+        for (CustomAttributeProviderList::const_iterator i=providers.begin(); i!=providers.end(); ++i)
+        {
+            if ((*i)->isDefined(type, true)) return true;
+        }
+    }
 
-	return false;
+    return false;
 }
 
 const CustomAttribute *CustomAttributeProvider::getAttribute(const Type &type, bool inherit) const
 {
-	for (CustomAttributeList::const_iterator i=attribs_.begin(); i!=attribs_.end(); ++i)
-		if (typeid(**i) == type.getStdTypeInfo()) return *i;
+    for (CustomAttributeList::const_iterator i=attribs_.begin(); i!=attribs_.end(); ++i)
+        if (typeid(**i) == type.getStdTypeInfo()) return *i;
 
-	if (inherit)
-	{
-		CustomAttributeProviderList providers;
-		getInheritedProviders(providers);
+    if (inherit)
+    {
+        CustomAttributeProviderList providers;
+        getInheritedProviders(providers);
 
-		for (CustomAttributeProviderList::const_iterator i=providers.begin(); i!=providers.end(); ++i)
-		{
-			const CustomAttribute *ca = (*i)->getAttribute(type, true);
-			if (ca) return ca;
-		}
-	}
+        for (CustomAttributeProviderList::const_iterator i=providers.begin(); i!=providers.end(); ++i)
+        {
+            const CustomAttribute *ca = (*i)->getAttribute(type, true);
+            if (ca) return ca;
+        }
+    }
 
-	return 0;
+    return 0;
 }

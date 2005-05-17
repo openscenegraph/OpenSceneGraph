9e60c7aebb5df0cacdcac56349e56ffabd2134b7
Robert OSFIELD <robert.osfield@gmail.com>
2005-05-17 09:06:12 +0000

    From Thom DeCarlo, build fixes for Cygwin.

--- src/osgPlugins/net/sockstream.cpp
+++ src/osgPlugins/net/sockstream.cpp
@@ -64,7 +64,7 @@
 #include <sstream>
 #include <string>
 
-#if (defined(__APPLE__)&&(__GNUC__<4)) || defined(WIN32) || defined (__sgi) || defined (__hpux) 
+#if (defined(__APPLE__)&&(__GNUC__<4)) || (defined(WIN32)&&!defined(__CYGWIN__)) || defined (__sgi) || defined (__hpux) 
 typedef int socklen_t;
 #endif
 

ded28eb31e43974db931fb10a10fa1a81f56fd50
Robert OSFIELD <robert.osfield@gmail.com>
2005-08-26 14:00:02 +0000

    Added beginings of new OverlayNode, for managing a render to texture which is then used to overaly other parts of the scene such as terrain.

---
+++ src/osgSim/OverlayNode.cpp
@@ -0,0 +1,36 @@
+/* -*-c++-*- OpenSceneGraph - Copyright (C) 1998-2005 Robert Osfield 
+ *
+ * This library is open source and may be redistributed and/or modified under  
+ * the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or 
+ * (at your option) any later version.  The full license is in LICENSE file
+ * included with this distribution, and on the openscenegraph.org website.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * OpenSceneGraph Public License for more details.
+*/
+
+#include <osgSim/OverlayNode>
+
+using namespace osgSim;
+
+OverlayNode::OverlayNode()
+{
+}
+
+OverlayNode::OverlayNode(const OverlayNode& es, const osg::CopyOp& copyop)
+{
+}
+
+void OverlayNode::traverse(osg::NodeVisitor& nv)
+{
+}
+
+void OverlayNode::setOverlaySubgraph(osg::Node* node)
+{
+}
+
+void OverlayNode::dirtyOverlayTexture()
+{
+}

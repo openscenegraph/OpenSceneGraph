34dd065d66399a66f7cf20c876aff39203d84a51
Robert Osfield <robert@openscenegraph.com>
2005-10-07 07:46:48 +0000

    Added into CVS missing PolgonMode files.

---
+++ src/osgPlugins/ive/PolygonMode.h
@@ -0,0 +1,16 @@
+#ifndef IVE_POLYGONMODE
+#define IVE_POLYGONMODE 1
+
+#include <osg/PolygonMode>
+#include "ReadWrite.h"
+
+namespace ive{
+class PolygonMode : public osg::PolygonMode, public ReadWrite {
+public:
+	void write(DataOutputStream* out);
+	void read(DataInputStream* in);
+};
+}
+
+#endif
+

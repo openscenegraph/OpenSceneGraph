e14262df852340a2e72407552a71fe9b95e225ab
Robert Osfield <robert@openscenegraph.com>
2005-10-25 14:01:50 +0000

    Updated wrappers.

--- src/osgWrappers/osgUtil/DelaunayTriangulator.cpp
+++ src/osgWrappers/osgUtil/DelaunayTriangulator.cpp
@@ -12,6 +12,7 @@
 #include <osg/Array>
 #include <osg/CopyOp>
 #include <osg/PrimitiveSet>
+#include <osg/Vec3>
 #include <osgUtil/DelaunayTriangulator>
 
 // Must undefine IN and OUT macros defined in Windows headers
@@ -22,6 +23,25 @@
 #undef OUT
 #endif
 
+BEGIN_OBJECT_REFLECTOR(osgUtil::DelaunayConstraint)
+	I_BaseType(osg::Geometry);
+	I_Constructor0();
+	I_Method3(void, addtriangle, IN, const int, i1, IN, const int, i2, IN, const int, i3);
+	I_Method0(const osg::DrawElementsUInt *, getTriangles);
+	I_Method0(osg::DrawElementsUInt *, getTriangles);
+	I_Method1(osg::Vec3Array *, getPoints, IN, const osg::Vec3Array *, points);
+	I_Method0(osg::DrawElementsUInt *, makeDrawable);
+	I_Method1(void, merge, IN, osgUtil::DelaunayConstraint *, dco);
+	I_Method1(void, removeVerticesInside, IN, const osgUtil::DelaunayConstraint *, dco);
+	I_Method1(float, windingNumber, IN, const osg::Vec3, testpoint);
+	I_Method1(bool, contains, IN, const osg::Vec3, testpoint);
+	I_Method1(bool, outside, IN, const osg::Vec3, testpoint);
+	I_Method0(void, handleOverlaps);
+	I_ReadOnlyProperty(osg::DrawElementsUInt *, Triangles);
+END_REFLECTOR
+
+TYPE_NAME_ALIAS(std::vector< osg::ref_ptr< osgUtil::DelaunayConstraint > >, osgUtil::DelaunayTriangulator::linelist);
+
 BEGIN_OBJECT_REFLECTOR(osgUtil::DelaunayTriangulator)
 	I_BaseType(osg::Referenced);
 	I_Constructor0();
@@ -30,14 +50,30 @@ BEGIN_OBJECT_REFLECTOR(osgUtil::DelaunayTriangulator)
 	I_Method0(const osg::Vec3Array *, getInputPointArray);
 	I_Method0(osg::Vec3Array *, getInputPointArray);
 	I_Method1(void, setInputPointArray, IN, osg::Vec3Array *, points);
+	I_Method1(void, addInputConstraint, IN, osgUtil::DelaunayConstraint *, dc);
 	I_Method0(const osg::Vec3Array *, getOutputNormalArray);
 	I_Method0(osg::Vec3Array *, getOutputNormalArray);
 	I_Method1(void, setOutputNormalArray, IN, osg::Vec3Array *, normals);
 	I_Method0(bool, triangulate);
 	I_Method0(const osg::DrawElementsUInt *, getTriangles);
 	I_Method0(osg::DrawElementsUInt *, getTriangles);
+	I_Method1(void, removeInternalTriangles, IN, osgUtil::DelaunayConstraint *, constraint);
 	I_Property(osg::Vec3Array *, InputPointArray);
 	I_Property(osg::Vec3Array *, OutputNormalArray);
 	I_ReadOnlyProperty(osg::DrawElementsUInt *, Triangles);
 END_REFLECTOR
+
+BEGIN_VALUE_REFLECTOR(osg::ref_ptr< osgUtil::DelaunayConstraint >)
+	I_Constructor0();
+	I_Constructor1(IN, osgUtil::DelaunayConstraint *, t);
+	I_Constructor1(IN, const osg::ref_ptr< osgUtil::DelaunayConstraint > &, rp);
+	I_Method0(bool, valid);
+	I_Method0(osgUtil::DelaunayConstraint *, get);
+	I_Method0(const osgUtil::DelaunayConstraint *, get);
+	I_Method0(osgUtil::DelaunayConstraint *, take);
+	I_Method0(osgUtil::DelaunayConstraint *, release);
+	I_ReadOnlyProperty(osgUtil::DelaunayConstraint *, );
+END_REFLECTOR
+
+STD_VECTOR_REFLECTOR(std::vector< osg::ref_ptr< osgUtil::DelaunayConstraint > >);
 

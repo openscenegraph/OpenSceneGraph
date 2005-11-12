7829b8257d12ab83b52c385f0b32029284fec8bc
Robert OSFIELD <robert.osfield@gmail.com>
2005-11-12 13:27:49 +0000

    Added .osg support for osgParticle::SmokeTrailEffect

---
+++ src/osgPlugins/osgParticle/IO_SmokeTrailEffect.cpp
@@ -0,0 +1,28 @@
+
+#include <osgParticle/SmokeTrailEffect>
+
+#include <osgDB/Registry>
+#include <osgDB/Input>
+#include <osgDB/Output>
+
+bool  SmokeTrailEffect_readLocalData(osg::Object &obj, osgDB::Input &fr);
+bool  SmokeTrailEffect_writeLocalData(const osg::Object &obj, osgDB::Output &fw);
+
+osgDB::RegisterDotOsgWrapperProxy  SmokeTrailEffect_Proxy
+(
+    new osgParticle::SmokeTrailEffect,
+    "SmokeTrailEffect",
+    "Object Node ParticleEffect SmokeTrailEffect",
+    SmokeTrailEffect_readLocalData,
+    SmokeTrailEffect_writeLocalData
+);
+
+bool SmokeTrailEffect_readLocalData(osg::Object &, osgDB::Input &)
+{
+    return false;
+}
+
+bool SmokeTrailEffect_writeLocalData(const osg::Object &, osgDB::Output &)
+{
+    return false;
+}

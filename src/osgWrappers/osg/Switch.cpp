ea4ca8063776460fae47c2e843c3d31ecad30747
Robert OSFIELD <robert.osfield@gmail.com>
2005-12-15 15:50:17 +0000

    Removed Switch::removedChild(Node*) as the Group::removeChild(Node*) implementation should be sufficient, as it calls the virtual removeChild(uint,uint).

--- src/osgWrappers/osg/Switch.cpp
+++ src/osgWrappers/osg/Switch.cpp
@@ -43,7 +43,6 @@ BEGIN_OBJECT_REFLECTOR(osg::Switch)
 	I_Method2(bool, addChild, IN, osg::Node *, child, IN, bool, value);
 	I_Method2(bool, insertChild, IN, unsigned int, index, IN, osg::Node *, child);
 	I_Method3(bool, insertChild, IN, unsigned int, index, IN, osg::Node *, child, IN, bool, value);
-	I_Method1(bool, removeChild, IN, osg::Node *, child);
 	I_MethodWithDefaults2(bool, removeChild, IN, unsigned int, pos, , IN, unsigned int, numChildrenToRemove, 1);
 	I_Method2(void, setValue, IN, unsigned int, pos, IN, bool, value);
 	I_Method1(bool, getValue, IN, unsigned int, pos);

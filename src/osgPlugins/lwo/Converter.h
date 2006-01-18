fe3b68dbb1d9536a807aed97d607ebbc22993f1d
Robert Osfield <robert@openscenegraph.com>
2006-01-18 12:13:48 +0000

    From Marco Jez:
    here is a patch that enables a new option named "BIND_TEXTURE_MAP" in the LWO plugin. Its purpose is to allow explicit binding between texture UV maps defined in the LWO file and OpenGL texture units, overriding the default mechanism that allocates texture units automatically. This is useful when you have an UV map built in Lightwave (for example an atlas map) but no textures actually using it, so you can keep the UV map (that would be discarded otherwise) and add a texture later int your program.
    
    Syntax is:
    BIND_TEXTURE_MAP <map_name> <texunit>

--- src/osgPlugins/lwo/Converter.h
+++ src/osgPlugins/lwo/Converter.h
@@ -21,50 +21,51 @@
 namespace lwosg
 {
 
-	class Converter {
-	public:
+    class Converter {
+    public:
 
-		struct Options {
-			osg::ref_ptr<CoordinateSystemFixer> csf;
-			int max_tex_units;
-			bool apply_light_model;
-			bool use_osgfx;
-			bool force_arb_compression;
-			bool combine_geodes;
+        struct Options {
+            osg::ref_ptr<CoordinateSystemFixer> csf;
+            int max_tex_units;
+            bool apply_light_model;
+            bool use_osgfx;
+            bool force_arb_compression;
+            bool combine_geodes;
+            VertexMap_binding_map texturemap_bindings;
 
-			Options()
-			:	csf(new LwoCoordFixer), 
-				max_tex_units(0), 
-				apply_light_model(true), 
-				use_osgfx(false), 
-				force_arb_compression(false), 
-				combine_geodes(false)
-			{
-			}
-		};
+            Options()
+            :    csf(new LwoCoordFixer), 
+                max_tex_units(0), 
+                apply_light_model(true), 
+                use_osgfx(false), 
+                force_arb_compression(false), 
+                combine_geodes(false)
+            {
+            }
+        };
 
-		Converter();
-		Converter(const Options &options, const osgDB::ReaderWriter::Options* db_options);
+        Converter();
+        Converter(const Options &options, const osgDB::ReaderWriter::Options* db_options);
 
-		osg::Group *convert(Object &obj);
-		osg::Group *convert(const iff::Chunk_list &data);
-		osg::Group *convert(const std::string &filename);
+        osg::Group *convert(Object &obj);
+        osg::Group *convert(const iff::Chunk_list &data);
+        osg::Group *convert(const std::string &filename);
 
-		inline osg::Group *get_root() { return root_.get(); }
-		inline const osg::Group *get_root() const { return root_.get(); }
+        inline osg::Group *get_root() { return root_.get(); }
+        inline const osg::Group *get_root() const { return root_.get(); }
 
-		inline const Options &get_options() const { return options_; }
-		inline Options &get_options() { return options_; }
-		inline void set_options(const Options &options) { options_ = options; }
+        inline const Options &get_options() const { return options_; }
+        inline Options &get_options() { return options_; }
+        inline void set_options(const Options &options) { options_ = options; }
 
-	protected:
-		void build_scene_graph(Object &obj);
+    protected:
+        void build_scene_graph(Object &obj);
 
-	private:
-		osg::ref_ptr<osg::Group> root_;
-		Options options_;
-		osg::ref_ptr<const osgDB::ReaderWriter::Options> db_options_;
-	};
+    private:
+        osg::ref_ptr<osg::Group> root_;
+        Options options_;
+        osg::ref_ptr<const osgDB::ReaderWriter::Options> db_options_;
+    };
 
 }
 
--- src/osgPlugins/lwo/VertexMap.h
+++ src/osgPlugins/lwo/VertexMap.h
@@ -21,53 +21,54 @@
 namespace lwosg
 {
 
-	/////////////////////////////////////////////////////////////////////////
-	// VERTEX MAP
+    /////////////////////////////////////////////////////////////////////////
+    // VERTEX MAP
 
-	typedef std::map<int, osg::Vec4> VertexMap_impl;
+    typedef std::map<int, osg::Vec4> VertexMap_impl;
 
-	class VertexMap: public VertexMap_impl, public osg::Referenced {
-	public:
-		VertexMap(): VertexMap_impl(), osg::Referenced() {}
+    class VertexMap: public VertexMap_impl, public osg::Referenced {
+    public:
+        VertexMap(): VertexMap_impl(), osg::Referenced() {}
 
-		osg::Vec2Array *asVec2Array(int num_vertices, const osg::Vec2 &default_value = osg::Vec2(0, 0), const osg::Vec2 &modulator = osg::Vec2(1, 1)) const;
-		osg::Vec3Array *asVec3Array(int num_vertices, const osg::Vec3 &default_value = osg::Vec3(0, 0, 0), const osg::Vec3 &modulator = osg::Vec3(1, 1, 1)) const;
-		osg::Vec4Array *asVec4Array(int num_vertices, const osg::Vec4 &default_value = osg::Vec4(0, 0, 0, 0), const osg::Vec4 &modulator = osg::Vec4(1, 1, 1, 1)) const;
+        osg::Vec2Array *asVec2Array(int num_vertices, const osg::Vec2 &default_value = osg::Vec2(0, 0), const osg::Vec2 &modulator = osg::Vec2(1, 1)) const;
+        osg::Vec3Array *asVec3Array(int num_vertices, const osg::Vec3 &default_value = osg::Vec3(0, 0, 0), const osg::Vec3 &modulator = osg::Vec3(1, 1, 1)) const;
+        osg::Vec4Array *asVec4Array(int num_vertices, const osg::Vec4 &default_value = osg::Vec4(0, 0, 0, 0), const osg::Vec4 &modulator = osg::Vec4(1, 1, 1, 1)) const;
 
-		VertexMap *remap(const std::vector<int> &remapping) const;
+        VertexMap *remap(const std::vector<int> &remapping) const;
 
-	protected:
-		virtual ~VertexMap() {}
-		VertexMap &operator=(const VertexMap &) { return *this; }
-	};
+    protected:
+        virtual ~VertexMap() {}
+        VertexMap &operator=(const VertexMap &) { return *this; }
+    };
 
 
-	/////////////////////////////////////////////////////////////////////////
-	// VERTEX MAP MAP
+    /////////////////////////////////////////////////////////////////////////
+    // VERTEX MAP MAP
 
-	typedef std::map<std::string, osg::ref_ptr<VertexMap> > VertexMap_map_impl;
+    typedef std::map<std::string, osg::ref_ptr<VertexMap> > VertexMap_map_impl;
+    typedef std::multimap<std::string, int> VertexMap_binding_map;
 
-	class VertexMap_map: public VertexMap_map_impl, public osg::Referenced {
-	public:
-		VertexMap_map(): VertexMap_map_impl(), osg::Referenced() {}
+    class VertexMap_map: public VertexMap_map_impl, public osg::Referenced {
+    public:
+        VertexMap_map(): VertexMap_map_impl(), osg::Referenced() {}
 
-		VertexMap *getOrCreate(const std::string &name)
-		{
-			osg::ref_ptr<VertexMap> &vmap = operator[](name);
-			if (!vmap.valid()) {
-				vmap = new VertexMap;
-			}
-			return vmap.get();
-		}
+        VertexMap *getOrCreate(const std::string &name)
+        {
+            osg::ref_ptr<VertexMap> &vmap = operator[](name);
+            if (!vmap.valid()) {
+                vmap = new VertexMap;
+            }
+            return vmap.get();
+        }
 
-		VertexMap_map *remap(const std::vector<int> &remapping) const;
+        VertexMap_map *remap(const std::vector<int> &remapping) const;
 
-	protected:
-		virtual ~VertexMap_map() {}
-		VertexMap_map &operator=(const VertexMap_map &) { return *this; }
+    protected:
+        virtual ~VertexMap_map() {}
+        VertexMap_map &operator=(const VertexMap_map &) { return *this; }
 
-	private:
-	};
+    private:
+    };
 
 }
 

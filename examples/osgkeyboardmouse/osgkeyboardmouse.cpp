1bf5abbe1c214ca70f2a1c1aa8039151f1a35ce9
Robert OSFIELD <robert.osfield@gmail.com>
2006-06-12 14:04:40 +0000

    Added pick handler.

--- examples/osgkeyboardmouse/osgkeyboardmouse.cpp
+++ examples/osgkeyboardmouse/osgkeyboardmouse.cpp
@@ -9,6 +9,7 @@
 
 #include <osg/Timer>
 #include <osg/io_utils>
+#include <osg/observer_ptr>
 
 #include <osgUtil/SceneView>
 #include <osgUtil/IntersectVisitor>
@@ -23,7 +24,7 @@
 
 #include <osgFX/Scribe>
 
-// Begining of glue classes to adapter Producer's keyboard mouse events to osgGA's abstraction events.
+// ----------- Begining of glue classes to adapter Producer's keyboard mouse events to osgGA's abstraction events.
 class MyKeyboardMouseCallback : public Producer::KeyboardMouseCallback
 {
 public:
@@ -99,8 +100,165 @@ public:
     
 };
 
-// End of glue classes to adapter Producer's keyboard mouse events to osgGA's abstraction events.
+// ----------- End of glue classes to adapter Producer's keyboard mouse events to osgGA's abstraction events.
 
+
+class CreateModelToSaveVisitor : public osg::NodeVisitor
+{
+public:
+
+    CreateModelToSaveVisitor():
+        osg::NodeVisitor(osg::NodeVisitor::TRAVERSE_ALL_CHILDREN)        
+    {
+        _group = new osg::Group;
+        _addToModel = false;
+    }
+    
+    virtual void apply(osg::Node& node)
+    {
+        osgFX::Scribe* scribe = dynamic_cast<osgFX::Scribe*>(&node);
+        if (scribe)
+        {
+            for(unsigned int i=0; i<scribe->getNumChildren(); ++i)
+            {
+                _group->addChild(scribe->getChild(i));
+            }
+        }
+        else
+        {
+            traverse(node);
+        }
+    }
+    
+    osg::ref_ptr<osg::Group> _group;
+    bool _addToModel;
+};
+
+// class to handle events with a pick
+class PickHandler : public osgGA::GUIEventHandler {
+public: 
+
+    PickHandler(osgUtil::SceneView* sceneView):
+        _sceneView(sceneView),
+        _mx(0.0),_my(0.0) {}
+
+    ~PickHandler() {}
+
+    bool handle(const osgGA::GUIEventAdapter& ea,osgGA::GUIActionAdapter&)
+    {
+        switch(ea.getEventType())
+        {
+            case(osgGA::GUIEventAdapter::KEYUP):
+            {
+                if (ea.getKey()=='s')
+                {
+                    saveSelectedModel();
+                }
+                return false;
+            }
+            case(osgGA::GUIEventAdapter::PUSH):
+            case(osgGA::GUIEventAdapter::MOVE):
+            {
+                _mx = ea.getX();
+                _my = ea.getY();
+                return false;
+            }
+            case(osgGA::GUIEventAdapter::RELEASE):
+            {
+                if (_mx == ea.getX() && _my == ea.getY())
+                {
+                    // only do a pick if the mouse hasn't moved
+                    pick(ea);
+                }
+                return true;
+            }    
+
+            default:
+                return false;
+        }
+    }
+
+    void pick(const osgGA::GUIEventAdapter& ea)
+    {
+
+        osg::Node* scene = _sceneView.valid() ? _sceneView->getSceneData() : 0;
+        if (!scene) return;
+
+        // remap the mouse x,y into viewport coordinates.
+        
+        float mx = _sceneView->getViewport()->x() + (int)((float)_sceneView->getViewport()->width()*(ea.getXnormalized()*0.5f+0.5f));
+        float my = _sceneView->getViewport()->y() + (int)((float)_sceneView->getViewport()->height()*(ea.getYnormalized()*0.5f+0.5f));
+   
+        // do the pick traversal.
+        osgUtil::PickVisitor pick(_sceneView->getViewport(),
+                                  _sceneView->getProjectionMatrix(), 
+                                  _sceneView->getViewMatrix(), mx, my);
+        scene->accept(pick);
+
+        osgUtil::PickVisitor::LineSegmentHitListMap& segHitList = pick.getSegHitList();
+        if (!segHitList.empty() && !segHitList.begin()->second.empty())
+        {
+            std::cout<<"Got hits"<<std::endl;
+
+            // get the hits for the first segment
+            osgUtil::PickVisitor::HitList& hits = segHitList.begin()->second;
+
+            // just take the first hit - nearest the eye point.
+            osgUtil::Hit& hit = hits.front();
+
+            osg::NodePath& nodePath = hit._nodePath;
+            osg::Node* node = (nodePath.size()>=1)?nodePath[nodePath.size()-1]:0;
+            osg::Group* parent = (nodePath.size()>=2)?dynamic_cast<osg::Group*>(nodePath[nodePath.size()-2]):0;
+
+            if (node) std::cout<<"  Hits "<<node->className()<<" nodePath size"<<nodePath.size()<<std::endl;
+
+            // now we try to decorate the hit node by the osgFX::Scribe to show that its been "picked"
+            if (parent && node)
+            {
+
+                std::cout<<"  parent "<<parent->className()<<std::endl;
+
+                osgFX::Scribe* parentAsScribe = dynamic_cast<osgFX::Scribe*>(parent);
+                if (!parentAsScribe)
+                {
+                    // node not already picked, so highlight it with an osgFX::Scribe
+                    osgFX::Scribe* scribe = new osgFX::Scribe();
+                    scribe->addChild(node);
+                    parent->replaceChild(node,scribe);
+                }
+                else
+                {
+                    // node already picked so we want to remove scribe to unpick it.
+                    osg::Node::ParentList parentList = parentAsScribe->getParents();
+                    for(osg::Node::ParentList::iterator itr=parentList.begin();
+                        itr!=parentList.end();
+                        ++itr)
+                    {
+                        (*itr)->replaceChild(parentAsScribe,node);
+                    }
+                }
+            }
+
+        }
+    }
+
+    void saveSelectedModel()
+    {
+        CreateModelToSaveVisitor cmtsv;
+        _sceneView->getSceneData()->accept(cmtsv);
+        
+        if (cmtsv._group->getNumChildren()>0)
+        {
+            std::cout<<"Writing selected compoents to 'selected_model.osg'"<<std::endl;
+            osgDB::writeNodeFile(*cmtsv._group, "selected_model.osg");
+        }
+    }
+
+protected:
+
+    osg::observer_ptr<osgUtil::SceneView> _sceneView;
+    float _mx,_my;
+};
 
 int main( int argc, char **argv )
 {
@@ -151,6 +309,7 @@ int main( int argc, char **argv )
     osg::ref_ptr<osgGA::StateSetManipulator> statesetManipulator = new osgGA::StateSetManipulator;
     statesetManipulator->setStateSet(sceneView->getGlobalStateSet());
     eventHandlers.push_back(statesetManipulator.get());
+    eventHandlers.push_back(new PickHandler(sceneView.get()));
     
     // create an event visitor to pass the events down to the scene graph nodes
     osg::ref_ptr<osgGA::EventVisitor> eventVisitor = new osgGA::EventVisitor;

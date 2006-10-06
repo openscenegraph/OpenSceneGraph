db41d84ed81ffcc83095f67b03f3b4e1cd8215d3
Robert OSFIELD <robert.osfield@gmail.com>
2006-10-06 14:16:11 +0000

    First cut at class interfaces and stubs for implementations for the new osgShadow NodeKit

---
+++ src/osgShadow/Version.cpp
@@ -0,0 +1,12 @@
+#include <osgShadow/Version>
+
+const char* osgShadowGetVersion()
+{
+    return "1.2";
+}
+
+
+const char* osgShaodowGetLibraryName()
+{
+    return "OpenSceneGraph Shadow Library";
+}

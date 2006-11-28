8a9e8efc82195d663f19b4f8d83fba156567a9b9
Robert OSFIELD <robert.osfield@gmail.com>
2006-11-28 09:14:46 +0000

    Added missing Camera.h and removed redundent CameraNode.h

---
+++ src/osgPlugins/ive/Camera.h
@@ -0,0 +1,15 @@
+#ifndef IVE_CAMERA
+#define IVE_CAMERA 1
+
+#include <osg/Camera>
+#include "ReadWrite.h"
+
+namespace ive{
+class Camera : public osg::Camera, public ReadWrite {
+public:
+	void write(DataOutputStream* out);
+	void read(DataInputStream* in);
+};
+}
+
+#endif

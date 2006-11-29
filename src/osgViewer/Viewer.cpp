dc546dce890b2cd1a7261af57752b8a543cbf427
Robert OSFIELD <robert.osfield@gmail.com>
2006-11-29 11:00:02 +0000

    Added dummy virtual method implementations

--- src/osgViewer/Viewer.cpp
+++ src/osgViewer/Viewer.cpp
@@ -6,7 +6,10 @@
  * included with this distribution, and on the openscenegraph.org website.
  * 
  * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * but WITHOUT ANY WARRANTY
+{
+}
+ without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
  * OpenSceneGraph Public License for more details.
 */
@@ -17,8 +20,50 @@ using namespace osgViewer;
 
 Viewer::Viewer()
 {
+    osg::notify(osg::NOTICE)<<"Viewer::Viewer() not implemented yet."<<std::endl;
 }
 
 Viewer::~Viewer()
 {
 }
+
+void Viewer::frame()
+{
+    osg::notify(osg::NOTICE)<<"Viewer::frame() not implemented yet."<<std::endl;
+}
+
+void Viewer::frameAdvance()
+{
+    osg::notify(osg::NOTICE)<<"Viewer::frameAdvance() not implemented yet."<<std::endl;
+}
+
+void Viewer::frameEventTraversal()
+{
+    osg::notify(osg::NOTICE)<<"Viewer::frameEventTraversal() not implemented yet."<<std::endl;
+}
+
+void Viewer::frameUpdateTraversal()
+{
+    osg::notify(osg::NOTICE)<<"Viewer::frameUpdateTraversal() not implemented yet."<<std::endl;
+}
+
+void Viewer::frameCullTraversal()
+{
+    osg::notify(osg::NOTICE)<<"Viewer::frameCullTraversal() not implemented yet."<<std::endl;
+}
+
+void Viewer::frameDrawTraversal()
+{
+    osg::notify(osg::NOTICE)<<"Viewer::frameDrawTraversal() not implemented yet."<<std::endl;
+}
+
+void Viewer::releaseAllGLObjects()
+{
+    osg::notify(osg::NOTICE)<<"Viewer::releaseAllGLObjects() not implemented yet."<<std::endl;
+}
+
+void Viewer::cleanup()
+{
+    osg::notify(osg::NOTICE)<<"Viewer::cleanup() not implemented yet."<<std::endl;
+}
+

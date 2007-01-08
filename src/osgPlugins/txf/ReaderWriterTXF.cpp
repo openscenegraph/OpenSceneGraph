b9f629f0cb48b9e3610f92cfb62290dbf589256e
Robert Osfield <robert@openscenegraph.com>
2007-01-08 12:04:04 +0000

    From Mathias Fröhlich, txf plugin for reading .txf textured texture files.

---
+++ src/osgPlugins/txf/ReaderWriterTXF.cpp
@@ -0,0 +1,62 @@
+/* -*-c++-*- OpenSceneGraph - Copyright (C) 2006 Mathias Froehlich
+ *
+ * This library is open source and may be redistributed and/or modified under
+ * the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or
+ * (at your option) any later version.  The full license is in LICENSE file
+ * included with this distribution, and on the openscenegraph.org website.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * OpenSceneGraph Public License for more details.
+*/
+
+#include <fstream>
+
+#include <osgDB/FileNameUtils>
+#include <osgDB/FileUtils>
+#include <osgDB/Registry>
+#include <osg/Notify>
+
+#include "TXFFont.h"
+
+class ReaderWriterTXF : public osgDB::ReaderWriter
+{
+    public:
+        virtual const char* className() const { return "TXF Font Reader/Writer"; }
+        
+        virtual bool acceptsExtension(const std::string& extension) const
+        {
+            return osgDB::equalCaseInsensitive(extension, "txf"); // GLU texture fonts
+        }
+
+        virtual ReadResult readObject(const std::string& file, const osgDB::ReaderWriter::Options* options) const
+        {
+            std::string ext = osgDB::getLowerCaseFileExtension(file);
+            if (!acceptsExtension(ext)) return ReadResult::FILE_NOT_HANDLED;
+
+            std::string fileName = osgDB::findDataFile(file, options);
+            if (fileName.empty()) return ReadResult::FILE_NOT_FOUND;
+
+            std::ifstream stream;
+            stream.open(fileName.c_str(), std::ios::in);
+            if (!stream.is_open()) return ReadResult::FILE_NOT_FOUND;
+
+            TXFFont* impl = new TXFFont(fileName);
+            osg::ref_ptr<osgText::Font> font = new osgText::Font(impl);
+            if (!impl->loadFont(stream)) return ReadResult::FILE_NOT_HANDLED;
+            return font.release();
+        }
+
+        virtual ReadResult readObject(std::istream& stream, const osgDB::ReaderWriter::Options*) const
+        {
+            TXFFont* impl = new TXFFont("streamed font");
+            osg::ref_ptr<osgText::Font> font = new osgText::Font(impl);
+            if (!impl->loadFont(stream)) return ReadResult::FILE_NOT_HANDLED;
+            return font.release();
+        }
+};
+
+// now register with Registry to instantiate the above
+// reader/writer.
+osgDB::RegisterReaderWriterProxy<ReaderWriterTXF> g_readerWriter_TXF_Proxy;
---
+++ src/osgPlugins/txf/TXFFont.h
@@ -0,0 +1,48 @@
+/* -*-c++-*- OpenSceneGraph - Copyright (C) 2006 Mathias Froehlich
+ *
+ * This library is open source and may be redistributed and/or modified under
+ * the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or
+ * (at your option) any later version.  The full license is in LICENSE file
+ * included with this distribution, and on the openscenegraph.org website.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * OpenSceneGraph Public License for more details.
+ */
+
+#ifndef TXF_FONT
+#define TXF_FONT
+
+#include <iosfwd>
+#include <string>
+#include <map>
+#include <osgText/Font>
+
+class TXFFont : public osgText::Font::FontImplementation
+{
+public:
+    TXFFont(const std::string& filename);
+
+    virtual ~TXFFont();
+
+    virtual std::string getFileName() const;
+
+    virtual void setFontResolution(unsigned int width, unsigned int height);
+
+    virtual osgText::Font::Glyph* getGlyph(unsigned int charcode);
+
+    virtual bool hasVertical() const;
+
+    virtual osg::Vec2 getKerning(unsigned int leftcharcode,unsigned int rightcharcode, osgText::KerningType kerningType);
+
+    bool loadFont(std::istream& stream);
+
+protected:
+    typedef std::map<unsigned int, osg::ref_ptr<osgText::Font::Glyph> > GlyphMap;
+
+    std::string _filename;
+    GlyphMap _chars;
+};
+
+#endif

c8463de647e881e75e9b88201b87c598701325e8
Robert OSFIELD <robert.osfield@gmail.com>
2007-01-12 15:08:45 +0000

    Added support for animation path manipulator to osgViewer path

--- applications/osgviewer/osgviewer.cpp
+++ applications/osgviewer/osgviewer.cpp
@@ -138,7 +138,7 @@ int main_osgProducer(osg::ArgumentParser& arguments)
 #include <osgGA/DriveManipulator>
 #include <osgGA/KeySwitchMatrixManipulator>
 #include <osgGA/StateSetManipulator>
-
+#include <osgGA/AnimationPathManipulator>
 
 int main_osgViewer(osg::ArgumentParser& arguments)
 {
@@ -151,6 +151,19 @@ int main_osgViewer(osg::ArgumentParser& arguments)
         keyswitchManipulator->addMatrixManipulator( '1', "Trackball", new osgGA::TrackballManipulator() );
         keyswitchManipulator->addMatrixManipulator( '2', "Flight", new osgGA::FlightManipulator() );
         keyswitchManipulator->addMatrixManipulator( '3', "Drive", new osgGA::DriveManipulator() );
+
+        std::string pathfile;
+        osg::ref_ptr<osgGA::AnimationPathManipulator> apm = 0;
+        while (arguments.read("-p",pathfile))
+        {
+            osgGA::AnimationPathManipulator* apm = new osgGA::AnimationPathManipulator(pathfile);
+            if (apm || !apm->valid()) 
+            {
+                unsigned int num = keyswitchManipulator->getNumMatrixManipulators();
+                keyswitchManipulator->addMatrixManipulator( '4', "Path", apm );
+                keyswitchManipulator->selectMatrixManipulator(num);
+            }
+        }
 
         viewer.setCameraManipulator( keyswitchManipulator.get() );
     }

edf050842a931a9527fcf4c53b0a2daebb83fde1
Robert Osfield <robert@openscenegraph.com>
2007-02-16 06:11:38 +0000

    From Eric Wing, updates to XCode projects and introduction of two SDL OSX files to the osgsimpleviewerSDL example

---
+++ examples/osgsimpleviewerSDL/SDLMainForMacOSX.h
@@ -0,0 +1,15 @@
+/*   SDLMain.m - main entry point for our Cocoa-ized SDL app
+       Initial Version: Darrell Walisser <dwaliss1@purdue.edu>
+       Non-NIB-Code & other changes: Max Horn <max@quendi.de>
+
+    Feel free to customize this file to suit your needs
+	
+	OSG users: This is the standard SDLMain (nibless) that 
+	comes with the SDL distribution. Only the file name has
+	been changed to avoid confusing non-Mac users.
+*/
+
+#import <Cocoa/Cocoa.h>
+
+@interface SDLMain : NSObject
+@end

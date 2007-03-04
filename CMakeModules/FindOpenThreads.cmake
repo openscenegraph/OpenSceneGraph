12839983262c1f01ea1910b3a096394a99d04805
Robert OSFIELD <robert.osfield@gmail.com>
2007-03-04 13:05:33 +0000

    From Eric Wing and others, first cut of CMake build support

---
+++ CMakeModules/FindOpenThreads.cmake
@@ -0,0 +1,60 @@
+# OpenThreads is a C++ based threading library. Its largest userbase 
+# seems to OpenSceneGraph so you might notice I accept OSGDIR as an
+# environment path.
+# I consider this part of the Findosg* suite used to find OpenSceneGraph 
+# components.
+# Each component is separate and you must opt in to each module.
+# 
+# Locate OpenThreads
+# This module defines
+# OPENTHREADS_LIBRARY
+# OPENTHREADS_FOUND, if false, do not try to link to OpenThreads
+# OPENTHREADS_INCLUDE_DIR, where to find the headers
+#
+# $OPENTHREADS_DIR is an environment variable that would
+# correspond to the ./configure --prefix=$OPENTHREADS_DIR
+# used in building osg.
+#
+# Created by Eric Wing.
+
+# Header files are presumed to be included like
+# #include <OpenThreads/Thread>
+
+# Try the user's environment request before anything else.
+FIND_PATH(OPENTHREADS_INCLUDE_DIR OpenThreads/Thread
+	$ENV{OPENTHREADS_DIR}/include
+	$ENV{OSG_DIR}/include
+	~/Library/Frameworks
+	/Library/Frameworks
+	/usr/local/include
+	/usr/include
+	/sw/include # Fink
+	/opt/local/include # DarwinPorts
+	/opt/csw/include # Blastwave
+	/opt/include
+	[HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session\ Manager\\Environment;OSG_ROOT]/include
+)
+
+
+FIND_LIBRARY(OPENTHREADS_LIBRARY 
+	NAMES OpenThreads OpenThreadsWin32
+	PATHS
+	$ENV{OPENTHREADS_DIR}/lib
+	$ENV{OSG_DIR}/lib
+	~/Library/Frameworks
+	/Library/Frameworks
+	/usr/local/lib
+	/usr/lib
+	/sw/lib
+	/opt/local/lib
+	/opt/csw/lib
+	/opt/lib
+	[HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session\ Manager\\Environment;OSG_ROOT]/lib
+)
+
+SET(OPENTHREADS_FOUND "NO")
+IF(OPENTHREADS_INCLUDE_DIR AND OPENTHREADS_LIBRARY)
+	SET(OPENTHREADS_FOUND "YES")
+ENDIF(OPENTHREADS_INCLUDE_DIR AND OPENTHREADS_LIBRARY)
+
+
---
+++ CMakeModules/FindQuickTime.cmake
@@ -0,0 +1,32 @@
+# Locate QuickTime
+# This module defines
+# QUICKTIME_LIBRARY
+# QUICKTIME_FOUND, if false, do not try to link to gdal 
+# QUICKTIME_INCLUDE_DIR, where to find the headers
+#
+# $QUICKTIME_DIR is an environment variable that would
+# correspond to the ./configure --prefix=$QUICKTIME_DIR
+# used in building gdal.
+#
+# Created by Eric Wing. 
+
+# QuickTime on OS X looks different than QuickTime for Windows,
+# so I am going to case the two.
+
+IF(APPLE)
+	FIND_PATH(QUICKTIME_INCLUDE_DIR QuickTime/QuickTime.h)
+	FIND_LIBRARY(QUICKTIME_LIBRARY QuickTime)
+ELSE(APPLE)
+	FIND_PATH(QUICKTIME_INCLUDE_DIR QuickTime.h
+		$ENV{QUICKTIME_DIR}/include
+		$ENV{QUICKTIME_DIR}
+	)
+	FIND_LIBRARY(QUICKTIME_LIBRARY QuickTime)
+ENDIF(APPLE)
+
+SET(QUICKTIME_FOUND "NO")
+IF(QUICKTIME_LIBRARY AND QUICKTIME_INCLUDE_DIR)
+	SET(QUICKTIME_FOUND "YES")
+ENDIF(QUICKTIME_LIBRARY AND QUICKTIME_INCLUDE_DIR)
+
+

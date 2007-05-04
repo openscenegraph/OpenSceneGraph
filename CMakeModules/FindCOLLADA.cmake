33f138671e5a755db15af985cff4c2f8ff7ecc62
Robert OSFIELD <robert.osfield@gmail.com>
2007-05-04 14:25:02 +0000

    Added first cut of Cmake COLLADA support

---
+++ CMakeModules/FindCOLLADA.cmake
@@ -0,0 +1,56 @@
+# Locate gdal
+# This module defines
+# COLLADA_LIBRARY
+# COLLADA_FOUND, if false, do not try to link to gdal 
+# COLLADA_INCLUDE_DIR, where to find the headers
+#
+# $COLLADA_DIR is an environment variable that would
+# correspond to the ./configure --prefix=$COLLADA_DIR
+#
+# Created by Robert Osfield. 
+
+FIND_PATH(COLLADA_INCLUDE_DIR dae.h
+    $ENV{COLLADA_DIR}/include
+    $ENV{COLLADA_DIR}
+    $ENV{OSGDIR}/include
+    $ENV{OSGDIR}
+    $ENV{OSG_ROOT}/include
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local/include
+    /usr/include
+    /sw/include # Fink
+    /opt/local/include # DarwinPorts
+    /opt/csw/include # Blastwave
+    /opt/include
+    [HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session\ Manager\\Environment;OSG_ROOT]/include
+    /usr/freeware/include
+)
+
+FIND_LIBRARY(COLLADA_LIBRARY 
+    NAMES collada_dom
+    PATHS
+    $ENV{COLLADA_DIR}/lib
+    $ENV{COLLADA_DIR}/lib-dbg
+    $ENV{COLLADA_DIR}
+    $ENV{OSGDIR}/lib
+    $ENV{OSGDIR}
+    $ENV{OSG_ROOT}/lib
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local/lib
+    /usr/lib
+    /sw/lib
+    /opt/local/lib
+    /opt/csw/lib
+    /opt/lib
+    [HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session\ Manager\\Environment;OSG_ROOT]/lib
+    /usr/freeware/lib64
+)
+
+SET(COLLADA_FOUND "NO")
+IF(COLLADA_LIBRARY AND COLLADA_INCLUDE_DIR)
+    SET(COLLADA_FOUND "YES")
+ENDIF(COLLADA_LIBRARY AND COLLADA_INCLUDE_DIR)
+
+

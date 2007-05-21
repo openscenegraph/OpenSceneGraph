be444c350d0d63cb30a5d673a7e8c1c4cf49aff0
Robert OSFIELD <robert.osfield@gmail.com>
2007-05-21 12:28:25 +0000

    Converted tabs to 4 spaces

--- examples/osgsimpleviewerWX/osgsimpleviewerWX.h
+++ examples/osgsimpleviewerWX/osgsimpleviewerWX.h
@@ -11,79 +11,79 @@
 class GraphicsWindowWX: public wxGLCanvas, virtual osgViewer::GraphicsWindow
 {
 public:
-	GraphicsWindowWX(wxWindow *parent, wxWindowID id = wxID_ANY,
-		const wxPoint& pos = wxDefaultPosition,
-		const wxSize& size = wxDefaultSize, long style = 0,
-		const wxString& name = wxT("TestGLCanvas"));
+    GraphicsWindowWX(wxWindow *parent, wxWindowID id = wxID_ANY,
+        const wxPoint& pos = wxDefaultPosition,
+        const wxSize& size = wxDefaultSize, long style = 0,
+        const wxString& name = wxT("TestGLCanvas"));
 
-	~GraphicsWindowWX();
+    ~GraphicsWindowWX();
 
-	void OnPaint(wxPaintEvent& event);
-	void OnSize(wxSizeEvent& event);
-	void OnEraseBackground(wxEraseEvent& event);
-	void OnKeyDown(wxKeyEvent &event);
-	void OnKeyUp(wxKeyEvent &event);
-	void OnMouse(wxMouseEvent &event);
+    void OnPaint(wxPaintEvent& event);
+    void OnSize(wxSizeEvent& event);
+    void OnEraseBackground(wxEraseEvent& event);
+    void OnKeyDown(wxKeyEvent &event);
+    void OnKeyUp(wxKeyEvent &event);
+    void OnMouse(wxMouseEvent &event);
 
-	//
-	// GraphicsWindow interface
-	//
+    //
+    // GraphicsWindow interface
+    //
 
-	void grabFocus();
+    void grabFocus();
     void grabFocusIfPointerInWindow();
     void useCursor(bool cursorOn);
 
-	bool makeCurrentImplementation();
-	void swapBuffersImplementation();
+    bool makeCurrentImplementation();
+    void swapBuffersImplementation();
 
 private:
-	wxCursor _oldCursor;
+    wxCursor _oldCursor;
 
-	DECLARE_EVENT_TABLE()
+    DECLARE_EVENT_TABLE()
 };
 
 class SimpleViewerWX : public osgViewer::SimpleViewer, public GraphicsWindowWX
 {
 public:
     SimpleViewerWX(wxWindow *parent, wxWindowID id = wxID_ANY,
-					const wxPoint& pos = wxDefaultPosition,
-					const wxSize& size = wxDefaultSize, long style = 0,
-					const wxString& name = wxT("TestGLCanvas")) :
-		GraphicsWindowWX(parent, id, pos, size, style, name)
-	{
-	}
+                    const wxPoint& pos = wxDefaultPosition,
+                    const wxSize& size = wxDefaultSize, long style = 0,
+                    const wxString& name = wxT("TestGLCanvas")) :
+        GraphicsWindowWX(parent, id, pos, size, style, name)
+    {
+    }
 
-	void render()
-	{ 
-		makeCurrentImplementation();
+    void render()
+    { 
+        makeCurrentImplementation();
 
-		// update and render the scene graph
-		frame();
+        // update and render the scene graph
+        frame();
 
-		swapBuffersImplementation();
-	}
+        swapBuffersImplementation();
+    }
 };
 
 class MainFrame : public wxFrame
 {
 public:
-	MainFrame(wxFrame *frame, const wxString& title, const wxPoint& pos, 
-		const wxSize& size, long style = wxDEFAULT_FRAME_STYLE);
+    MainFrame(wxFrame *frame, const wxString& title, const wxPoint& pos, 
+        const wxSize& size, long style = wxDEFAULT_FRAME_STYLE);
 
-	void SetSimpleViewer(SimpleViewerWX *viewer);
-	void OnIdle(wxIdleEvent& event);
+    void SetSimpleViewer(SimpleViewerWX *viewer);
+    void OnIdle(wxIdleEvent& event);
 
 private:
-	SimpleViewerWX *_viewerWindow;
+    SimpleViewerWX *_viewerWindow;
 
-	DECLARE_EVENT_TABLE()
+    DECLARE_EVENT_TABLE()
 };
 
 /* Define a new application type */
 class wxOsgApp : public wxApp
 {
 public:
-	bool OnInit();
+    bool OnInit();
 };
 
 #endif // _WXSIMPLEVIEWERWX_H_

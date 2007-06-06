75b6ae0e3c99886148a4f004dfadfbe32f785f98
Robert OSFIELD <robert.osfield@gmail.com>
2007-06-06 11:10:44 +0000

    Updated wrappers

--- src/osgWrappers/osgViewer/View.cpp
+++ src/osgWrappers/osgViewer/View.cpp
@@ -308,3 +308,44 @@ BEGIN_OBJECT_REFLECTOR(osgViewer::View)
 	                 __void__setUpViewOnSingleScreen__unsigned_int);
 END_REFLECTOR
 
+BEGIN_VALUE_REFLECTOR(osg::ref_ptr< osgGA::GUIEventHandler >)
+	I_Constructor0(____ref_ptr,
+	               "",
+	               "");
+	I_Constructor1(IN, osgGA::GUIEventHandler *, ptr,
+	               Properties::NON_EXPLICIT,
+	               ____ref_ptr__T_P1,
+	               "",
+	               "");
+	I_Constructor1(IN, const osg::ref_ptr< osgGA::GUIEventHandler > &, rp,
+	               Properties::NON_EXPLICIT,
+	               ____ref_ptr__C5_ref_ptr_R1,
+	               "",
+	               "");
+	I_Method0(osgGA::GUIEventHandler *, get,
+	          Properties::NON_VIRTUAL,
+	          __T_P1__get,
+	          "",
+	          "");
+	I_Method0(bool, valid,
+	          Properties::NON_VIRTUAL,
+	          __bool__valid,
+	          "",
+	          "");
+	I_Method0(osgGA::GUIEventHandler *, release,
+	          Properties::NON_VIRTUAL,
+	          __T_P1__release,
+	          "",
+	          "");
+	I_Method1(void, swap, IN, osg::ref_ptr< osgGA::GUIEventHandler > &, rp,
+	          Properties::NON_VIRTUAL,
+	          __void__swap__ref_ptr_R1,
+	          "",
+	          "");
+	I_SimpleProperty(osgGA::GUIEventHandler *, , 
+	                 __T_P1__get, 
+	                 0);
+END_REFLECTOR
+
+STD_LIST_REFLECTOR(std::list< osg::ref_ptr< osgGA::GUIEventHandler > >)
+

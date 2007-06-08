b3cec3d986183ef41d2a40c2554ed1d1260e7a4f
Robert Osfield <robert@openscenegraph.com>
2007-06-08 12:13:33 +0000

    Added default file load to avoid the need for command line option.

--- examples/osgcubemap/osgcubemap.cpp
+++ examples/osgcubemap/osgcubemap.cpp
@@ -78,7 +78,12 @@ int main(int argc, char *argv[])
     
     // load the nodes from the commandline arguments.
     osg::Node* rootnode = osgDB::readNodeFiles(arguments);
-    if (!rootnode) {
+
+    // if not loaded assume no arguments passed in, try use default mode instead.
+    if (!rootnode) rootnode = osgDB::readNodeFile("cessna.osg");
+
+    if (!rootnode)
+    {
         osg::notify(osg::NOTICE)<<"Please specify a model filename on the command line."<<std::endl;
         return 1;
     }

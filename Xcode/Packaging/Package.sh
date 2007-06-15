c8c1e1107ef3d7ca9d03f55393a90ed40f8f6c05
Robert Osfield <robert@openscenegraph.com>
2007-06-15 17:47:16 +0000

    From Martin Lavery, Updates to Packages

--- Xcode/Packaging/Package.sh
+++ Xcode/Packaging/Package.sh
@@ -6,11 +6,13 @@ mv Binary/Examples/osgviewer.app Binary/Applications
 mv Binary/Examples/osgarchive.app Binary/Applications
 mv Binary/Examples/osgversion.app Binary/Applications
 mv Binary/Examples/osgconv.app Binary/Applications
-mv Binary/Applications/osgviewer.app/Contents/MacOS/osgviewer Binary/Applications
-mv Binary/Applications/osgarchive.app/Contents/MacOS/osgarchive Binary/Applications
-mv Binary/Applications/osgversion.app/Contents/MacOS/osgversion Binary/Applications
-mv Binary/Applications/osgconv.app/Contents/MacOS/osgconv Binary/Applications
-rm -rf Binary/Applications/osg*.app
+cp Binary/Applications/*.app Binary/Applictions/Bundles/
+cp Binary/Applications/osgviewer.app/Contents/MacOS/osgviewer Binary/Applications
+cp Binary/Applications/osgarchive.app/Contents/MacOS/osgarchive Binary/Applications
+cp Binary/Applications/osgversion.app/Contents/MacOS/osgversion Binary/Applications
+cp Binary/Applications/osgconv.app/Contents/MacOS/osgconv Binary/Applications
+mv Binary/Applications/osg*.app Binary/Applications/Bundles/
+
 
 echo Copy Frameworks
 cp -r ../OpenSceneGraph/build/Development/*.framework Binary/Frameworks/
@@ -28,4 +30,5 @@ open OpenSceneGraph.pmproj
 open Packages/Examples.pmproj
 open Packages/PlugIns.pmproj
 open Packages/Frameworks.pmproj
-open Packages/Applications.pmproj
+open Packages/ApplicationsBundles.pmproj
+open Packages/ApplicationsCmdline.pmproj

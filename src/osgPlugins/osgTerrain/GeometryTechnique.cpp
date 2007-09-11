884b3b7aa2148535e9f5bcb48dc1bb1ed569ee17
Robert Osfield <robert@openscenegraph.com>
2007-09-11 13:32:46 +0000

    Seperated out GeometryTechnique .osg support.

---
+++ src/osgPlugins/osgTerrain/GeometryTechnique.cpp
@@ -0,0 +1,40 @@
+#include <osgTerrain/GeometryTechnique>
+
+#include <iostream>
+#include <string>
+
+#include <osg/Vec3>
+#include <osg/Vec4>
+#include <osg/io_utils>
+
+#include <osgDB/ReadFile>
+#include <osgDB/Registry>
+#include <osgDB/Input>
+#include <osgDB/Output>
+#include <osgDB/ParameterOutput>
+
+bool GeometryTechnique_readLocalData(osg::Object &obj, osgDB::Input &fr);
+bool GeometryTechnique_writeLocalData(const osg::Object &obj, osgDB::Output &fw);
+
+osgDB::RegisterDotOsgWrapperProxy GeometryTechnique_Proxy
+(
+    new osgTerrain::GeometryTechnique,
+    "GeometryTechnique",
+    "GeometryTechnique Object",
+    GeometryTechnique_readLocalData,
+    GeometryTechnique_writeLocalData
+);
+
+
+bool GeometryTechnique_readLocalData(osg::Object& obj, osgDB::Input &fr)
+{
+    osgTerrain::GeometryTechnique& gt = static_cast<osgTerrain::GeometryTechnique&>(obj);
+    bool itrAdvanced = false;
+    return itrAdvanced;
+}
+
+bool GeometryTechnique_writeLocalData(const osg::Object& obj, osgDB::Output& fw)
+{
+    const osgTerrain::GeometryTechnique& gt = static_cast<const osgTerrain::GeometryTechnique&>(obj);
+    return true;
+}

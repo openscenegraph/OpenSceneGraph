c56e0fcdd088185a84601b42ab436fb115032d05
Robert OSFIELD <robert.osfield@gmail.com>
2007-09-14 11:00:32 +0000

    Ran conversion of tabs to four spaces

--- examples/osgviewerFOX/FOX_OSG_MDIView.h
+++ examples/osgviewerFOX/FOX_OSG_MDIView.h
@@ -11,31 +11,31 @@ using namespace FX;
 
 class FOX_OSG_MDIView : public FXMDIChild{
 
-	FXDECLARE(FOX_OSG_MDIView);
+    FXDECLARE(FOX_OSG_MDIView)
 
 public:
-	FOX_OSG_MDIView(FXMDIClient *p, const FXString &name,
-		FXIcon *ic=NULL, FXPopup *pup=NULL, FXuint opts=0,
-		FXint x=0, FXint y=0, FXint w=0, FXint h=0);
+    FOX_OSG_MDIView(FXMDIClient *p, const FXString &name,
+        FXIcon *ic=NULL, FXPopup *pup=NULL, FXuint opts=0,
+        FXint x=0, FXint y=0, FXint w=0, FXint h=0);
 
-	virtual ~FOX_OSG_MDIView();
+    virtual ~FOX_OSG_MDIView();
 
-	enum{
-		ID_CHORE=FXMDIChild::ID_LAST,
-		ID_LAST
-	};
+    enum{
+        ID_CHORE=FXMDIChild::ID_LAST,
+        ID_LAST
+    };
 
-	// callback
-	long OnIdle(FXObject* , FXSelector, void*);
+    // callback
+    long OnIdle(FXObject* , FXSelector, void*);
 
-	void SetViewer(osgViewer::Viewer *viewer);
+    void SetViewer(osgViewer::Viewer *viewer);
 
 protected:
-	FOX_OSG_MDIView(){};
+    FOX_OSG_MDIView(){};
 
 private:
-	osg::ref_ptr<osgViewer::Viewer>		m_osgViewer;
-	GraphicsWindowFOX*					m_gwFox;
+    osg::ref_ptr<osgViewer::Viewer>        m_osgViewer;
+    GraphicsWindowFOX*                    m_gwFox;
 };
 
 #endif // _FOXOSGMDIVIEW_H_

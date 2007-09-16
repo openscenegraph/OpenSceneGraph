c583dbe3c5636c7a17c958da938373b81a077f2d
Robert Osfield <robert@openscenegraph.com>
2007-09-16 17:43:00 +0000

    Fixed warning

--- src/osgPlugins/osgTerrain/GeometryTechnique.cpp
+++ src/osgPlugins/osgTerrain/GeometryTechnique.cpp
@@ -28,13 +28,13 @@ osgDB::RegisterDotOsgWrapperProxy GeometryTechnique_Proxy
 
 bool GeometryTechnique_readLocalData(osg::Object& obj, osgDB::Input &fr)
 {
-    osgTerrain::GeometryTechnique& gt = static_cast<osgTerrain::GeometryTechnique&>(obj);
+    //osgTerrain::GeometryTechnique& gt = static_cast<osgTerrain::GeometryTechnique&>(obj);
     bool itrAdvanced = false;
     return itrAdvanced;
 }
 
 bool GeometryTechnique_writeLocalData(const osg::Object& obj, osgDB::Output& fw)
 {
-    const osgTerrain::GeometryTechnique& gt = static_cast<const osgTerrain::GeometryTechnique&>(obj);
+    //const osgTerrain::GeometryTechnique& gt = static_cast<const osgTerrain::GeometryTechnique&>(obj);
     return true;
 }

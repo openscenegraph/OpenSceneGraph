a2172dda5237b4cfb2d72b2c9cb8e1b091dbf751
Robert OSFIELD <robert.osfield@gmail.com>
2007-12-24 15:32:15 +0000

    Fom Brede Johansen, "I have added the OSGPL license to the OpenFlight source files"

--- src/osgPlugins/OpenFlight/GeometryRecords.cpp
+++ src/osgPlugins/OpenFlight/GeometryRecords.cpp
@@ -1,7 +1,20 @@
+/* -*-c++-*- OpenSceneGraph - Copyright (C) 1998-2006 Robert Osfield 
+ *
+ * This library is open source and may be redistributed and/or modified under  
+ * the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or 
+ * (at your option) any later version.  The full license is in LICENSE file
+ * included with this distribution, and on the openscenegraph.org website.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * OpenSceneGraph Public License for more details.
+*/
+
 //
 // OpenFlight® loader for OpenSceneGraph
 //
-//  Copyright (C) 2005-2006  Brede Johansen
+//  Copyright (C) 2005-2007  Brede Johansen
 //
 
 #include <assert.h>

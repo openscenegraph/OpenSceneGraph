c5388d1eef73accb8601d9114cce812711a18b6f
Robert OSFIELD <robert.osfield@gmail.com>
2008-01-28 20:27:33 +0000

    From Rainer Oder, removed duplicate conditional

--- applications/osgarchive/osgarchive.cpp
+++ applications/osgarchive/osgarchive.cpp
@@ -155,7 +155,7 @@ int main( int argc, char **argv )
                 std::cout<<"readObejct time = "<<osg::Timer::instance()->delta_m(start,osg::Timer::instance()->tick())<<std::endl;
                 if (obj.valid())
                 {
-                    if (obj.valid()) osgDB::writeObjectFile(*obj, *itr);
+                    osgDB::writeObjectFile(*obj, *itr);
                 }
             }
         }

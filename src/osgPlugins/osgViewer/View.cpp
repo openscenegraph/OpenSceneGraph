7e6c17d7f8fb6aabeec0120ba026aa00b4625672
Robert OSFIELD <robert.osfield@gmail.com>
2008-01-28 15:41:42 +0000

    Added projectorMatrix parameter support to *SphericalDisplay setup functions and .view, this allows one to flip, rotate, or turn up side the position of the projector.  Note, projector at base of display is the default.

--- src/osgPlugins/osgViewer/View.cpp
+++ src/osgPlugins/osgViewer/View.cpp
@@ -21,6 +21,55 @@ osgDB::RegisterDotOsgWrapperProxy View_Proxy
     View_writeLocalData
 );
 
+
+static bool readMatrix(osg::Matrix& matrix, osgDB::Input& fr, const char* keyword)
+{
+    bool iteratorAdvanced = false;
+    
+    if (fr[0].matchWord(keyword) && fr[1].isOpenBracket())
+    {
+        int entry = fr[0].getNoNestedBrackets();
+
+        fr += 2;
+
+        int row=0;
+        int col=0;
+        double v;
+        while (!fr.eof() && fr[0].getNoNestedBrackets()>entry)
+        {
+            if (fr[0].getFloat(v))
+            {
+                matrix(row,col)=v;
+                ++col;
+                if (col>=4)
+                {
+                    col = 0;
+                    ++row;
+                }
+                ++fr;
+            }
+            else fr.advanceOverCurrentFieldOrBlock();
+        }
+        iteratorAdvanced = true;
+    }        
+        
+    return iteratorAdvanced;
+}
+
+
+static bool writeMatrix(const osg::Matrix& matrix, osgDB::Output& fw, const char* keyword)
+{
+    fw.indent() << keyword <<" {" << std::endl;
+    fw.moveIn();
+    fw.indent() << matrix(0,0) << " " << matrix(0,1) << " " << matrix(0,2) << " " << matrix(0,3) << std::endl;
+    fw.indent() << matrix(1,0) << " " << matrix(1,1) << " " << matrix(1,2) << " " << matrix(1,3) << std::endl;
+    fw.indent() << matrix(2,0) << " " << matrix(2,1) << " " << matrix(2,2) << " " << matrix(2,3) << std::endl;
+    fw.indent() << matrix(3,0) << " " << matrix(3,1) << " " << matrix(3,2) << " " << matrix(3,3) << std::endl;
+    fw.moveOut();
+    fw.indent() << "}"<< std::endl;
+    return true;
+}
+
 osg::Image* readIntensityImage(osgDB::Input& fr, bool& itrAdvanced)
 {
     if (fr.matchSequence("intensityMap {"))
@@ -107,6 +156,7 @@ bool View_readLocalData(osg::Object &obj, osgDB::Input &fr)
         double collar = 0.45;
         unsigned int screenNum = 0;
         unsigned int intensityFormat = 8;
+        osg::Matrix matrix; 
         std::string filename;
         osg::ref_ptr<osg::Image> intensityMap;
         int entry = fr[0].getNoNestedBrackets();
@@ -122,6 +172,7 @@ bool View_readLocalData(osg::Object &obj, osgDB::Input &fr)
             if (fr.read("intensityFile",filename)) local_itrAdvanced = true;
             if (fr.matchSequence("intensityMap {")) intensityMap = readIntensityImage(fr,local_itrAdvanced);
             if (fr.read("intensityFormat",intensityFormat)) local_itrAdvanced = true;
+            if (readMatrix(matrix,fr,"projectorMatrix")) local_itrAdvanced = true;
             
             if (!local_itrAdvanced) ++fr;
         }
@@ -144,8 +195,8 @@ bool View_readLocalData(osg::Object &obj, osgDB::Input &fr)
         }
 
 
-        if (matchedFirst) view.setUpViewFor3DSphericalDisplay(radius, collar, screenNum, intensityMap.get());
-        else view.setUpViewForPanoramicSphericalDisplay(radius, collar, screenNum, intensityMap.get());
+        if (matchedFirst) view.setUpViewFor3DSphericalDisplay(radius, collar, screenNum, intensityMap.get(), matrix);
+        else view.setUpViewForPanoramicSphericalDisplay(radius, collar, screenNum, intensityMap.get(), matrix);
     }
 
     int x = 0;

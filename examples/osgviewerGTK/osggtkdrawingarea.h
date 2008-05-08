6a3e1f0e577bdf30684d27d19115bbc38ca33a3a
Robert OSFIELD <robert.osfield@gmail.com>
2008-05-08 16:39:10 +0000

    From Jeremy Moles, osgviewerGTK example

---
+++ examples/osgviewerGTK/osggtkdrawingarea.h
@@ -0,0 +1,207 @@
+// by: Jeremy Moles <jeremy@emperorlinux.com> 2007
+
+#include <gtk/gtk.h>
+#include <gtk/gtkgl.h>
+#include <osgViewer/Viewer>
+#include <osgGA/TrackballManipulator>
+
+// This is an implementation of SimpleViewer that is designed to be subclassed
+// and used as a GtkDrawingArea in a GTK application. Because of the implemention
+// of GTK, I was unable to derive from GtkWidget and instead had to "wrap" it.
+// Conceptually, however, you can think of an OSGGTKDrawingArea as both an OSG
+// Viewer AND GtkDrawingArea.
+//
+// While it is possible to use this class directly, it won't end up doing anything
+// interesting without calls to queueDraw, which ideally are done in the user's
+// subclass implementation (see: osgviewerGTK).
+class OSGGTKDrawingArea : public osgViewer::Viewer {
+	GtkWidget*     _widget;
+	GdkGLConfig*   _glconfig;
+	GdkGLContext*  _context;
+	GdkGLDrawable* _drawable;
+
+	unsigned int _state;
+
+	osgGA::EventQueue& _queue;
+
+	static OSGGTKDrawingArea* _self(gpointer self) {
+		return static_cast<OSGGTKDrawingArea*>(self);
+	}
+
+	// A simple helper function to connect us to the various GTK signals.
+	void _connect(const char* name, GCallback callback) {
+		g_signal_connect(G_OBJECT(_widget), name, callback, this);
+	}
+
+	void _realize             (GtkWidget*);
+	void _unrealize           (GtkWidget*);
+	bool _expose_event        (GtkWidget*, GdkEventExpose*);
+	bool _configure_event     (GtkWidget*, GdkEventConfigure*);
+	bool _motion_notify_event (GtkWidget*, GdkEventMotion*);
+	bool _button_press_event  (GtkWidget*, GdkEventButton*);
+	bool _key_press_event     (GtkWidget*, GdkEventKey*);
+
+	// The following functions are static "wrappers" so that we can invoke the
+	// bound methods of a class instance by passing the "this" pointer as the
+	// self argument and invoking it explicitly.
+	static void _srealize(GtkWidget* widget, gpointer self) {
+		_self(self)->_realize(widget);
+	}
+
+	static void _sunrealize(GtkWidget* widget, gpointer self) {
+		_self(self)->_unrealize(widget);
+	}
+
+	static bool _sexpose_event(GtkWidget* widget, GdkEventExpose* expose, gpointer self) {
+		return _self(self)->_expose_event(widget, expose);
+	}
+
+	static bool _sconfigure_event(
+		GtkWidget*         widget,
+		GdkEventConfigure* event,
+		gpointer           self
+	) {
+		return _self(self)->_configure_event(widget, event);
+	}
+
+	static bool _smotion_notify_event(
+		GtkWidget*      widget,
+		GdkEventMotion* event,
+		gpointer        self
+	) {
+		return _self(self)->_motion_notify_event(widget, event);
+	}
+
+	static bool _sbutton_press_event(
+		GtkWidget*      widget,
+		GdkEventButton* event,
+		gpointer        self
+	) {
+		return _self(self)->_button_press_event(widget, event);
+	}
+
+	static bool _skey_press_event(
+		GtkWidget*   widget,
+		GdkEventKey* event,
+		gpointer     self
+	) {
+		return _self(self)->_key_press_event(widget, event);
+	}
+
+protected:
+	// You can override these in your subclass if you'd like. :)
+	// Right now they're fairly uninformative, but they could be easily extended.
+	// Note that the "state" information isn't passed around to each function
+	// but is instead stored and abstracted internally. See below.
+
+	virtual void gtkRealize   () {};
+	virtual void gtkUnrealize () {};
+	virtual bool gtkExpose    () {
+		return true;
+	};
+
+	// The new width and height.
+	virtual bool gtkConfigure(int, int) {
+		return true;
+	};
+
+	// The "normalized" coordinates of the mouse.
+	virtual bool gtkMotionNotify(double, double) {
+		return true;
+	};
+
+	// The "normalized" coordinates of the mouse and the mouse button code on down.
+	virtual bool gtkButtonPress(double, double, unsigned int) {
+		return true;
+	};
+
+	// The "normalized" coordinates of the mouse and mouse button code on release.
+	virtual bool gtkButtonRelease(double, double, unsigned int) {
+		return true;
+	}
+
+	// The X key value on down.
+	virtual bool gtkKeyPress(unsigned int) {
+		return true;
+	};
+
+	// The X key value on release.
+	virtual bool gtkKeyRelease(unsigned int) {
+		return true;
+	};
+
+	// These functions wrap state tests of the most recent state in the
+	// GtkDrawingArea.
+
+	inline bool stateShift() {
+		return _state & GDK_SHIFT_MASK;
+	}
+
+	inline bool stateLock() {
+		return _state & GDK_LOCK_MASK;
+	}
+
+	inline bool stateControl() {
+		return _state & GDK_CONTROL_MASK;
+	}
+
+	inline bool stateMod() {
+		return _state & (
+			GDK_MOD1_MASK |
+			GDK_MOD2_MASK |
+			GDK_MOD3_MASK |
+			GDK_MOD4_MASK |
+			GDK_MOD5_MASK
+		);
+	}
+
+	inline bool stateButton() {
+		return _state & (
+			GDK_BUTTON1_MASK |
+			GDK_BUTTON2_MASK |
+			GDK_BUTTON3_MASK |
+			GDK_BUTTON4_MASK |
+			GDK_BUTTON5_MASK
+		);
+	}
+
+public:
+	OSGGTKDrawingArea  ();
+	~OSGGTKDrawingArea ();
+
+	bool createWidget(int, int);
+
+	GtkWidget* getWidget() {
+		return _widget;
+	}
+
+	bool gtkGLBegin() {
+		if(_drawable and _context) return gdk_gl_drawable_gl_begin(_drawable, _context);
+
+		else return false;
+	}
+
+	void gtkGLEnd() {
+		if(_drawable) gdk_gl_drawable_gl_end(_drawable);
+	}
+
+	// Because of GTK's internal double buffering, I'm not sure if we're really
+	// taking advantage of OpenGL's internal swapping.
+	bool gtkGLSwap() {
+		if(_drawable and gdk_gl_drawable_is_double_buffered(_drawable)) {
+			gdk_gl_drawable_swap_buffers(_drawable);
+
+			return true;
+		}
+
+		else {
+			glFlush();
+
+			return false;
+		}
+	}
+
+	void queueDraw() {
+		gtk_widget_queue_draw(_widget);
+	}
+};
---
+++ examples/osgviewerGTK/osgviewerGTK.cpp
@@ -0,0 +1,234 @@
+// by: Jeremy Moles <jeremy@emperorlinux.com> 2007
+
+#include <iostream>
+#include <string>
+#include <osg/Stats>
+#include <osgDB/ReadFile>
+
+#include "osggtkdrawingarea.h"
+
+const char* HELP_TEXT =
+	"Use CTRL or SHIFT plus right-click to pull up a fake menu.\n"
+	"Use the standard TrackballManipulator keys to rotate the loaded\n"
+	"model (with caveats; the model won't keep rotating).\n"
+	"\n"
+	"<b>OpenSceneGraph Project, 2008</b>"
+;
+
+// If you want to see how to connect class method to callbacks, take a look at the
+// implementation of OSGGTKDrawingArea. It's dirty, but it's the only way I could
+// come up with.
+bool activate(GtkWidget* widget, gpointer) {
+	GtkWidget* label = gtk_bin_get_child(GTK_BIN(widget));
+
+	std::cout << "MENU: " << gtk_label_get_label(GTK_LABEL(label)) << std::endl;
+
+	return true;
+}
+
+// Our derived OSGGTKDrawingArea "widget." Redraws occur while the mouse buttons
+// are held down and mouse motion is detected.
+//
+// This is the easiest way to demonstrate the use of OSGGTKDrawingArea. We override
+// a few of the event methods to setup our menu and to issue redraws. Note that an
+// unmodified OSGGTKDrawingArea never calls queueDraw, so OSG is never asked to render
+// itself.
+class ExampleOSGGTKDrawingArea : public OSGGTKDrawingArea {
+	GtkWidget* _menu;
+	
+	unsigned int _tid;
+
+	// A helper function to easily setup our menu entries.
+	void _menuAdd(const std::string& title) {
+		GtkWidget* item = gtk_menu_item_new_with_label(title.c_str());
+		
+		gtk_menu_shell_append(GTK_MENU_SHELL(_menu), item);
+
+		g_signal_connect(G_OBJECT(item), "activate", G_CALLBACK(activate), 0);
+	}
+
+	bool _clicked(GtkWidget* widget) {
+		const char* text = gtk_label_get_label(
+			GTK_LABEL(gtk_bin_get_child(GTK_BIN(widget)))
+		);
+
+		if(not std::strncmp(text, "Close", 5)) gtk_main_quit();
+	
+		else if(not std::strncmp(text, "Open File", 9)) {
+			GtkWidget* of = gtk_file_chooser_dialog_new(
+				"Please select an OSG file...",
+				GTK_WINDOW(gtk_widget_get_toplevel(getWidget())),
+				GTK_FILE_CHOOSER_ACTION_OPEN,
+				GTK_STOCK_CANCEL,
+				GTK_RESPONSE_CANCEL,
+				GTK_STOCK_OPEN,
+				GTK_RESPONSE_ACCEPT,
+				NULL
+			);
+
+			if(gtk_dialog_run(GTK_DIALOG(of)) == GTK_RESPONSE_ACCEPT) {
+				char* file = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(of));
+
+				osg::ref_ptr<osg::Node> model = osgDB::readNodeFile(file);
+
+				if(model.valid()) {
+					setSceneData(model.get());
+				
+					queueDraw();
+				}
+
+				g_free(file);
+			}
+			
+			gtk_widget_destroy(of);
+		}
+
+		// Assume we're wanting FPS toggling.
+		else {
+			if(not _tid) {
+				_tid = g_timeout_add(
+					15,
+					(GSourceFunc)(ExampleOSGGTKDrawingArea::timeout),
+					this
+				);
+
+				gtk_button_set_label(GTK_BUTTON(widget), "Toggle 60 FPS (off)");
+			}
+
+			else {
+				g_source_remove(_tid);
+				gtk_button_set_label(GTK_BUTTON(widget), "Toggle 60 FPS (on)");
+
+				_tid = 0;
+			}
+		}
+
+		return true;
+	}
+
+protected:
+	// Check right-click release to see if we need to popup our menu.
+	bool gtkButtonRelease(double, double, unsigned int button) {
+		if(button == 3 and (stateControl() or stateShift())) gtk_menu_popup(
+			GTK_MENU(_menu),
+			0,
+			0,
+			0,
+			0,
+			button,
+			0
+		);
+
+		return true;
+	}
+
+	// Our "main" drawing pump. Since our app is just a model viewer, we use
+	// click+motion as our criteria for issuing OpenGL refreshes.
+	bool gtkMotionNotify(double, double) {
+		if(stateButton()) queueDraw();
+
+		return true;
+	}
+
+public:
+	ExampleOSGGTKDrawingArea():
+	OSGGTKDrawingArea (),
+	_menu             (gtk_menu_new()),
+	_tid              (0) {
+		_menuAdd("Option");
+		_menuAdd("Another Option");
+		_menuAdd("Still More Options");
+
+		gtk_widget_show_all(_menu);
+
+		getCamera()->setStats(new osg::Stats("omg"));
+	}
+
+	~ExampleOSGGTKDrawingArea() {}
+
+	// Public so that we can use this as a callback in main().
+	static bool clicked(GtkWidget* widget, gpointer self) {
+		return static_cast<ExampleOSGGTKDrawingArea*>(self)->_clicked(widget);
+	}
+
+	//static gboolean timeout(GtkWidget* widget) {
+	static bool timeout(void* self) {
+		static_cast<ExampleOSGGTKDrawingArea*>(self)->queueDraw();
+
+		return true;
+	}
+};
+
+// Our main() function! FINALLY! Most of this code is GTK stuff, so it's mostly boilerplate.
+// If we wanted to get real jiggy with it we could use Glade and cut down about 20 lines of
+// code or so.
+int main(int argc, char** argv) {
+	gtk_init(&argc, &argv);
+	gtk_gl_init(&argc, &argv);
+
+	ExampleOSGGTKDrawingArea da;
+
+	if(da.createWidget(640, 480)) {
+		if(argc >= 2) {
+			osg::ref_ptr<osg::Node> model = osgDB::readNodeFile(argv[1]);
+
+			if(model.valid()) da.setSceneData(model.get());
+		}
+
+		GtkWidget* window    = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+		GtkWidget* vbox1     = gtk_vbox_new(false, 3);
+		GtkWidget* vbox2     = gtk_vbox_new(false, 3);
+		GtkWidget* hbox      = gtk_hbox_new(false, 3);
+		GtkWidget* label     = gtk_label_new("");
+		GtkWidget* buttons[] = {
+			gtk_button_new_with_label("Open File"),
+			gtk_button_new_with_label("Toggle 60 FPS (on)"),
+			gtk_button_new_with_label("Close")
+		};
+
+		gtk_label_set_use_markup(GTK_LABEL(label), true);
+		gtk_label_set_label(GTK_LABEL(label), HELP_TEXT);
+
+		for(unsigned int i = 0; i < sizeof(buttons) / sizeof(GtkWidget*); i++) {
+			gtk_box_pack_start(
+				GTK_BOX(vbox2),
+				buttons[i],
+				false,
+				false,
+				0
+			);
+
+			g_signal_connect(
+				G_OBJECT(buttons[i]),
+				"clicked",
+				G_CALLBACK(ExampleOSGGTKDrawingArea::clicked),
+				&da
+			);
+		}
+
+		gtk_window_set_title(GTK_WINDOW(window), "osgviewerGTK");
+
+		gtk_box_pack_start(GTK_BOX(hbox), vbox2, true, true, 2);
+		gtk_box_pack_start(GTK_BOX(hbox), label, true, true, 2);
+
+		gtk_box_pack_start(GTK_BOX(vbox1), da.getWidget(), true, true, 2);
+		gtk_box_pack_start(GTK_BOX(vbox1), hbox, false, false, 2);
+
+		gtk_container_set_reallocate_redraws(GTK_CONTAINER(window), true);
+		gtk_container_add(GTK_CONTAINER(window), vbox1);
+
+		g_signal_connect(
+			G_OBJECT(window),
+			"delete_event",
+			G_CALLBACK(gtk_main_quit),
+			0
+		);
+
+		gtk_widget_show_all(window);
+		gtk_main();
+	}
+
+	else return 1;
+
+	return 0;
+}

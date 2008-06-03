8d0c7890cede9cd350c714a3da20d76d25fcac5d
Robert Osfield <robert@openscenegraph.com>
2008-06-03 16:13:49 +0000

    First cut of osgkdtree example, this will be used as a base of the development of native kdtree support to help speed up intersection testing.

---
+++ examples/osgkdtree/osgkdtree.cpp
@@ -0,0 +1,53 @@
+/* OpenSceneGraph example, osgintersection.
+*
+*  Permission is hereby granted, free of charge, to any person obtaining a copy
+*  of this software and associated documentation files (the "Software"), to deal
+*  in the Software without restriction, including without limitation the rights
+*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the Software is
+*  furnished to do so, subject to the following conditions:
+*
+*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+*  THE SOFTWARE.
+*/
+
+#include <osg/ArgumentParser>
+#include <osg/ApplicationUsage>
+#include <osg/Timer>
+#include <osg/CoordinateSystemNode>
+#include <osg/Notify>
+#include <osg/io_utils>
+
+#include <osgDB/ReadFile>
+
+#include <osgUtil/IntersectionVisitor>
+#include <osgUtil/LineSegmentIntersector>
+
+#include <osgSim/LineOfSight>
+#include <osgSim/HeightAboveTerrain>
+#include <osgSim/ElevationSlice>
+
+#include <iostream>
+
+
+int main(int argc, char **argv)
+{
+    // use an ArgumentParser object to manage the program arguments.
+    osg::ArgumentParser arguments(&argc,argv);
+    
+    osg::ref_ptr<osg::Node> scene = osgDB::readNodeFiles(arguments);
+    
+    if (!scene) 
+    {
+        std::cout<<"No model loaded, please specify a valid model on the command line."<<std::endl;
+        return 0;
+    }
+    
+    
+    return 0;
+}

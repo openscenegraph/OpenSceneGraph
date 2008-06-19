2ba5f002d29fcb739b62964f77aa1cbd4e7f6cf7
Robert Osfield <robert@openscenegraph.com>
2008-06-19 14:57:36 +0000

    from Andrew Bettison, LineStipple support for .ive

---
+++ src/osgPlugins/ive/LineStipple.h
@@ -0,0 +1,15 @@
+#ifndef IVE_LINESTIPPLE
+#define IVE_LINESTIPPLE 1
+
+#include <osg/LineStipple>
+#include "ReadWrite.h"
+
+namespace ive{
+class LineStipple : public osg::LineStipple, public ReadWrite {
+public:
+    void write(DataOutputStream* out);
+    void read(DataInputStream* in);
+};
+}
+
+#endif

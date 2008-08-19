c044ce565270b342dc1f91e7fd1b58069cac01e3
Robert OSFIELD <robert.osfield@gmail.com>
2008-08-19 17:41:55 +0000

    Added checked against an empty _kdNodes list before commensing with intersection

--- src/osg/KdTree.cpp
+++ src/osg/KdTree.cpp
@@ -755,6 +755,8 @@ bool KdTree::build(BuildOptions& options, osg::Geometry* geometry)
 
 bool KdTree::intersect(const osg::Vec3& start, const osg::Vec3& end, LineSegmentIntersections& intersections) const
 {
+    if (_kdNodes.empty()) return false;
+
     int numIntersectionsBefore = intersections.size();
 
     IntersectKdTree intersector(*_vertices,

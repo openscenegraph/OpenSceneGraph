9dd8951840d0e4d6515b21122e81a182ffab124a
Robert OSFIELD <robert.osfield@gmail.com>
2008-09-01 12:39:19 +0000

    From John Vidar Larring, initial cut of .ive support for ShapeAttributeList user data

---
+++ src/osgPlugins/ive/ShapeAttributeList.h
@@ -0,0 +1,18 @@
+#ifndef IVE_SHAPEATTRIBUTELIST
+#define IVE_SHAPEATTRIBUTELIST 1
+
+#include <osgSim/ShapeAttribute>
+#include "ReadWrite.h"
+
+namespace ive{
+class ShapeAttributeList : public osgSim::ShapeAttributeList, public ReadWrite {
+public:
+    void write(DataOutputStream* out);
+    void read(DataInputStream* in);
+    
+    void write(DataOutputStream* out, const osgSim::ShapeAttribute& sa);
+    void read(DataInputStream* in, osgSim::ShapeAttribute& sa);
+};
+}
+
+#endif

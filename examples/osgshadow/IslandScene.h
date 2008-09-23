b1c93f05f684755549af8464cf03f8d23dbbf719
Robert Osfield <robert@openscenegraph.com>
2008-09-23 09:59:23 +0000

    From Wojciech Lweandowski, added support for noew LispSM shadow technique (use --lispsm on CmdLine) and added IslandScene database (use -4 on CmdLine).

---
+++ examples/osgshadow/IslandScene.h
@@ -0,0 +1,9 @@
+#ifndef ISLANDSCENE_H
+#define ISLANDSCENE_H
+
+// declare namespace and function to create the island scene 
+namespace ModelFour { 
+    osg::Node* createModel(osg::ArgumentParser& /*arguments*/); 
+}
+
+#endif

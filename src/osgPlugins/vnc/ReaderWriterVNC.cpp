34500985e2f87b4b51d8fa8fe16f9b33bf06c98f
Robert OSFIELD <robert.osfield@gmail.com>
2008-11-03 16:55:06 +0000

    Added GPL copyright notice

--- src/osgPlugins/vnc/ReaderWriterVNC.cpp
+++ src/osgPlugins/vnc/ReaderWriterVNC.cpp
@@ -1,3 +1,15 @@
+/* -*-c++-*- OpenSceneGraph - Copyright (C) 1999-2008 Robert Osfield 
+ *
+ * This software is open source and may be redistributed and/or modified under  
+ * the terms of the GNU General Public License (GPL) version 2.0.
+ * The full license is in LICENSE.txt file included with this distribution,.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * include LICENSE.txt for more details.
+*/
+
 #include <osgDB/ReaderWriter>
 #include <osgDB/FileNameUtils>
 #include <osgDB/Registry>

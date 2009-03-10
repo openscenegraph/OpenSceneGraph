ea5ae93932b4a79c10956a723d25860c676e2c5d
Robert OSFIELD <robert.osfield@gmail.com>
2009-03-10 13:46:55 +0000

    Added copyright + fixed typo

--- src/osgPlugins/ffmpeg/ReaderWriterFFmpeg.cpp
+++ src/osgPlugins/ffmpeg/ReaderWriterFFmpeg.cpp
@@ -1,3 +1,15 @@
+/* -*-c++-*- OpenSceneGraph - Copyright (C) 1998-2006 Robert Osfield 
+ *
+ * This library is open source and may be redistributed and/or modified under  
+ * the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or 
+ * (at your option) any later version.  The full license is in LICENSE file
+ * included with this distribution, and on the openscenegraph.org website.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * OpenSceneGraph Public License for more details.
+*/
 
 #include <osgDB/Registry>
 #include <osgDB/FileNameUtils>
@@ -16,6 +28,8 @@ public:
 
     ReaderWriterFFmpeg()
     {
+        supportsProtocol("http","Read video/audio from http using ffmpeg.");
+
         supportsExtension("avi", "");
         supportsExtension("flv", "");
         supportsExtension("mov", "");

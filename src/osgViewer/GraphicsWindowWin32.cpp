c707945f0cdeaa0ba729fb3f277524446fa37e5d
Robert OSFIELD <robert.osfield@gmail.com>
2009-05-07 13:18:35 +0000

    From Chris Denham, fix for swapBuffers warning when window is minimized under Windows.
      svn merge -r 10146:10147 http://www.openscenegraph.org/svn/osg/OpenSceneGraph/trunk/src/osgViewer/

--- src/osgViewer/GraphicsWindowWin32.cpp
+++ src/osgViewer/GraphicsWindowWin32.cpp
@@ -1782,7 +1782,7 @@ void GraphicsWindowWin32::closeImplementation()
 void GraphicsWindowWin32::swapBuffersImplementation()
 {
     if (!_realized) return;
-    if (!::SwapBuffers(_hdc))
+    if (!::SwapBuffers(_hdc) && ::GetLastError() != 0)
     {
         reportErrorForScreen("GraphicsWindowWin32::swapBuffersImplementation() - Unable to swap display buffers", _traits->screenNum, ::GetLastError());
     }

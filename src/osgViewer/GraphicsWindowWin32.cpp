54d2034da3743f40d620b214b06137f87d6caad1
Robert OSFIELD <robert.osfield@gmail.com>
2009-05-08 07:49:54 +0000

    From Neil Groves, fixed unitialized variable

--- src/osgViewer/GraphicsWindowWin32.cpp
+++ src/osgViewer/GraphicsWindowWin32.cpp
@@ -1029,6 +1029,7 @@ GraphicsWindowWin32::GraphicsWindowWin32( osg::GraphicsContext::Traits* traits )
   _closeWindow(false),
   _destroyWindow(false),
   _destroying(false),
+  _mouseCursor(0),
   _appMouseCursor(LeftArrowCursor),
   _applyWorkaroundForMultimonitorMultithreadNVidiaWin32Issues( false )
 {

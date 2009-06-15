c4c5ca756601ae68291a8af788e74022918807f1
Cedric Pinson <cedric.pinson@plopbyte.net>
2009-06-15 14:48:37 +0000

    From Cedric Pinson, split timeline classes in differents files, cleanup and add a statshandler to visualize current action in timeline

---
+++ src/osgAnimation/StatsVisitor.cpp
@@ -0,0 +1,86 @@
+/*  -*-c++-*- 
+ *  Copyright (C) 2009 Cedric Pinson <cedric.pinson@plopbyte.net>
+ *
+ * This library is open source and may be redistributed and/or modified under  
+ * the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or 
+ * (at your option) any later version.  The full license is in LICENSE file
+ * included with this distribution, and on the openscenegraph.org website.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * OpenSceneGraph Public License for more details.
+*/
+
+#include <osgAnimation/StatsVisitor>
+#include <osgAnimation/Timeline>
+
+osgAnimation::StatsActionVisitor::StatsActionVisitor() {}
+void osgAnimation::StatsActionVisitor::reset() { _channels.clear(); }
+
+osgAnimation::StatsActionVisitor::StatsActionVisitor(osg::Stats* stats,unsigned int frame)
+{
+    _frame = frame;
+    _stats = stats;
+}
+
+void osgAnimation::StatsActionVisitor::apply(Timeline& tm)
+{
+    _stats->setAttribute(_frame,"Timeline", tm.getCurrentTime());
+    tm.traverse(*this);
+}
+
+void osgAnimation::StatsActionVisitor::apply(Action& action)
+{
+    _channels.push_back(action.getName());
+    _stats->setAttribute(_frame,action.getName(),0);
+    if (isActive())
+    {
+        _channels.push_back(action.getName());
+        _stats->setAttribute(_frame,action.getName(),1);
+    }
+}
+
+void osgAnimation::StatsActionVisitor::apply(BlendIn& action)
+{
+    _channels.push_back(action.getName());
+    _stats->setAttribute(_frame,action.getName(),0);
+    if (isActive()) 
+    {
+        _channels.push_back(action.getName());
+        _stats->setAttribute(_frame,action.getName(), action.getWeight());
+    }
+}
+
+void osgAnimation::StatsActionVisitor::apply(BlendOut& action)
+{
+    _channels.push_back(action.getName());
+    _stats->setAttribute(_frame,action.getName(),0);
+    if (isActive()) 
+    {
+        _channels.push_back(action.getName());
+        _stats->setAttribute(_frame,action.getName(), action.getWeight());
+    }
+}
+
+void osgAnimation::StatsActionVisitor::apply(ActionAnimation& action)
+{
+    _channels.push_back(action.getName());
+    _stats->setAttribute(_frame,action.getName(),0);
+    if (isActive()) 
+    {
+        _channels.push_back(action.getName());
+        _stats->setAttribute(_frame,action.getName(), action.getAnimation()->getWeight());
+    }
+}
+
+void osgAnimation::StatsActionVisitor::apply(StripAnimation& action)
+{
+    _channels.push_back(action.getName());
+    _stats->setAttribute(_frame,action.getName(),0);
+    if (isActive())
+    {
+        _channels.push_back(action.getName());
+        _stats->setAttribute(_frame,action.getName(), action.getActionAnimation()->getAnimation()->getWeight());
+    }
+}
--- src/osgAnimation/Timeline.cpp
+++ src/osgAnimation/Timeline.cpp
@@ -13,6 +13,7 @@
 */
 
 #include <osgAnimation/Timeline>
+#include <osgAnimation/StatsVisitor>
 #include <limits.h>
 
 using namespace osgAnimation;
@@ -29,11 +30,13 @@ osgAnimation::Timeline::Timeline()
     _previousFrameEvaluated = 0;
     _evaluating = 0;
     _numberFrame = UINT_MAX; // something like infinity
+    _collectStats = false;
+    _stats = new osg::Stats("Timeline");
     setName("Timeline");
 }
 
-osgAnimation::Timeline::Timeline(const Timeline& nc,const osg::CopyOp& op) : osg::Object(nc, op),
-    _actions(nc._actions)
+osgAnimation::Timeline::Timeline(const Timeline& nc,const osg::CopyOp& op) : Action(nc, op),
+                                                                             _actions(nc._actions)
 {
     _lastUpdate = 0;
     _currentFrame = 0;
@@ -44,5 +47,200 @@ osgAnimation::Timeline::Timeline(const Timeline& nc,const osg::CopyOp& op) : osg
     _previousFrameEvaluated = 0;
     _evaluating = 0;
     _numberFrame = UINT_MAX; // something like infinity
+    _collectStats = false;
+    _stats = new osg::Stats("Timeline");
     setName("Timeline");
+}
+
+void osgAnimation::Timeline::traverse(ActionVisitor& visitor)
+{
+    visitor.pushTimelineOnStack(this);
+    // update from high priority to low priority
+    for( ActionLayers::reverse_iterator iterAnim = _actions.rbegin(); iterAnim != _actions.rend(); ++iterAnim )
+    {
+        ActionList& list = iterAnim->second;
+        for (unsigned int i = 0; i < list.size(); i++)
+        {
+            visitor.pushFrameActionOnStack(list[i]);
+            if (list[i].second) list[i].second->accept(visitor);
+            visitor.popFrameAction();
+        }
+    }
+    visitor.popTimeline();
+}
+
+
+void osgAnimation::Timeline::setStats(osg::Stats* stats) { _stats = stats;}
+osg::Stats* osgAnimation::Timeline::getStats() { return _stats;}
+void osgAnimation::Timeline::collectStats(bool state) { _collectStats = state;}
+osgAnimation::StatsActionVisitor* osgAnimation::Timeline::getStatsVisitor() { return _statsVisitor; }
+
+void osgAnimation::Timeline::clearActions()
+{
+    _actions.clear();
+    _addActionOperations.clear();
+    _removeActionOperations.clear();
+}
+
+void osgAnimation::Timeline::update(double simulationTime)
+{
+    // first time we call update we generate one frame
+    UpdateActionVisitor updateTimeline;
+    if (!_initFirstFrame)
+    {
+        _lastUpdate = simulationTime;
+        _initFirstFrame = true;
+
+        updateTimeline.setFrame(_currentFrame);
+        accept(updateTimeline);
+
+        if (_collectStats)
+        {
+            if (!_statsVisitor)
+                _statsVisitor = new osgAnimation::StatsActionVisitor();
+            _statsVisitor->setStats(_stats);
+            _statsVisitor->setFrame(_currentFrame);
+            _statsVisitor->reset();
+            accept(*_statsVisitor);
+        }
+
+        processPendingOperation();
+    }
+
+    // find the number of frame pass since the last update
+    double delta = (simulationTime - _lastUpdate);
+    double nbframes = delta * _fps * _speed;
+    unsigned int nb = static_cast<unsigned int>(floor(nbframes));
+
+    for (unsigned int i = 0; i < nb; i++)
+    {
+        if (_state == Play)
+            _currentFrame++;
+
+        updateTimeline.setFrame(_currentFrame);
+        accept(updateTimeline);
+        if (_collectStats) 
+        {
+            if (!_statsVisitor)
+                _statsVisitor = new StatsActionVisitor;
+            _statsVisitor->setStats(_stats);
+            _statsVisitor->setFrame(_currentFrame);
+            _statsVisitor->reset();
+            accept(*_statsVisitor);
+        }
+
+        processPendingOperation();
+    }
+    if (nb)
+    {
+        _lastUpdate += ((double)nb) / _fps;
+    }
+}
+
+void osgAnimation::Timeline::removeAction(Action* action)
+{
+    if (getEvaluating())
+        _removeActionOperations.push_back(FrameAction(0, action));
+    else
+        internalRemoveAction(action);
+}
+
+void osgAnimation::Timeline::addActionAt(unsigned int frame, Action* action, int priority)
+{
+    if (getEvaluating())
+        _addActionOperations.push_back(Command(priority,FrameAction(frame, action)));
+    else
+        internalAddAction(priority, FrameAction(frame, action));
+}
+void osgAnimation::Timeline::addActionAt(double t, Action* action, int priority)
+{
+    unsigned int frame = static_cast<unsigned int>(floor(t * _fps));
+    addActionAt(frame, action, priority);
+}
+
+void osgAnimation::Timeline::addActionNow(Action* action, int priority)
+{
+    addActionAt(getCurrentFrame(), action, priority);
+}
+
+void osgAnimation::Timeline::processPendingOperation()
+{
+    // process all pending add action operation
+    while( !_addActionOperations.empty())
+    {
+        internalAddAction(_addActionOperations.back()._priority, _addActionOperations.back()._action);
+        _addActionOperations.pop_back();
+    }
+
+    // process all pending remove action operation
+    while( !_removeActionOperations.empty())
+    {
+        internalRemoveAction(_removeActionOperations.back().second.get());
+        _removeActionOperations.pop_back();
+    }
+}
+
+void osgAnimation::Timeline::internalRemoveAction(Action* action)
+{
+    for (ActionLayers::iterator it = _actions.begin(); it != _actions.end(); it++)
+    {
+        ActionList& fa = it->second;
+        for (unsigned int i = 0; i < fa.size(); i++)
+            if (fa[i].second.get() == action)
+            {
+                fa.erase(fa.begin() + i);
+                return;
+            }
+    }
+}
+
+void osgAnimation::Timeline::internalAddAction(int priority, const FrameAction& ftl)
+{
+    _actions[priority].insert(_actions[priority].begin(), ftl);
+//    _actions[priority].push_back(ftl);
+}
+
+#if 0
+void osgAnimation::Timeline::evaluateCallback(unsigned int frame)
+{
+    // update from high priority to low priority
+    for( ActionLayers::reverse_iterator iterAnim = _actions.rbegin(); iterAnim != _actions.rend(); ++iterAnim )
+    {
+        // update all animation
+        ActionList& list = iterAnim->second;
+        for (unsigned int i = 0; i < list.size(); i++)
+        {
+            unsigned int firstFrame = list[i].first;
+            Action* action = list[i].second.get();
+            // check if current frame of timeline hit an action interval
+            if (frame >= firstFrame && 
+                frame < (firstFrame + action->getNumFrames()) )
+                action->evaluateCallback(frame - firstFrame);
+        }
+    }
+    processPendingOperation();
+}
+#endif
+
+bool osgAnimation::Timeline::isActive(Action* activeAction)
+{
+    // update from high priority to low priority
+    for( ActionLayers::iterator iterAnim = _actions.begin(); iterAnim != _actions.end(); ++iterAnim )
+    {
+        // update all animation
+        ActionList& list = iterAnim->second;
+        for (unsigned int i = 0; i < list.size(); i++)
+        {
+            Action* action = list[i].second.get();
+            if (action == activeAction) 
+            {
+                unsigned int firstFrame = list[i].first;
+                // check if current frame of timeline hit an action interval
+                if (_currentFrame >= firstFrame && 
+                    _currentFrame < (firstFrame + action->getNumFrames()) )
+                    return true;
+            }
+        }
+    }
+    return false;
 }

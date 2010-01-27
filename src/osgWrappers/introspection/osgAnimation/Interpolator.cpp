b9116c31038edebd96cc5903a47098e38af78689
Robert OSFIELD <robert.osfield@gmail.com>
2010-01-27 15:00:01 +0000

    Updated wrappers

--- src/osgWrappers/introspection/osgAnimation/Interpolator.cpp
+++ src/osgWrappers/introspection/osgAnimation/Interpolator.cpp
@@ -60,16 +60,3 @@ TYPE_NAME_ALIAS(osgAnimation::TemplateCubicBezierInterpolator< osg::Vec3 COMMA  
 
 TYPE_NAME_ALIAS(osgAnimation::TemplateCubicBezierInterpolator< osg::Vec4 COMMA  osgAnimation::Vec4CubicBezier >, osgAnimation::Vec4CubicBezierInterpolator)
 
-BEGIN_OBJECT_REFLECTOR(osgAnimation::TemplateLinearInterpolator< osg::Matrixf COMMA  osg::Matrixf >)
-	I_DeclaringFile("osgAnimation/Interpolator");
-	I_BaseType(osgAnimation::TemplateInterpolatorBase);
-	I_Constructor0(____TemplateLinearInterpolator,
-	               "",
-	               "");
-	I_Method3(void, getValue, IN, const osgAnimation::TemplateKeyframeContainer< KEY > &, keyframes, IN, float, time, IN, TYPE &, result,
-	          Properties::NON_VIRTUAL,
-	          __void__getValue__C5_TemplateKeyframeContainerT1_KEY__R1__float__TYPE_R1,
-	          "",
-	          "");
-END_REFLECTOR
-

e9d8b3f3ef89da8e799b413d0d49befb1374078b
Robert OSFIELD <robert.osfield@gmail.com>
2010-03-11 16:46:01 +0000

    Clean up of initialization of statics/use of getenv

--- src/osg/Geometry.cpp
+++ src/osg/Geometry.cpp
@@ -535,14 +535,6 @@ unsigned int Geometry::getPrimitiveSetIndex(const PrimitiveSet* primitiveset) co
 
 bool Geometry::computeFastPathsUsed()
 {
-    static bool s_DisableFastPathInDisplayLists = getenv("OSG_DISABLE_FAST_PATH_IN_DISPLAY_LISTS")!=0;
-    if (_useDisplayList && s_DisableFastPathInDisplayLists)
-    {
-        osg::notify(osg::DEBUG_INFO)<<"Geometry::computeFastPathsUsed() - Disabling fast paths in display lists"<<std::endl;
-        _supportsVertexBufferObjects = _fastPath = false;
-        return _fastPath;
-    }
-
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //
     // check to see if fast path can be used.

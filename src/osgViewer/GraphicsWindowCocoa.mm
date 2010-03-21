b5743c1c9f716c360e4a52867170a408dd182482
Paul MARTZ <pmartz@skew-matrix.com>
2010-03-21 20:45:30 +0000

    2.8 branch. The main point of this commit is to eliminate static initializers for OS X. Merges: 9894, 11207, and 11263.

--- src/osgViewer/GraphicsWindowCocoa.mm
+++ src/osgViewer/GraphicsWindowCocoa.mm
@@ -1378,16 +1378,17 @@ GraphicsWindowCocoa::~GraphicsWindowCocoa()
 // CocoaWindowingSystemInterface
 // ----------------------------------------------------------------------------------------------------------
 
-struct CocoaWindowingSystemInterface : public DarwinWindowingSystemInterface {
-    
-    CocoaWindowingSystemInterface()
-    :    DarwinWindowingSystemInterface()
-    
-    {        
+struct CocoaWindowingSystemInterface : public DarwinWindowingSystemInterface
+{
+
+    CocoaWindowingSystemInterface() : DarwinWindowingSystemInterface()
+    {
     }
-    
+
     void initAsStandaloneApplication() 
     {
+        _init();
+
         static bool s_inited = false;
         if (s_inited) return;
         s_inited = true;
@@ -1417,6 +1418,8 @@ struct CocoaWindowingSystemInterface : public DarwinWindowingSystemInterface {
     
     virtual osg::GraphicsContext* createGraphicsContext(osg::GraphicsContext::Traits* traits) 
     {
+        _init();
+
         if (!traits->pbuffer) 
         {
             GraphicsWindowCocoa::WindowData* windowData = traits->inheritedWindowData ? dynamic_cast<GraphicsWindowCocoa::WindowData*>(traits->inheritedWindowData.get()) : NULL;

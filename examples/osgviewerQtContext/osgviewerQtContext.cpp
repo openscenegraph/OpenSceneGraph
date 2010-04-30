84b283e4544c792cdd98a8a3da9ac162ac107eab
Robert OSFIELD <robert.osfield@gmail.com>
2010-04-30 12:22:31 +0000

    From Wang Rui, "I've just made another OSG+Qt (GUI) examples to demonstrate how to create OSG views, add them to a CompositeViewer, and add corresponding widgets to Qt layouts or as popup windows. The example inherits a GraphicsWindowQt from the GraphicsWindow base class and implements most of the virtual methods. A QGLWidget is created at the same time to perform keyboards and mouse events, who is also added as the main widget's child.
    The new example, named osgviewerQtContext (because of deriving from GraphicsContext), works fine on Windows XP SP3 and Qt 4.5.0, with 4 widgets in QGridLayout and a popup window and 60Hz frame rate. I haven't tested it on Unix/Linux and Mac OSX yet. So any feedback from these platforms is appreciated. I wish this example be a useful complement to current osgviewerQt and osgviewerQtWidgets ones. :)
    
    Some unfinished functionalities: inheritedWindowData, sharedContext, and more tests needed."

---
+++ examples/osgviewerQtContext/osgviewerQtContext.cpp
@@ -0,0 +1,92 @@
+#include <QtCore/QTimer>
+#include <QtGui/QApplication>
+#include <QtGui/QGridLayout>
+#include <osgViewer/CompositeViewer>
+#include <osgViewer/ViewerEventHandlers>
+#include <osgGA/TrackballManipulator>
+#include <osgDB/ReadFile>
+#include <iostream>
+#include "GraphicsWindowQt"
+
+class ViewerWidget : public QWidget, public osgViewer::CompositeViewer
+{
+public:
+    ViewerWidget() : QWidget()
+    {
+        setThreadingModel(osgViewer::CompositeViewer::SingleThreaded);
+
+        QWidget* widget1 = addViewWidget( createCamera(0,0,100,100), osgDB::readNodeFile("cow.osg") );
+        QWidget* widget2 = addViewWidget( createCamera(0,0,100,100), osgDB::readNodeFile("glider.osg") );
+        QWidget* widget3 = addViewWidget( createCamera(0,0,100,100), osgDB::readNodeFile("axes.osg") );
+        QWidget* widget4 = addViewWidget( createCamera(0,0,100,100), osgDB::readNodeFile("fountain.osg") );
+        QWidget* popupWidget = addViewWidget( createCamera(900,100,320,240,"Popup window",true),
+                                            osgDB::readNodeFile("dumptruck.osg") );
+        popupWidget->show();
+
+        QGridLayout* grid = new QGridLayout;
+        grid->addWidget( widget1, 0, 0 );
+        grid->addWidget( widget2, 0, 1 );
+        grid->addWidget( widget3, 1, 0 );
+        grid->addWidget( widget4, 1, 1 );
+        setLayout( grid );
+
+        connect( &_timer, SIGNAL(timeout()), this, SLOT(update()) );
+        _timer.start( 10 );
+    }
+    
+    QWidget* addViewWidget( osg::Camera* camera, osg::Node* scene )
+    {
+        osgViewer::View* view = new osgViewer::View;
+        view->setCamera( camera );
+        addView( view );
+        
+        view->setSceneData( scene );
+        view->addEventHandler( new osgViewer::StatsHandler );
+        view->setCameraManipulator( new osgGA::TrackballManipulator );
+        
+        GraphicsWindowQt* gw = dynamic_cast<GraphicsWindowQt*>( camera->getGraphicsContext() );
+        return gw ? gw->getGraphWidget() : NULL;
+    }
+    
+    osg::Camera* createCamera( int x, int y, int w, int h, const std::string& name="", bool windowDecoration=false )
+    {
+        osg::DisplaySettings* ds = osg::DisplaySettings::instance();
+        osg::ref_ptr<osg::GraphicsContext::Traits> traits = new osg::GraphicsContext::Traits;
+        traits->windowName = name;
+        traits->windowDecoration = windowDecoration;
+        traits->x = x;
+        traits->y = y;
+        traits->width = w;
+        traits->height = h;
+        traits->doubleBuffer = true;
+        traits->alpha = ds->getMinimumNumAlphaBits();
+        traits->stencil = ds->getMinimumNumStencilBits();
+        traits->sampleBuffers = ds->getMultiSamples();
+        traits->samples = ds->getNumMultiSamples();
+        
+        osg::ref_ptr<osg::Camera> camera = new osg::Camera;
+        camera->setGraphicsContext( new GraphicsWindowQt(traits.get()) );
+        
+        camera->setClearColor( osg::Vec4(0.2, 0.2, 0.6, 1.0) );
+        camera->setViewport( new osg::Viewport(0, 0, traits->width, traits->height) );
+        camera->setProjectionMatrixAsPerspective(
+            30.0f, static_cast<double>(traits->width)/static_cast<double>(traits->height), 1.0f, 10000.0f );
+        return camera.release();
+    }
+    
+    virtual void paintEvent( QPaintEvent* event )
+    { frame(); }
+
+protected:
+    
+    QTimer _timer;
+};
+
+int main( int argc, char** argv )
+{
+    QApplication app(argc, argv);
+    ViewerWidget* viewWidget = new ViewerWidget;
+    viewWidget->setGeometry( 100, 100, 800, 600 );
+    viewWidget->show();
+    return app.exec();
+}

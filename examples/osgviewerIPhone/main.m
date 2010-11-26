b523cb15c1876da624019835ba821555f7fbaa7a
Robert Osfield <robert@openscenegraph.com>
2010-11-26 18:19:28 +0000

    From Tomas Holgarth and Stephan Huber, " attached you'll find the second part of the IOS-submission. It contains
    * GraphicsWindowIOS, which supports external and "retina" displays,
     multisample-buffers (for IOS > 4.0) and multi-touch-events
    * an ios-specific implementation of the imageio-plugin
    * an iphone-viewer example
    * cMake support for creating a xcode-project
    * an updated ReadMe-file describing the necessary steps to get a
     working xcode-project-file from CMake
    
    Please credit Thomas Hogarth and Stephan Huber for these changes.
    
    This brings the ios-support in line with the git-fork on github. It
    needs some more testing and some more love, the cmake-process is still a
    little complicated.
    
    You'll need a special version of the freetype lib compiled for IOS,
    there's one bundled in the OpenFrameworks-distribution, which can be used."
    
    Notes, from Robert Osfield, modified CMakeLists.txt files so that the IOS specific paths are within IF(APPLE) blocks.

---
+++ examples/osgviewerIPhone/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iphoneViewer
+//
+//  Created by Thomas Hogarth on 10/05/2009.
+//  Copyright HogBox 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool *pool = [NSAutoreleasePool new];
+    UIApplicationMain(argc, argv, nil, @"iphoneViewerAppDelegate");
+    [pool release];
+    return 0;
+}

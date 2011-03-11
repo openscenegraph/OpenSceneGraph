50366ccb91c61ec889a1e9bf0e406e7056e38dfb
Robert Osfield <robert@openscenegraph.com>
2011-03-11 16:31:01 +0000

    From Benjamin Wasty and David Guthrie, "currently, non-alpha-numeric keys are not recognized (except as modifiers) in osgQt, so I added the mapping code from my Qt integration to GraphicsWindowQt (which is based on Delta3D code from David Guthrie - he gave me permission to submit it under OSGPL)."

--- src/osgQt/GraphicsWindowQt.cpp
+++ src/osgQt/GraphicsWindowQt.cpp
@@ -16,6 +16,96 @@
 namespace osgQt
 {
 
+class QtKeyboardMap
+{
+
+public:
+    QtKeyboardMap()
+    { 
+        mKeyMap[Qt::Key_Escape     ] = osgGA::GUIEventAdapter::KEY_Escape;
+        mKeyMap[Qt::Key_Delete   ] = osgGA::GUIEventAdapter::KEY_Delete;
+        mKeyMap[Qt::Key_Home       ] = osgGA::GUIEventAdapter::KEY_Home;
+        mKeyMap[Qt::Key_Enter      ] = osgGA::GUIEventAdapter::KEY_KP_Enter;
+        mKeyMap[Qt::Key_End        ] = osgGA::GUIEventAdapter::KEY_End;
+        mKeyMap[Qt::Key_Return     ] = osgGA::GUIEventAdapter::KEY_Return;
+        mKeyMap[Qt::Key_PageUp     ] = osgGA::GUIEventAdapter::KEY_Page_Up;
+        mKeyMap[Qt::Key_PageDown   ] = osgGA::GUIEventAdapter::KEY_Page_Down;
+        mKeyMap[Qt::Key_Left       ] = osgGA::GUIEventAdapter::KEY_Left;
+        mKeyMap[Qt::Key_Right      ] = osgGA::GUIEventAdapter::KEY_Right;
+        mKeyMap[Qt::Key_Up         ] = osgGA::GUIEventAdapter::KEY_Up;
+        mKeyMap[Qt::Key_Down       ] = osgGA::GUIEventAdapter::KEY_Down;
+        mKeyMap[Qt::Key_Backspace  ] = osgGA::GUIEventAdapter::KEY_BackSpace;
+        mKeyMap[Qt::Key_Tab        ] = osgGA::GUIEventAdapter::KEY_Tab;
+        mKeyMap[Qt::Key_Space      ] = osgGA::GUIEventAdapter::KEY_Space;
+        mKeyMap[Qt::Key_Delete     ] = osgGA::GUIEventAdapter::KEY_Delete;
+        mKeyMap[Qt::Key_Alt      ] = osgGA::GUIEventAdapter::KEY_Alt_L;
+        mKeyMap[Qt::Key_Shift    ] = osgGA::GUIEventAdapter::KEY_Shift_L;
+        mKeyMap[Qt::Key_Control  ] = osgGA::GUIEventAdapter::KEY_Control_L;
+        mKeyMap[Qt::Key_Meta     ] = osgGA::GUIEventAdapter::KEY_Meta_L;
+
+        mKeyMap[Qt::Key_F1             ] = osgGA::GUIEventAdapter::KEY_F1;
+        mKeyMap[Qt::Key_F2             ] = osgGA::GUIEventAdapter::KEY_F2;
+        mKeyMap[Qt::Key_F3             ] = osgGA::GUIEventAdapter::KEY_F3;
+        mKeyMap[Qt::Key_F4             ] = osgGA::GUIEventAdapter::KEY_F4;
+        mKeyMap[Qt::Key_F5             ] = osgGA::GUIEventAdapter::KEY_F5;
+        mKeyMap[Qt::Key_F6             ] = osgGA::GUIEventAdapter::KEY_F6;
+        mKeyMap[Qt::Key_F7             ] = osgGA::GUIEventAdapter::KEY_F7;
+        mKeyMap[Qt::Key_F8             ] = osgGA::GUIEventAdapter::KEY_F8;
+        mKeyMap[Qt::Key_F9             ] = osgGA::GUIEventAdapter::KEY_F9;
+        mKeyMap[Qt::Key_F10            ] = osgGA::GUIEventAdapter::KEY_F10;
+        mKeyMap[Qt::Key_F11            ] = osgGA::GUIEventAdapter::KEY_F11;
+        mKeyMap[Qt::Key_F12            ] = osgGA::GUIEventAdapter::KEY_F12;
+        mKeyMap[Qt::Key_F13            ] = osgGA::GUIEventAdapter::KEY_F13;
+        mKeyMap[Qt::Key_F14            ] = osgGA::GUIEventAdapter::KEY_F14;
+        mKeyMap[Qt::Key_F15            ] = osgGA::GUIEventAdapter::KEY_F15;
+        mKeyMap[Qt::Key_F16            ] = osgGA::GUIEventAdapter::KEY_F16;
+        mKeyMap[Qt::Key_F17            ] = osgGA::GUIEventAdapter::KEY_F17;
+        mKeyMap[Qt::Key_F18            ] = osgGA::GUIEventAdapter::KEY_F18;
+        mKeyMap[Qt::Key_F19            ] = osgGA::GUIEventAdapter::KEY_F19;
+        mKeyMap[Qt::Key_F20            ] = osgGA::GUIEventAdapter::KEY_F20;
+
+        mKeyMap[Qt::Key_hyphen         ] = '-';
+        mKeyMap[Qt::Key_Equal         ] = '=';
+
+        mKeyMap[Qt::Key_division      ] = osgGA::GUIEventAdapter::KEY_KP_Divide;
+        mKeyMap[Qt::Key_multiply      ] = osgGA::GUIEventAdapter::KEY_KP_Multiply;
+        mKeyMap[Qt::Key_Minus         ] = '-';
+        mKeyMap[Qt::Key_Plus          ] = '+';
+        //mKeyMap[Qt::Key_H              ] = osgGA::GUIEventAdapter::KEY_KP_Home;
+        //mKeyMap[Qt::Key_                    ] = osgGA::GUIEventAdapter::KEY_KP_Up;
+        //mKeyMap[92                    ] = osgGA::GUIEventAdapter::KEY_KP_Page_Up;
+        //mKeyMap[86                    ] = osgGA::GUIEventAdapter::KEY_KP_Left;
+        //mKeyMap[87                    ] = osgGA::GUIEventAdapter::KEY_KP_Begin;
+        //mKeyMap[88                    ] = osgGA::GUIEventAdapter::KEY_KP_Right;
+        //mKeyMap[83                    ] = osgGA::GUIEventAdapter::KEY_KP_End;
+        //mKeyMap[84                    ] = osgGA::GUIEventAdapter::KEY_KP_Down;
+        //mKeyMap[85                    ] = osgGA::GUIEventAdapter::KEY_KP_Page_Down;
+        mKeyMap[Qt::Key_Insert        ] = osgGA::GUIEventAdapter::KEY_KP_Insert;
+        //mKeyMap[Qt::Key_Delete        ] = osgGA::GUIEventAdapter::KEY_KP_Delete;
+    }
+
+    ~QtKeyboardMap()
+    {
+    }
+
+    int remapKey(QKeyEvent* event)
+    {
+        KeyMap::iterator itr = mKeyMap.find(event->key());
+        if (itr == mKeyMap.end())
+        {
+            return int(*(event->text().toAscii().data()));
+        }
+        else
+            return itr->second;
+    }
+
+    private:
+    typedef std::map<unsigned int, int> KeyMap;
+    KeyMap mKeyMap;
+};
+
+static QtKeyboardMap s_QtKeyboardMap;
+
 GraphWidget::GraphWidget( const QGLFormat& format, QWidget* parent, const QGLWidget* shareWidget, Qt::WindowFlags f )
 : QGLWidget(format, parent, shareWidget, f)
 {
@@ -43,13 +133,15 @@ void GraphWidget::resizeEvent( QResizeEvent* event )
 void GraphWidget::keyPressEvent( QKeyEvent* event )
 {
     setKeyboardModifiers( event );
-    _gw->getEventQueue()->keyPress( (osgGA::GUIEventAdapter::KeySymbol) *(event->text().toAscii().data()) );
+    int value = s_QtKeyboardMap.remapKey(event);
+    _gw->getEventQueue()->keyPress(value);
 }
 
 void GraphWidget::keyReleaseEvent( QKeyEvent* event )
 {
     setKeyboardModifiers( event );
-    _gw->getEventQueue()->keyRelease( (osgGA::GUIEventAdapter::KeySymbol) *(event->text().toAscii().data()) );
+    int value = s_QtKeyboardMap.remapKey(event);
+    _gw->getEventQueue()->keyRelease(value);
 }
 
 void GraphWidget::mousePressEvent( QMouseEvent* event )

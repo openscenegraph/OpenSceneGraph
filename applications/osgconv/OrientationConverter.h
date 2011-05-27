d7c472e54785118bdc7cd508877b2ce5df6dd301
Robert OSFIELD <robert.osfield@gmail.com>
2011-05-27 11:18:44 +0000

    Fixed indentation

--- applications/osgconv/OrientationConverter.h
+++ applications/osgconv/OrientationConverter.h
@@ -8,23 +8,23 @@
 
 class OrientationConverter {
     public :
-    	OrientationConverter(void);
-	void setRotation( const osg::Vec3 &from, 
-	                    const osg::Vec3 &to  );
-	void setRotation( float degrees, const osg::Vec3 &axis  );
-	void setTranslation( const osg::Vec3 &trans);
-	void setScale( const osg::Vec3 &trans);
-        
+        OrientationConverter(void);
+        void setRotation( const osg::Vec3 &from,
+                          const osg::Vec3 &to  );
+        void setRotation( float degrees, const osg::Vec3 &axis  );
+        void setTranslation( const osg::Vec3 &trans);
+        void setScale( const osg::Vec3 &trans);
+
         /** return the root of the updated subgraph as the subgraph
           * the node passed in my flatten during optimization.*/
-	osg::Node* convert( osg::Node* node );
+        osg::Node* convert( osg::Node* node );
 
     private :
-    	OrientationConverter( const OrientationConverter& ) {}
-	OrientationConverter& operator = (const OrientationConverter& ) { return *this; }
+        OrientationConverter( const OrientationConverter& ) {}
+        OrientationConverter& operator = (const OrientationConverter& ) { return *this; }
 
-	osg::Matrix R, T, S;
-	bool _trans_set;
+        osg::Matrix R, T, S;
+        bool _trans_set;
 
 };
 #endif

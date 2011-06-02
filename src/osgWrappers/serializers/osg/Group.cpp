49f96b27fde450901309c9f74334a2c9369198c6
Robert OSFIELD <robert.osfield@gmail.com>
2011-06-02 20:38:13 +0000

    Added ref_ptr<> usage to avoid possible memory leak.

--- src/osgWrappers/serializers/osg/Group.cpp
+++ src/osgWrappers/serializers/osg/Group.cpp
@@ -13,7 +13,8 @@ static bool readChildren( osgDB::InputStream& is, osg::Group& node )
     unsigned int size = 0; is >> size >> osgDB::BEGIN_BRACKET;
     for ( unsigned int i=0; i<size; ++i )
     {
-        osg::Node* child = dynamic_cast<osg::Node*>( is.readObject() );
+        osg::ref_ptr<osg::Object> obj = is.readObject();
+        osg::Node* child = dynamic_cast<osg::Node*>( obj.get() );
         if ( child ) node.addChild( child );
     }
     is >> osgDB::END_BRACKET;

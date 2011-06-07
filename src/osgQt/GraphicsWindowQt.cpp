4f7903f732b6d9907b408bb5bb4f24cf2ee451cb
Robert Osfield <robert@openscenegraph.com>
2011-06-07 14:26:13 +0000

    From Mathias Froehlich, build fixes for legacy unix systems

--- src/osgQt/GraphicsWindowQt.cpp
+++ src/osgQt/GraphicsWindowQt.cpp
@@ -342,7 +342,11 @@ bool GraphicsWindowQt::init( QWidget* parent, const QGLWidget* shareWidget, Qt::
         // WindowFlags
         Qt::WindowFlags flags = f | Qt::Window | Qt::CustomizeWindowHint;
         if ( _traits->windowDecoration )
-            flags |= Qt::WindowTitleHint | Qt::WindowMinMaxButtonsHint | Qt::WindowSystemMenuHint | Qt::WindowCloseButtonHint;
+            flags |= Qt::WindowTitleHint | Qt::WindowMinMaxButtonsHint | Qt::WindowSystemMenuHint
+#if (QT_VERSION_CHECK(4, 5, 0) <= QT_VERSION)
+                | Qt::WindowCloseButtonHint
+#endif
+                ;
 
         // create widget
         _widget = new GLWidget( traits2qglFormat( _traits.get() ), parent, shareWidget, flags );

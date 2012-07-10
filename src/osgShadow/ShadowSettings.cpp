c4136a856df1fc08232be6b789ca943152a3aedb
Robert OSFIELD <robert.osfield@gmail.com>
2012-07-10 15:50:46 +0000

    Moved mask settings into ShadowSettings

--- src/osgShadow/ShadowSettings.cpp
+++ src/osgShadow/ShadowSettings.cpp
@@ -16,6 +16,8 @@
 using namespace osgShadow;
 
 ShadowSettings::ShadowSettings():
+     _receivesShadowTraversalMask(0xffffffff),
+     _castsShadowTraversalMask(0xffffffff),
     _lightNum(-1),
     _baseShadowTextureUnit(1),
     _useShadowMapTextureOverride(true),
@@ -33,6 +35,8 @@ ShadowSettings::ShadowSettings():
 
 ShadowSettings::ShadowSettings(const ShadowSettings& ss, const osg::CopyOp& copyop):
     Object(ss,copyop),
+    _receivesShadowTraversalMask(ss._receivesShadowTraversalMask),
+    _castsShadowTraversalMask(ss._castsShadowTraversalMask),
     _lightNum(ss._lightNum),
     _baseShadowTextureUnit(ss._baseShadowTextureUnit),
     _useShadowMapTextureOverride(ss._useShadowMapTextureOverride),

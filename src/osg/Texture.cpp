42cb311f413eea290c698f83d81a05e7cf2980fe
Robert Osfield <robert@openscenegraph.com>
2012-07-24 09:05:04 +0000

    Added FireGL to prefernce list for use glGenerateMipMap

--- src/osg/Texture.cpp
+++ src/osg/Texture.cpp
@@ -2473,6 +2473,7 @@ Texture::Extensions::Extensions(unsigned int contextID)
     std::string rendererString(renderer ? renderer : "");
 
     bool radeonHardwareDetected = (rendererString.find("Radeon")!=std::string::npos || rendererString.find("RADEON")!=std::string::npos);
+    bool fireGLHardwareDetected = (rendererString.find("FireGL")!=std::string::npos || rendererString.find("FIREGL")!=std::string::npos);
 
     bool builtInSupport = OSG_GLES2_FEATURES || OSG_GL3_FEATURES;
 
@@ -2511,7 +2512,7 @@ Texture::Extensions::Extensions(unsigned int contextID)
 
     _isGenerateMipMapSupported = builtInSupport || isGLExtensionOrVersionSupported(contextID,"GL_SGIS_generate_mipmap", 1.4f);
 
-    _preferGenerateMipmapSGISForPowerOfTwo = radeonHardwareDetected ? false : true;
+    _preferGenerateMipmapSGISForPowerOfTwo = (radeonHardwareDetected||fireGLHardwareDetected) ? false : true;
 
     _isTextureMultisampledSupported = isGLExtensionSupported(contextID,"GL_ARB_texture_multisample");
 

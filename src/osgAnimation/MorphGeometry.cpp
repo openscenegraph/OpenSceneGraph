12084189b0713f24047845e92962a18f44f27e00
Robert OSFIELD <robert.osfield@gmail.com>
2013-01-30 10:40:26 +0000

    Fixed the erronous header so that it is OSGPL like the rest of the osgAnimation.

--- src/osgAnimation/MorphGeometry.cpp
+++ src/osgAnimation/MorphGeometry.cpp
@@ -1,26 +1,17 @@
 /*  -*-c++-*-
  *  Copyright (C) 2008 Cedric Pinson <cedric.pinson@plopbyte.net>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This library is open source and may be redistributed and/or modified under
+ * the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or
+ * (at your option) any later version.  The full license is in LICENSE file
+ * included with this distribution, and on the openscenegraph.org website.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * Authors:
- *
- * Roland Smeenk
- * Cedric Pinson <cedric.pinson@plopbyte.net>
- *
+ * OpenSceneGraph Public License for more details.
  */
+
 #include <osg/Geode>
 #include <osgAnimation/MorphGeometry>
 

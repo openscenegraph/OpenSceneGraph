426b50075d612925565f614da2c4123640656854
Robert OSFIELD <robert.osfield@gmail.com>
2013-08-21 19:54:03 +0000

    Added initial classes for present3D refactor

---
+++ src/osgWrappers/serializers/osgPresentation/Group.cpp
@@ -0,0 +1,11 @@
+#include <osgPresentation/Group>
+#include <osgDB/ObjectWrapper>
+#include <osgDB/InputStream>
+#include <osgDB/OutputStream>
+
+REGISTER_OBJECT_WRAPPER( Group,
+                         new osgPresentation::Group,
+                         osgPresentation::Group,
+                         "osg::Object osg::Node osg::Group osg::Transform osg::MatrixTransform osgPresentation::Group" )
+{
+}
---
+++ src/osgWrappers/serializers/osgPresentation/Presentation.cpp
@@ -0,0 +1,11 @@
+#include <osgPresentation/Presentation>
+#include <osgDB/ObjectWrapper>
+#include <osgDB/InputStream>
+#include <osgDB/OutputStream>
+
+REGISTER_OBJECT_WRAPPER( Presentation,
+                         new osgPresentation::Presentation,
+                         osgPresentation::Presentation,
+                         "osg::Object osg::Node osg::Group osg::Transform osg::MatrixTransform osgPresentation::Group osgPresentation::Presentation" )
+{
+}

31e83258aa51c0f62b4d65fc72b74b7982b45424
Robert OSFIELD <robert.osfield@gmail.com>
2013-10-02 11:29:58 +0000

    From Dmitry Marakasov, "These are FreeBSD bits for src/osgPlugins/osc/osc/OscHostEndianness.h, required to build newever OSG on this OS. Also corrects file name in the error message - I was confused not to find OSCHostEndianness.h after I've got this error.
    Tested by successfully building OSG 3.2.0 with this patch on FreeBSD
    9.1."

--- src/osgPlugins/osc/osc/OscHostEndianness.h
+++ src/osgPlugins/osc/osc/OscHostEndianness.h
@@ -60,6 +60,15 @@
         #else
             #error Unknown machine endianness detected.
         #endif
+    #elif defined(__FreeBSD__)
+        #include <sys/endian.h>
+        #if (_BYTE_ORDER == _LITTLE_ENDIAN)
+            #define __LITTLE_ENDIAN__
+        #elif (_BYTE_ORDER == _BIG_ENDIAN)
+            #define __BIG_ENDIAN__
+        #else
+            #error Unknown machine endianness detected.
+        #endif
     #endif
 
     #if   defined(__LITTLE_ENDIAN__)
@@ -74,7 +83,7 @@
 
     #else
 
-        #error please edit OSCHostEndianness.h to configure endianness
+        #error please edit OscHostEndianness.h to configure endianness
 
     #endif
 

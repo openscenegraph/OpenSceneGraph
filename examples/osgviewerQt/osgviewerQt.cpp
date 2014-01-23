a08ecc1449eb23f9b97c9ac1f598b1b01328cbde
Robert OSFIELD <robert.osfield@gmail.com>
2014-01-23 10:09:57 +0000

    Cleaned up the Qt find package, and made changed the default threading model to SingleThreaded when using Qt5 to avoid crash due to regresssion since Qt4.

--- examples/osgviewerQt/osgviewerQt.cpp
+++ examples/osgviewerQt/osgviewerQt.cpp
@@ -93,7 +93,13 @@ int main( int argc, char** argv )
 {
     osg::ArgumentParser arguments(&argc, argv);
 
+#if QT_VERSION >= 0x050000
+    // Qt5 is currently crashing and reporting "Cannot make QOpenGLContext current in a different thread" when the viewer is run multi-threaded, this is regression from Qt4
+    osgViewer::ViewerBase::ThreadingModel threadingModel = osgViewer::ViewerBase::SingleThreaded;
+#else
     osgViewer::ViewerBase::ThreadingModel threadingModel = osgViewer::ViewerBase::CullDrawThreadPerContext;
+#endif
+
     while (arguments.read("--SingleThreaded")) threadingModel = osgViewer::ViewerBase::SingleThreaded;
     while (arguments.read("--CullDrawThreadPerContext")) threadingModel = osgViewer::ViewerBase::CullDrawThreadPerContext;
     while (arguments.read("--DrawThreadPerContext")) threadingModel = osgViewer::ViewerBase::DrawThreadPerContext;

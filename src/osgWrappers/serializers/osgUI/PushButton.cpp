8484ac81609d4b899f88686d93e1c964fd2c0637
Robert OSFIELD <robert.osfield@gmail.com>
2014-05-21 16:15:02 +0000

    Addd method implementation in serializers

--- src/osgWrappers/serializers/osgUI/PushButton.cpp
+++ src/osgWrappers/serializers/osgUI/PushButton.cpp
@@ -11,4 +11,8 @@ REGISTER_OBJECT_WRAPPER( PushButton,
                          "osg::Object osg::Node osg::Group osgUI::Widget osgUI::PushButton" )
 {
     ADD_STRING_SERIALIZER( Text, std::string());
+    ADD_METHOD( pressed );
+    ADD_METHOD( pressedImplementation );
+    ADD_METHOD( released );
+    ADD_METHOD( releasedImplementation );
 }

20f663dc88c9e81033e7b5029360269b5aefd789
Robert OSFIELD <robert.osfield@gmail.com>
2014-06-30 07:39:16 +0000

    Merged librsvg-2.0>=2.35 version check from svn/trunk.
    git-svn-id: http://svn.openscenegraph.org/osg/OpenSceneGraph/branches/OpenSceneGraph-3.2@14325 16af8721-9629-0410-8352-f15c8da7e697

--- CMakeModules/FindRSVG.cmake
+++ CMakeModules/FindRSVG.cmake
@@ -5,7 +5,9 @@ IF(PKG_CONFIG_FOUND)
 
     INCLUDE(FindPkgConfig)
 
-    PKG_CHECK_MODULES(RSVG librsvg-2.0)
+    #Version 2.35 introduces the rsvg_cleanup function which is used
+    PKG_CHECK_MODULES(RSVG librsvg-2.0>=2.35)
+
     PKG_CHECK_MODULES(CAIRO cairo)
 
     IF (RSVG_FOUND AND NOT CAIRO_FOUND)

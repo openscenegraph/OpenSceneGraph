53e337611a189c231a9ff002a389f71eb0ed2fa4
Robert Osfield <robert@openscenegraph.com>
2014-11-24 15:19:20 +0000

    From Jan Klimke, "i noticed, that there are a couple of additional flaws when building osg for Mac OS X 10.10 Yosemite.
    The mac os sdk version is recognized by the current CMAKE script as 10.1 instead of 10.10 since it cuts the version string from the 4th place. I introduced a more reliable version checking based on splitting the returned version code into MAJOR MINOR and PATCH parts and reassemble the OSG sdk version afterwards.
    
    I replaced the existing CMake code against the following (returning now version 10.10 as expected):
    
     # Determine the canonical name of the selected Platform SDK
       EXECUTE_PROCESS(COMMAND "/usr/bin/sw_vers" "-productVersion"
                       OUTPUT_VARIABLE OSG_OSX_SDK_NAME
                       OUTPUT_STRIP_TRAILING_WHITESPACE)
       STRING(REPLACE "." ";" MACOS_VERSION_LIST ${OSG_OSX_SDK_NAME})
       LIST(GET MACOS_VERSION_LIST 0 MACOS_VERSION_MAJOR)
       LIST(GET MACOS_VERSION_LIST 1 MACOS_VERSION_MINOR)
       LIST(GET MACOS_VERSION_LIST 2 MACOS_VERSION_PATCH)
    
       SET(OSG_OSX_SDK_NAME "macosx${MACOS_VERSION_MAJOR}.${MACOS_VERSION_MINOR}")
    
    Also i added the check for the new Version to some more find scripts.
    
    Additionally the nil object in Objective C now seems to be equivalent with a null_ptr that cannot be passed as GLInt anymore. So i switched this in the PixelBufferCocoa.mm to pass a zero instead of nil.
    "
    
    
    git-svn-id: http://svn.openscenegraph.org/osg/OpenSceneGraph/trunk@14527 16af8721-9629-0410-8352-f15c8da7e697

--- CMakeModules/FindQuickTime.cmake
+++ CMakeModules/FindQuickTime.cmake
@@ -65,7 +65,7 @@ ELSE()
       ENDIF()
       # Disable quicktime for >= 10.7, as it's officially deprecated
 
-      IF(${OSG_OSX_SDK_NAME} STREQUAL "macosx10.7" OR ${OSG_OSX_SDK_NAME} STREQUAL "macosx10.8" OR ${OSG_OSX_SDK_NAME} STREQUAL "macosx10.9")
+      IF(${OSG_OSX_SDK_NAME} STREQUAL "macosx10.7" OR ${OSG_OSX_SDK_NAME} STREQUAL "macosx10.8" OR ${OSG_OSX_SDK_NAME} STREQUAL "macosx10.9" OR ${OSG_OSX_SDK_NAME} STREQUAL "macosx10.10")
           MESSAGE("disabling quicktime because it's not supported by the selected SDK ${OSG_OSX_SDK_NAME}")
           SET(QUICKTIME_FOUND "NO")
       ENDIF()

7fbeeefea878cf280dd7050baa993fb587e1c0cb
Robert Osfield <robert@openscenegraph.com>
2015-01-21 11:09:29 +0000

    Added TextureWeights uniform support and controls to osgFX::MultiTextureControl to support usage with shaders.
    git-svn-id: http://svn.openscenegraph.org/osg/OpenSceneGraph/trunk@14668 16af8721-9629-0410-8352-f15c8da7e697

--- src/osgFX/MultiTextureControl.cpp
+++ src/osgFX/MultiTextureControl.cpp
@@ -19,13 +19,17 @@
 using namespace osg;
 using namespace osgFX;
 
-MultiTextureControl::MultiTextureControl()
+MultiTextureControl::MultiTextureControl():
+    _useTexEnvCombine(true),
+    _useTextureWeightsUniform(true)
 {
 }
 
 MultiTextureControl::MultiTextureControl(const MultiTextureControl& copy, const osg::CopyOp& copyop):
     Group(copy,copyop),
-    _textureWeightList(copy._textureWeightList)
+    _textureWeightList(copy._textureWeightList),
+    _useTexEnvCombine(copy._useTexEnvCombine),
+    _useTextureWeightsUniform(copy._useTextureWeightsUniform)
 {
     updateStateSet();
 }
@@ -45,104 +49,117 @@ void MultiTextureControl::updateStateSet()
 {
     osg::ref_ptr<osg::StateSet> stateset = new osg::StateSet;
 
-    unsigned int numTextureUnitsOn = 0;
-    unsigned int unit;
-    for(unit=0;unit<_textureWeightList.size();++unit)
+    if (_useTexEnvCombine)
     {
-        if (_textureWeightList[unit]>0.0f) ++numTextureUnitsOn;
-    }
-
-    if (numTextureUnitsOn<=1)
-    {
+        unsigned int numTextureUnitsOn = 0;
+        unsigned int unit;
         for(unit=0;unit<_textureWeightList.size();++unit)
         {
-            if (_textureWeightList[unit]>0.0f)
+            if (_textureWeightList[unit]>0.0f) ++numTextureUnitsOn;
+        }
+
+        if (numTextureUnitsOn<=1)
+        {
+            for(unit=0;unit<_textureWeightList.size();++unit)
             {
-                osg::TexEnv* texenv = new osg::TexEnv(osg::TexEnv::MODULATE);
-                stateset->setTextureAttribute(unit, texenv);
-                stateset->setTextureMode(unit, GL_TEXTURE_2D, osg::StateAttribute::ON);
+                if (_textureWeightList[unit]>0.0f)
+                {
+                    osg::TexEnv* texenv = new osg::TexEnv(osg::TexEnv::MODULATE);
+                    stateset->setTextureAttribute(unit, texenv);
+                    stateset->setTextureMode(unit, GL_TEXTURE_2D, osg::StateAttribute::ON);
+                }
+                else
+                {
+                    stateset->setTextureMode(unit, GL_TEXTURE_2D, osg::StateAttribute::OFF | osg::StateAttribute::OVERRIDE);
+                }
             }
-            else
+
+        }
+        else if (_textureWeightList.size()==2)
+        {
             {
-                stateset->setTextureMode(unit, GL_TEXTURE_2D, osg::StateAttribute::OFF | osg::StateAttribute::OVERRIDE);
+                osg::TexEnvCombine* texenv = new osg::TexEnvCombine;
+                texenv->setCombine_RGB(osg::TexEnvCombine::INTERPOLATE);
+                texenv->setSource0_RGB(osg::TexEnvCombine::TEXTURE0+0);
+                texenv->setOperand0_RGB(osg::TexEnvCombine::SRC_COLOR);
+                texenv->setSource1_RGB(osg::TexEnvCombine::TEXTURE0+1);
+                texenv->setOperand1_RGB(osg::TexEnvCombine::SRC_COLOR);
+                texenv->setSource2_RGB(osg::TexEnvCombine::CONSTANT);
+                texenv->setOperand2_RGB(osg::TexEnvCombine::SRC_COLOR);
+
+                float r = _textureWeightList[0]/(_textureWeightList[0]+_textureWeightList[1]);
+                texenv->setConstantColor(osg::Vec4(r,r,r,r));
+
+                stateset->setTextureAttribute(0, texenv);
+            }
+
+            {
+                osg::TexEnvCombine* texenv = new osg::TexEnvCombine;
+                texenv->setCombine_RGB(osg::TexEnvCombine::MODULATE);
+                texenv->setSource0_RGB(osg::TexEnvCombine::PREVIOUS);
+                texenv->setOperand0_RGB(osg::TexEnvCombine::SRC_COLOR);
+                texenv->setSource1_RGB(osg::TexEnvCombine::PRIMARY_COLOR);
+                texenv->setOperand1_RGB(osg::TexEnvCombine::SRC_COLOR);
+
+                stateset->setTextureAttribute(1, texenv);
             }
         }
+        else if (_textureWeightList.size()==3)
+        {
+            float b = (_textureWeightList[0]+_textureWeightList[1])/(_textureWeightList[0]+_textureWeightList[1]+_textureWeightList[2]);
+            float a = _textureWeightList[0]/(_textureWeightList[0]+_textureWeightList[1]);
 
+            {
+                osg::TexEnvCombine* texenv = new osg::TexEnvCombine;
+                texenv->setCombine_RGB(osg::TexEnvCombine::INTERPOLATE);
+                texenv->setSource0_RGB(osg::TexEnvCombine::TEXTURE0+0);
+                texenv->setOperand0_RGB(osg::TexEnvCombine::SRC_COLOR);
+                texenv->setSource1_RGB(osg::TexEnvCombine::TEXTURE0+1);
+                texenv->setOperand1_RGB(osg::TexEnvCombine::SRC_COLOR);
+                texenv->setSource2_RGB(osg::TexEnvCombine::CONSTANT);
+                texenv->setOperand2_RGB(osg::TexEnvCombine::SRC_COLOR);
+
+                texenv->setConstantColor(osg::Vec4(a,a,a,a));
+
+                stateset->setTextureAttribute(0, texenv);
+            }
+
+            {
+                osg::TexEnvCombine* texenv = new osg::TexEnvCombine;
+                texenv->setCombine_RGB(osg::TexEnvCombine::INTERPOLATE);
+                texenv->setSource0_RGB(osg::TexEnvCombine::PREVIOUS);
+                texenv->setOperand0_RGB(osg::TexEnvCombine::SRC_COLOR);
+                texenv->setSource1_RGB(osg::TexEnvCombine::TEXTURE0+2);
+                texenv->setOperand1_RGB(osg::TexEnvCombine::SRC_COLOR);
+                texenv->setSource2_RGB(osg::TexEnvCombine::CONSTANT);
+                texenv->setOperand2_RGB(osg::TexEnvCombine::SRC_COLOR);
+
+                texenv->setConstantColor(osg::Vec4(b,b,b,b));
+
+                stateset->setTextureAttribute(1, texenv);
+            }
+
+            {
+                osg::TexEnvCombine* texenv = new osg::TexEnvCombine;
+                texenv->setCombine_RGB(osg::TexEnvCombine::MODULATE);
+                texenv->setSource0_RGB(osg::TexEnvCombine::PREVIOUS);
+                texenv->setOperand0_RGB(osg::TexEnvCombine::SRC_COLOR);
+                texenv->setSource1_RGB(osg::TexEnvCombine::PRIMARY_COLOR);
+                texenv->setOperand1_RGB(osg::TexEnvCombine::SRC_COLOR);
+
+                stateset->setTextureAttribute(2, texenv);
+            }
+        }
     }
-    else if (_textureWeightList.size()==2)
+
+    if (_useTextureWeightsUniform && _textureWeightList.size()>0)
     {
+        osg::ref_ptr<osg::Uniform> uniform = new osg::Uniform(osg::Uniform::FLOAT, "TextureWeights", _textureWeightList.size());
+        for(unsigned int i=0; i<_textureWeightList.size(); ++i)
         {
-            osg::TexEnvCombine* texenv = new osg::TexEnvCombine;
-            texenv->setCombine_RGB(osg::TexEnvCombine::INTERPOLATE);
-            texenv->setSource0_RGB(osg::TexEnvCombine::TEXTURE0+0);
-            texenv->setOperand0_RGB(osg::TexEnvCombine::SRC_COLOR);
-            texenv->setSource1_RGB(osg::TexEnvCombine::TEXTURE0+1);
-            texenv->setOperand1_RGB(osg::TexEnvCombine::SRC_COLOR);
-            texenv->setSource2_RGB(osg::TexEnvCombine::CONSTANT);
-            texenv->setOperand2_RGB(osg::TexEnvCombine::SRC_COLOR);
-
-            float r = _textureWeightList[0]/(_textureWeightList[0]+_textureWeightList[1]);
-            texenv->setConstantColor(osg::Vec4(r,r,r,r));
-
-            stateset->setTextureAttribute(0, texenv);
+            uniform->setElement(i, _textureWeightList[i]);
         }
-
-        {
-            osg::TexEnvCombine* texenv = new osg::TexEnvCombine;
-            texenv->setCombine_RGB(osg::TexEnvCombine::MODULATE);
-            texenv->setSource0_RGB(osg::TexEnvCombine::PREVIOUS);
-            texenv->setOperand0_RGB(osg::TexEnvCombine::SRC_COLOR);
-            texenv->setSource1_RGB(osg::TexEnvCombine::PRIMARY_COLOR);
-            texenv->setOperand1_RGB(osg::TexEnvCombine::SRC_COLOR);
-
-            stateset->setTextureAttribute(1, texenv);
-        }
-    }
-    else if (_textureWeightList.size()==3)
-    {
-        float b = (_textureWeightList[0]+_textureWeightList[1])/(_textureWeightList[0]+_textureWeightList[1]+_textureWeightList[2]);
-        float a = _textureWeightList[0]/(_textureWeightList[0]+_textureWeightList[1]);
-
-        {
-            osg::TexEnvCombine* texenv = new osg::TexEnvCombine;
-            texenv->setCombine_RGB(osg::TexEnvCombine::INTERPOLATE);
-            texenv->setSource0_RGB(osg::TexEnvCombine::TEXTURE0+0);
-            texenv->setOperand0_RGB(osg::TexEnvCombine::SRC_COLOR);
-            texenv->setSource1_RGB(osg::TexEnvCombine::TEXTURE0+1);
-            texenv->setOperand1_RGB(osg::TexEnvCombine::SRC_COLOR);
-            texenv->setSource2_RGB(osg::TexEnvCombine::CONSTANT);
-            texenv->setOperand2_RGB(osg::TexEnvCombine::SRC_COLOR);
-
-            texenv->setConstantColor(osg::Vec4(a,a,a,a));
-
-            stateset->setTextureAttribute(0, texenv);
-        }
-
-        {
-            osg::TexEnvCombine* texenv = new osg::TexEnvCombine;
-            texenv->setCombine_RGB(osg::TexEnvCombine::INTERPOLATE);
-            texenv->setSource0_RGB(osg::TexEnvCombine::PREVIOUS);
-            texenv->setOperand0_RGB(osg::TexEnvCombine::SRC_COLOR);
-            texenv->setSource1_RGB(osg::TexEnvCombine::TEXTURE0+2);
-            texenv->setOperand1_RGB(osg::TexEnvCombine::SRC_COLOR);
-            texenv->setSource2_RGB(osg::TexEnvCombine::CONSTANT);
-            texenv->setOperand2_RGB(osg::TexEnvCombine::SRC_COLOR);
-
-            texenv->setConstantColor(osg::Vec4(b,b,b,b));
-
-            stateset->setTextureAttribute(1, texenv);
-        }
-
-        {
-            osg::TexEnvCombine* texenv = new osg::TexEnvCombine;
-            texenv->setCombine_RGB(osg::TexEnvCombine::MODULATE);
-            texenv->setSource0_RGB(osg::TexEnvCombine::PREVIOUS);
-            texenv->setOperand0_RGB(osg::TexEnvCombine::SRC_COLOR);
-            texenv->setSource1_RGB(osg::TexEnvCombine::PRIMARY_COLOR);
-            texenv->setOperand1_RGB(osg::TexEnvCombine::SRC_COLOR);
-
-            stateset->setTextureAttribute(2, texenv);
-        }
+        stateset->addUniform(uniform.get());
     }
 
     setStateSet(stateset.get());

9200d6d924d1e47f1509833b74a1615d7493c1a7
Robert OSFIELD <robert.osfield@gmail.com>
2015-04-13 09:50:27 +0000

    From Laurens Voerman, I have modified the FindOpenEXR.cmake to find the debug libraries, and to define a variable (OPENEXR_LIBRARIES_VARS) with the variable names that might have a _DEBUG version defined.
    git-svn-id: http://svn.openscenegraph.org/osg/OpenSceneGraph/trunk@14830 16af8721-9629-0410-8352-f15c8da7e697

--- CMakeModules/FindOpenEXR.cmake
+++ CMakeModules/FindOpenEXR.cmake
@@ -9,6 +9,7 @@
 #
 # Created by Robert Osfield. 
 
+
 FIND_PATH(OPENEXR_INCLUDE_DIR OpenEXR/ImfIO.h
     $ENV{OPENEXR_DIR}/include
     $ENV{OPENEXR_DIR}
@@ -23,78 +24,41 @@ FIND_PATH(OPENEXR_INCLUDE_DIR OpenEXR/ImfIO.h
     /usr/freeware/include
 )
 
-FIND_LIBRARY(OPENEXR_IlmIlf_LIBRARY 
-    NAMES IlmImf
-    PATHS
-    $ENV{OPENEXR_DIR}/lib
-    $ENV{OPENEXR_DIR}
-    ~/Library/Frameworks
-    /Library/Frameworks
-    /usr/local/lib
-    /usr/lib
-    /sw/lib
-    /opt/local/lib
-    /opt/csw/lib
-    /opt/lib
-    /usr/freeware/lib64
-)
+# Macro to find exr libraries (deduplicating search paths)
+# example: OPENEXR_FIND_VAR(OPENEXR_IlmIlf_LIBRARY IlmIlf)
+MACRO(OPENEXR_FIND_VAR varname libname)
+    FIND_LIBRARY( ${varname}
+        NAMES ${libname}
+        PATHS
+        $ENV{OPENEXR_DIR}/lib
+        $ENV{OPENEXR_DIR}
+        ~/Library/Frameworks
+        /Library/Frameworks
+        /usr/local/lib
+        /usr/lib
+        /sw/lib
+        /opt/local/lib
+        /opt/csw/lib
+        /opt/lib
+        /usr/freeware/lib64
+    )
+ENDMACRO(OPENEXR_FIND_VAR)
 
-FIND_LIBRARY(OPENEXR_IlmThread_LIBRARY 
-    NAMES IlmThread
-    PATHS
-    $ENV{OPENEXR_DIR}/lib
-    $ENV{OPENEXR_DIR}
-    ~/Library/Frameworks
-    /Library/Frameworks
-    /usr/local/lib
-    /usr/lib
-    /sw/lib
-    /opt/local/lib
-    /opt/csw/lib
-    /opt/lib
-    /usr/freeware/lib64
-)
+# Macro to find exr libraries (and debug versions)
+# example: OPENEXR_FIND(IlmIlf)
+MACRO(OPENEXR_FIND libname)
+    OPENEXR_FIND_VAR(OPENEXR_${libname}_LIBRARY ${libname})
+    OPENEXR_FIND_VAR(OPENEXR_${libname}_LIBRARY_DEBUG ${libname}d)
+ENDMACRO(OPENEXR_FIND)
 
-
-FIND_LIBRARY(OPENEXR_Iex_LIBRARY 
-    NAMES Iex
-    PATHS
-    $ENV{OPENEXR_DIR}/lib
-    $ENV{OPENEXR_DIR}
-    ~/Library/Frameworks
-    /Library/Frameworks
-    /usr/local/lib
-    /usr/lib
-    /sw/lib
-    /opt/local/lib
-    /opt/csw/lib
-    /opt/lib
-    /usr/freeware/lib64
-)
-
-FIND_LIBRARY(OPENEXR_Half_LIBRARY 
-    NAMES Half
-    PATHS
-    $ENV{OPENEXR_DIR}/lib
-    $ENV{OPENEXR_DIR}
-    ~/Library/Frameworks
-    /Library/Frameworks
-    /usr/local/lib
-    /usr/lib
-    /sw/lib
-    /opt/local/lib
-    /opt/csw/lib
-    /opt/lib
-    /usr/freeware/lib64
-)
+OPENEXR_FIND(IlmIlf)
+OPENEXR_FIND(IlmThread)
+OPENEXR_FIND(Iex)
+OPENEXR_FIND(Half)
 
 SET(OPENEXR_FOUND "NO")
 IF(OPENEXR_INCLUDE_DIR AND OPENEXR_IlmIlf_LIBRARY AND OPENEXR_IlmThread_LIBRARY AND OPENEXR_Iex_LIBRARY AND OPENEXR_Half_LIBRARY)
-    SET(OPENEXR_LIBRARIES
-        ${OPENEXR_IlmIlf_LIBRARY}
-        ${OPENEXR_IlmThread_LIBRARY}
-        ${OPENEXR_Half_LIBRARY}
-        ${OPENEXR_Iex_LIBRARY}
-    )
+    SET(OPENEXR_LIBRARIES ${OPENEXR_IlmIlf_LIBRARY} ${OPENEXR_IlmThread_LIBRARY} ${OPENEXR_Half_LIBRARY} ${OPENEXR_Iex_LIBRARY} )
+    SET(OPENEXR_LIBRARIES_VARS OPENEXR_IlmIlf_LIBRARY OPENEXR_IlmThread_LIBRARY OPENEXR_Half_LIBRARY OPENEXR_Iex_LIBRARY )
     SET(OPENEXR_FOUND "YES")
 ENDIF(OPENEXR_INCLUDE_DIR AND OPENEXR_IlmIlf_LIBRARY AND OPENEXR_IlmThread_LIBRARY AND OPENEXR_Iex_LIBRARY AND OPENEXR_Half_LIBRARY)

bc3a77cb1561ed2e47e88ab507180bcf15309f31
Robert OSFIELD <robert.osfield@gmail.com>
2015-07-17 18:31:22 +0000

    From KOS, "'ve mentioned in the "osg-users" ML about build error with the newest GStreamer 1.5
    Please, have a look at the attached patch.
    
    CMakeModules/FindGStreamer.cmake"
    
    
    git-svn-id: http://svn.openscenegraph.org/osg/OpenSceneGraph/trunk@14984 16af8721-9629-0410-8352-f15c8da7e697

--- CMakeModules/FindGStreamer.cmake
+++ CMakeModules/FindGStreamer.cmake
@@ -83,18 +83,18 @@ else ()
     find_package(PkgConfig)    
 
     macro(FIND_GSTREAMER_COMPONENT _component_prefix _pkgconfig_name _header _library)
-        pkg_check_modules(PC_${_component_prefix} QUIET ${_pkgconfig_name})
+        pkg_check_modules(${_component_prefix} QUIET ${_pkgconfig_name})
 
-        find_path(${_component_prefix}_INCLUDE_DIRS
-            NAMES ${_header}
-            HINTS ${PC_${_component_prefix}_INCLUDE_DIRS} ${PC_${_component_prefix}_INCLUDEDIR}
-            PATH_SUFFIXES gstreamer-1.0
-        )
+#        find_path(${_component_prefix}_INCLUDE_DIRS
+#            NAMES ${_header}
+#            HINTS ${PC_${_component_prefix}_INCLUDE_DIRS} ${PC_${_component_prefix}_INCLUDEDIR}
+#            PATH_SUFFIXES gstreamer-1.0
+#        )
 
-        find_library(${_component_prefix}_LIBRARIES
-            NAMES ${_library}
-            HINTS ${PC_${_component_prefix}_LIBRARY_DIRS} ${PC_${_component_prefix}_LIBDIR}
-        )
+#        find_library(${_component_prefix}_LIBRARIES
+#            NAMES ${_library}
+#            HINTS ${PC_${_component_prefix}_LIBRARY_DIRS} ${PC_${_component_prefix}_LIBDIR}
+#        )
     endmacro()
 endif ()
 

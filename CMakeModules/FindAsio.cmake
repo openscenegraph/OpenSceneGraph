8f73ddc27d727c7ce0950568508fe3b8761cdeae
Robert Osfield <robert@openscenegraph.com>
2015-08-11 08:44:46 +0000

    Moved the FIND_PACKAGE(Boost) from the root CMakeLists.txt into the local Find scripts to avoid Boost check when not neccessary.
    git-svn-id: http://svn.openscenegraph.org/osg/OpenSceneGraph/branches/OpenSceneGraph-3.4@15094 16af8721-9629-0410-8352-f15c8da7e697

--- CMakeModules/FindAsio.cmake
+++ CMakeModules/FindAsio.cmake
@@ -1,9 +1,9 @@
 # Locate ASIO-headers (http://think-async.com/Asio)
 # This module defines
-# ASIO_FOUND, if false, do not try to link to gdal 
+# ASIO_FOUND, if false, do not try to link to gdal
 # ASIO_INCLUDE_DIR, where to find the headers
 #
-# Created by Stephan Maximilian Huber 
+# Created by Stephan Maximilian Huber
 
 FIND_PATH(ASIO_INCLUDE_DIR
   NAMES
@@ -14,7 +14,9 @@ FIND_PATH(ASIO_INCLUDE_DIR
 )
 
 SET(ASIO_FOUND "NO")
-FIND_PACKAGE( Boost 1.37 )
-IF(Boost_FOUND AND ASIO_INCLUDE_DIR) 
-  SET(ASIO_FOUND "YES")
+IF(ASIO_INCLUDE_DIR)
+    FIND_PACKAGE( Boost 1.37 )
+    IF(Boost_FOUND)
+        SET(ASIO_FOUND "YES")
+    ENDIF()
 ENDIF()

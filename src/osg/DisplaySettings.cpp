636d59591227ce45157454d0b7aa5dffd3786b47
Robert Osfield <robert@openscenegraph.com>
2015-10-26 11:43:28 +0000

    From Alberto Luaces, "here are two minimal capitalization fixes for mingw cross-compilation, where case sensitivity matters."
    git-svn-id: http://svn.openscenegraph.org/osg/OpenSceneGraph/branches/OpenSceneGraph-3.4@15172 16af8721-9629-0410-8352-f15c8da7e697

--- src/osg/DisplaySettings.cpp
+++ src/osg/DisplaySettings.cpp
@@ -24,7 +24,7 @@ using namespace osg;
 using namespace std;
 
 #if defined(WIN32) && !defined(__CYGWIN__)
-#include<Windows.h>
+#include<windows.h>
 extern "C" { OSG_EXPORT DWORD NvOptimusEnablement=0x00000001; }
 #else
 extern "C" { int NvOptimusEnablement=0x00000001; }

6de3bd77500ce5f1c85ce87575f66e9974a5ab90
tsujie <tsujie@gmail.com>
2016-03-01 10:48:47 -0800

    Fix OpenGL errors for OpenGL ES devices

---
+++ ImageStream.cpp
@@ -0,0 +1,45 @@
+/* -*-c++-*- OpenSceneGraph - Copyright (C) 1998-2006 Robert Osfield
+ *
+ * This library is open source and may be redistributed and/or modified under
+ * the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or
+ * (at your option) any later version.  The full license is in LICENSE file
+ * included with this distribution, and on the openscenegraph.org website.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * OpenSceneGraph Public License for more details.
+*/
+
+#include <osg/ImageStream>
+
+using namespace osg;
+
+ImageStream::ImageStream():
+    _status(INVALID),
+    _loopingMode(LOOPING)
+{
+    setDataVariance(DYNAMIC);
+
+#if !defined(OSG_GLES1_AVAILABLE) && !defined(OSG_GLES2_AVAILABLE)
+//#ifndef __APPLE__
+    // disabled under OSX for time being while we resolve why PBO
+    // doesn't function properly under OSX.
+    setPixelBufferObject(new PixelBufferObject(this));
+//#endif
+#endif
+}
+
+ImageStream::ImageStream(const ImageStream& image,const CopyOp& copyop):
+    Image(image,copyop),
+    _status(image._status),
+    _loopingMode(image._loopingMode),
+    _audioStreams(image._audioStreams)
+{
+}
+
+int ImageStream::compare(const Image& rhs) const
+{
+    return Image::compare(rhs);
+}
+

5fdf9829bb9be67ad180e438b47e940acc1fadfc
Robert Osfield <robert@openscenegraph.com>
2016-06-23 07:52:14 +0100

    Removed dead code

--- src/osgText/GlyphGeometry.cpp
+++ src/osgText/GlyphGeometry.cpp
@@ -1146,8 +1146,6 @@ OSGTEXT_EXPORT osg::Geometry* computeTextGeometry(osg::Geometry* glyphGeometry, 
         backFace->push_back(back_indices[p2]);
     }
 
-    bool shareVerticesWithFaces = true;
-
     // now build up the bevel
     for(osg::Geometry::PrimitiveSetList::iterator itr = bevelPrimitiveSets.begin();
         itr != bevelPrimitiveSets.end();
@@ -1174,42 +1172,29 @@ OSGTEXT_EXPORT osg::Geometry* computeTextGeometry(osg::Geometry* glyphGeometry, 
             osg::Vec3& base_vertex = (*orig_vertices)[ basei ];
             osg::Vec3 up = top_vertex-base_vertex;
 
-            if (shareVerticesWithFaces)
+            if (front_indices[basei]==NULL_VALUE)
             {
-                if (front_indices[basei]==NULL_VALUE)
-                {
-                    front_indices[basei] = vertices->size();
-                    vertices->push_back(base_vertex);
-                }
-
-                bevelIndices[i*no_vertices_on_bevel + 0] = front_indices[basei];
-
-                for(unsigned int j=1; j<no_vertices_on_bevel-1; ++j)
-                {
-                    const osg::Vec2& pv = profileVertices[j];
-                    osg::Vec3 pos( base_vertex + (forward * pv.x()) + (up * pv.y()) );
-                    bevelIndices[i*no_vertices_on_bevel + j] = vertices->size();
-                    vertices->push_back(pos);
-                }
-
-                if (back_indices[basei]==NULL_VALUE)
-                {
-                    back_indices[basei] = vertices->size();
-                    vertices->push_back(base_vertex + forward);
-                }
-
-                bevelIndices[i*no_vertices_on_bevel + no_vertices_on_bevel-1] = back_indices[basei];
+                front_indices[basei] = vertices->size();
+                vertices->push_back(base_vertex);
             }
-            else
+
+            bevelIndices[i*no_vertices_on_bevel + 0] = front_indices[basei];
+
+            for(unsigned int j=1; j<no_vertices_on_bevel-1; ++j)
             {
-                for(unsigned int j=0; j<no_vertices_on_bevel; ++j)
-                {
-                    const osg::Vec2& pv = profileVertices[j];
-                    osg::Vec3 pos( base_vertex + (forward * pv.x()) + (up * pv.y()) );
-                    bevelIndices[i*no_vertices_on_bevel + j] = vertices->size();
-                    vertices->push_back(pos);
-                }
+                const osg::Vec2& pv = profileVertices[j];
+                osg::Vec3 pos( base_vertex + (forward * pv.x()) + (up * pv.y()) );
+                bevelIndices[i*no_vertices_on_bevel + j] = vertices->size();
+                vertices->push_back(pos);
             }
+
+            if (back_indices[basei]==NULL_VALUE)
+            {
+                back_indices[basei] = vertices->size();
+                vertices->push_back(base_vertex + forward);
+            }
+
+            bevelIndices[i*no_vertices_on_bevel + no_vertices_on_bevel-1] = back_indices[basei];
         }
 
         osg::DrawElementsUShort* elements = new osg::DrawElementsUShort(GL_TRIANGLES);

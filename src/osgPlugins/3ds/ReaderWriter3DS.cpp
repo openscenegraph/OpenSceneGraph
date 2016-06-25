3609d84cef43bca784b5f3c5e448e7f475e6bbbb
Robert Osfield <robert@openscenegraph.com>
2016-06-25 15:28:50 +0100

    FIxed copy and paste error

--- src/osgPlugins/3ds/ReaderWriter3DS.cpp
+++ src/osgPlugins/3ds/ReaderWriter3DS.cpp
@@ -619,7 +619,7 @@ osg::Node* ReaderWriter3DS::ReaderObject::processNode(StateSetMap& drawStateMap,
         {
             // add our geometry to group (where our children already are)
             // creates geometry under modifier node
-            processMesh(drawStateMap,meshTransform,mesh,meshAppliedMatPtr);
+            processMesh(drawStateMap,group,mesh,meshAppliedMatPtr);
             return group;
         }
         else

483982de3a7439f088e1e2ac044d629c82f65d80
Robert Osfield <robert@openscenegraph.com>
2016-07-07 07:31:15 +0100

    Fixed coverity issue

--- src/osgGA/SphericalManipulator.cpp
+++ src/osgGA/SphericalManipulator.cpp
@@ -284,7 +284,7 @@ bool SphericalManipulator::calcMovement()
 
     float dx=0.0f;
     float dy=0.0f;
-    unsigned int buttonMask=osgGA::GUIEventAdapter::NONE;
+    unsigned int buttonMask=0;
 
     if (_ga_t0->getEventType()==GUIEventAdapter::SCROLL)
     {

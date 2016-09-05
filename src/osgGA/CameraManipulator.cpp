ec344a85b4b368ae8f748d53df9b5b7e33dd6f4c
Robert Osfield <robert@openscenegraph.com>
2016-09-05 13:44:59 +0100

    Fixed warnings

--- src/osgGA/CameraManipulator.cpp
+++ src/osgGA/CameraManipulator.cpp
@@ -21,6 +21,8 @@ CameraManipulator::CameraManipulator()
 
 
 CameraManipulator::CameraManipulator(const CameraManipulator& mm, const CopyOp& copyOp):
+     osg::Object(mm,copyOp),
+     osg::Callback(mm,copyOp),
      inherited(mm, copyOp),
      _intersectTraversalMask(mm._intersectTraversalMask),
      _autoComputeHomePosition(mm._autoComputeHomePosition),

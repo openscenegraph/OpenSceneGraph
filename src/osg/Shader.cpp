7e0551e7e83159ac3814a27fc25d42d59240aa7c
Robert Osfield <robert@openscenegraph.com>
2016-10-25 20:17:55 +0100

    Added #pragma import_texture_modes() -> #define into #pragma data structure a GLSL parsing

--- src/osg/Shader.cpp
+++ src/osg/Shader.cpp
@@ -738,10 +738,12 @@ void Shader::PerContextShader::detachShader(GLuint program) const
     _extensions->glDetachShader( program, _glShaderHandle );
 }
 
-void Shader::_parseShaderDefines(const std::string& str, ShaderDefines& defines)
+bool Shader::_parseShaderDefines(const std::string& str, ShaderDefines& defines, unsigned int& index)
 {
     OSG_INFO<<"Shader::_parseShaderDefines("<<str<<")"<<std::endl;
     std::string::size_type start_of_parameter = 0;
+    bool firstString = true;
+    bool indexSet = false;
     do
     {
         // skip spaces, tabs, commans
@@ -764,13 +766,34 @@ void Shader::_parseShaderDefines(const std::string& str, ShaderDefines& defines)
         if (start_of_parameter<end_of_parameter)
         {
             std::string parameter = str.substr(start_of_parameter, end_of_parameter-start_of_parameter);
-            defines.insert(parameter);
-            OSG_INFO<<"   defines.insert("<<parameter<<")"<<std::endl;
+
+            if (firstString)
+            {
+                firstString = false;
+                if (parameter[0]>='0' && parameter[0]<='9')
+                {
+                    index = atoi(parameter.c_str());
+                    indexSet = false;
+                    OSG_INFO<<"   setting index("<<index<<")"<<std::endl;
+                }
+                else
+                {
+                    OSG_INFO<<"   defines.insert("<<parameter<<")"<<std::endl;
+                    defines.insert(parameter);
+                }
+            }
+            else
+            {
+                OSG_INFO<<"   defines.insert("<<parameter<<")"<<std::endl;
+                defines.insert(parameter);
+            }
+
         }
 
         start_of_parameter = end_of_parameter+1;
 
     } while (start_of_parameter<str.size());
+    return indexSet;
 }
 
 
@@ -804,34 +827,21 @@ void Shader::_computeShaderDefines()
                 std::string str(_shaderSource, open_brackets+1, eol-open_brackets-1);
 
                 // OSG_NOTICE<<"    parameter str = ["<<str<<"]"<<std::endl;
-                if (keyword == "import_defines") _parseShaderDefines(str, _shaderPragmas.defines);
-                else if (keyword == "import_modes") _parseShaderDefines(str, _shaderPragmas.modes);
-                else if (keyword == "requires") _parseShaderDefines(str, _shaderPragmas.requirements);
+                unsigned int index;
+                if (keyword == "import_defines") _parseShaderDefines(str, _shaderPragmas.defines, index);
+                else if (keyword == "import_modes") _parseShaderDefines(str, _shaderPragmas.modes, index);
+                else if (keyword == "import_texture_modes")
+                {
+                    ShaderDefines sd;
+                    index = 0;
+                    _parseShaderDefines(str, sd, index);
+                    if (index>=_shaderPragmas.textureModes.size()) _shaderPragmas.textureModes.resize(index+1);
+                    _shaderPragmas.textureModes[index] = sd;
+                }
+                else if (keyword == "requires") _parseShaderDefines(str, _shaderPragmas.requirements, index);
                 else {
                     OSG_NOTICE<<"  keyword not matched ["<<keyword<<"]"<<std::endl;
                 }
-#if 1
-                for(ShaderDefines::iterator itr = _shaderPragmas.defines.begin();
-                    itr != _shaderPragmas.defines.end();
-                    ++itr)
-                {
-                    OSG_NOTICE<<"      define ["<<*itr<<"]"<<std::endl;
-                }
-
-                for(ShaderDefines::iterator itr = _shaderPragmas.requirements.begin();
-                    itr != _shaderPragmas.requirements.end();
-                    ++itr)
-                {
-                    OSG_NOTICE<<"      requirements ["<<*itr<<"]"<<std::endl;
-                }
-
-                for(ShaderDefines::iterator itr = _shaderPragmas.modes.begin();
-                    itr != _shaderPragmas.modes.end();
-                    ++itr)
-                {
-                    OSG_NOTICE<<"      modes ["<<*itr<<"]"<<std::endl;
-                }
-#endif
 
             }
 #if 1
@@ -845,5 +855,39 @@ void Shader::_computeShaderDefines()
         pos = eol;
     }
 
+#if 1
+    for(ShaderDefines::iterator itr = _shaderPragmas.defines.begin();
+        itr != _shaderPragmas.defines.end();
+        ++itr)
+    {
+        OSG_NOTICE<<"      define ["<<*itr<<"]"<<std::endl;
+    }
+
+    for(ShaderDefines::iterator itr = _shaderPragmas.requirements.begin();
+        itr != _shaderPragmas.requirements.end();
+        ++itr)
+    {
+        OSG_NOTICE<<"      requirements ["<<*itr<<"]"<<std::endl;
+    }
+
+    for(ShaderDefines::iterator itr = _shaderPragmas.modes.begin();
+        itr != _shaderPragmas.modes.end();
+        ++itr)
+    {
+        OSG_NOTICE<<"      modes ["<<*itr<<"]"<<std::endl;
+    }
+
+    for(unsigned int i=0; i<_shaderPragmas.textureModes.size(); ++i)
+    {
+        OSG_NOTICE<<"      texture_modes, unit=["<<i<<"]"<<std::endl;
+        const ShaderDefines& tsd = _shaderPragmas.textureModes[i];
+        for(ShaderDefines::const_iterator itr = tsd.begin();
+            itr != tsd.end();
+            ++itr)
+        {
+            OSG_NOTICE<<"          texture_modes ["<<*itr<<"]"<<std::endl;
+        }
+    }
+#endif
 }
 

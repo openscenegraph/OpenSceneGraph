b493657c47b3270ecee8cb3b923f8ad689f0a28f
Thomas Hogarth <thomas.hogarth@gmail.com>
2017-03-18 17:35:00 +0000

    Shaders now using vertex colors

--- examples/osgviewerIPhone/shaders.h
+++ examples/osgviewerIPhone/shaders.h
@@ -10,19 +10,22 @@
 const char* ColorShaderVert =
 "#version 300 es\n"
 "in vec4 osg_Vertex;\n"
+"in vec4 osg_Color;\n"
+"out vec4 vertColor;\n"
 "uniform mat4 osg_ModelViewProjectionMatrix;\n"
 "void main()\n"
 "{\n"
 "    gl_Position = osg_ModelViewProjectionMatrix * osg_Vertex;\n"
+"    vertColor = osg_Color;\n"
 "}\n";
 
 const char* ColorShaderFrag =
 "#version 300 es\n"
-"uniform lowp vec4 color;\n"
+"in lowp vec4 vertColor;\n"
 "out lowp vec4 fragColor;\n"
 "void main()\n"
 "{\n"
-"    fragColor = color;\n"
+"    fragColor = vertColor;\n"
 "}\n";
 
 #elif OSG_GLES2_FEATURES
@@ -30,18 +33,21 @@ const char* ColorShaderFrag =
 const char* ColorShaderVert =
 "#version 100\n"
 "attribute vec4 osg_Vertex;\n"
+"attribute vec4 osg_Color;\n"
 "uniform mat4 osg_ModelViewProjectionMatrix;\n"
+"varying vec4 vertColor;\n"
 "void main()\n"
 "{\n"
 "    gl_Position = osg_ModelViewProjectionMatrix * osg_Vertex;\n"
+"    vertColor = osg_Color;\n"
 "}\n";
 
 const char* ColorShaderFrag =
 "#version 100\n"
-"uniform lowp vec4 color;\n"
+"varying lowp vec4 vertColor;\n"
 "void main()\n"
 "{\n"
-"    gl_FragColor = color;\n"
+"    gl_FragColor = vertColor;\n"
 "}\n";
 
 #else
@@ -60,25 +66,27 @@ const char* ColorShaderFrag = NULL;
 const char* TextShaderVert =
 "#version 300 es\n"
 "in vec4 osg_Vertex;\n"
+"in vec4 osg_Color;\n"
 "in vec4 osg_MultiTexCoord0;\n"
 "uniform mat4 osg_ModelViewProjectionMatrix;\n"
+"out vec4 vertColor;\n"
 "out vec4 texCoord;\n"
 "void main()\n"
 "{\n"
 "    gl_Position = osg_ModelViewProjectionMatrix * osg_Vertex;\n"
+"    vertColor = osg_Color;\n"
 "    texCoord = osg_MultiTexCoord0;\n"
 "}\n";
 
 const char* TextShaderFrag =
 "#version 300 es\n"
+"in lowp vec4 vertColor;\n"
 "in lowp vec4 texCoord;\n"
 "uniform sampler2D glyphTexture;\n"
-"uniform lowp vec4 color;\n"
 "out lowp vec4 fragColor;\n"
 "void main()\n"
 "{\n"
-"    //lowp vec4 gc = texture(glyphTexture, texCoord.xy);\n"
-"    fragColor = color * texture(glyphTexture, texCoord.xy).a;\n"
+"    fragColor = vertColor * texture(glyphTexture, texCoord.xy).a;\n"
 "}\n";
 
 #elif OSG_GLES2_FEATURES
@@ -86,23 +94,26 @@ const char* TextShaderFrag =
 const char* TextShaderVert =
 "#version 100\n"
 "attribute vec4 osg_Vertex;\n"
+"attribute vec4 osg_Color;\n"
 "attribute vec4 osg_MultiTexCoord0;\n"
 "uniform mat4 osg_ModelViewProjectionMatrix;\n"
+"varying vec4 vertColor;\n"
 "varying vec4 texCoord;\n"
 "void main()\n"
 "{\n"
 "    gl_Position = osg_ModelViewProjectionMatrix * osg_Vertex;\n"
+"    vertColor = osg_Color;\n"
 "    texCoord = osg_MultiTexCoord0;\n"
 "}\n";
 
 const char* TextShaderFrag =
 "#version 100\n"
+"varying lowp vec4 vertColor;\n"
 "varying lowp vec4 texCoord;\n"
 "uniform sampler2D glyphTexture;\n"
-"uniform lowp vec4 color;\n"
 "void main()\n"
 "{\n"
-"    gl_FragColor = color * texture2D(glyphTexture, texCoord.xy).a;\n"
+"    gl_FragColor = vertColor * texture2D(glyphTexture, texCoord.xy).a;\n"
 "}\n";
 
 #else

59d3b1ebacab7e4f64b2a73563a5884591fe457e
Michael Kapelko <kornerr@gmail.com>
2017-08-22 22:49:56 +0300

    Fix stat64 build issue with NDK 15 by definining at stat for Android

--- src/osgDB/FileUtils.cpp
+++ src/osgDB/FileUtils.cpp
@@ -95,6 +95,10 @@ typedef char TCHAR;
     #endif
 #endif
 
+#if defined(__ANDROID__)
+    #define stat64 stat
+#endif
+
     // set up _S_ISDIR()
 #if !defined(S_ISDIR)
 #  if defined( _S_IFDIR) && !defined( __S_IFDIR)

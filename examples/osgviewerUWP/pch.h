9dc45bcd71b8782e1fb603aecdf8755b9ec6658f
Giuseppe Donvito <donvito@illogic.us>
2017-09-06 12:02:31 +0200

    Added osgviewerUWP example.

---
+++ examples/osgviewerUWP/pch.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include <memory>
+#include <wrl.h>
+
+#define GL_GLEXT_PROTOTYPES 1 // why do we need this when vs template example didn't?
+// OpenGL ES includes
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
+
+// EGL includes
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <EGL/eglplatform.h>
+
+// ANGLE include for Windows Store
+#include <angle_windowsstore.h>

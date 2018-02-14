a69417f5a362455c3a47c2293cbb00ec789ca77c
Laurens Voerman <l.voerman@rug.nl>
2018-02-14 10:56:53 +0100

    avoid visual studio warning about gethostbyname being deprecated by #define _WINSOCK_DEPRECATED_NO_WARNINGS

--- src/osgPlugins/osc/ip/win32/NetworkingUtils.cpp
+++ src/osgPlugins/osc/ip/win32/NetworkingUtils.cpp
@@ -28,7 +28,7 @@
 	WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 #include "ip/NetworkingUtils.h"
-
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <winsock2.h>   // this must come first to prevent errors with MSVC7
 #include <windows.h>
 #include <stdlib.h>

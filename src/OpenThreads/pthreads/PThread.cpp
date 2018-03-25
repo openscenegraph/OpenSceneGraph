20cbb25cd104c029b99d10a8c01cc18aa0d0b9d6
Robert Osfield <robert@openscenegraph.com>
2018-03-25 12:13:50 +0100

    Added check if thraeding is running before executing start thread.

--- src/OpenThreads/pthreads/PThread.cpp
+++ src/OpenThreads/pthreads/PThread.cpp
@@ -638,6 +638,12 @@ bool Thread::isRunning()
 //
 int Thread::start() {
 
+    PThreadPrivateData *pd = static_cast<PThreadPrivateData *> (_prvData);
+    if (pd->isRunning())
+    {
+        return 0;
+    }
+
     int status;
     pthread_attr_t thread_attr;
 
@@ -646,8 +652,6 @@ int Thread::start() {
     {
         return status;
     }
-
-    PThreadPrivateData *pd = static_cast<PThreadPrivateData *> (_prvData);
 
     //-------------------------------------------------------------------------
     // Set the stack size if requested, but not less than a platform reasonable

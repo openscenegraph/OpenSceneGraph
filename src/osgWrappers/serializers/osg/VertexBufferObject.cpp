535217f5e0efca26036077ea7915d18d64c10d97
Julien Valentin <julienvalentin51@gmail.Com>
2018-04-24 01:56:29 +0200

    add wrapper for osg::DrawIndirectBufferObject
    add wrapper for osg::DrawIndirectBufferObject

--- src/osgWrappers/serializers/osg/VertexBufferObject.cpp
+++ src/osgWrappers/serializers/osg/VertexBufferObject.cpp
@@ -3,9 +3,20 @@
 #include <osgDB/InputStream>
 #include <osgDB/OutputStream>
 
+namespace wrapper_osgVertexBufferObject{
 REGISTER_OBJECT_WRAPPER( VertexBufferObject,
                          new osg::VertexBufferObject,
                          osg::VertexBufferObject,
                          "osg::Object osg::BufferObject osg::VertexBufferObject" )
 {
+}
+}
+
+namespace wrapper_osgDrawIndirectBufferObject{
+REGISTER_OBJECT_WRAPPER( DrawIndirectBufferObject,
+                         new osg::DrawIndirectBufferObject,
+                         osg::DrawIndirectBufferObject,
+                         "osg::Object osg::BufferObject osg::DrawIndirectBufferObject" )
+{
+}
 }

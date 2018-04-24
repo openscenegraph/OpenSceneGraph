ef3d672f8cc5896b1e46e05bc4d1fd7492766fdf
Robert Osfield <robert@openscenegraph.com>
2018-04-24 16:22:13 +0100

    Added OSG_GL_CONTEXT_STRING cmake variable to set include/osg/GL headers that sets Traits::glContextVersion.

--- src/osg/DisplaySettings.cpp
+++ src/osg/DisplaySettings.cpp
@@ -240,7 +240,7 @@ void DisplaySettings::setDefaults()
 
     _implicitBufferAttachmentRenderMask = DEFAULT_IMPLICIT_BUFFER_ATTACHMENT;
     _implicitBufferAttachmentResolveMask = DEFAULT_IMPLICIT_BUFFER_ATTACHMENT;
-    _glContextVersion = "1.0";
+    _glContextVersion = OSG_GL_CONTEXT_STRING;
     _glContextFlags = 0;
     _glContextProfileMask = 0;
 

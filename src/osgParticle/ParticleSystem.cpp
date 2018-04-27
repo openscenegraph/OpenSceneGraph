31c9dbc881d420674571206ed0a55e4ae5a9181e
Robert Osfield <robert@openscenegraph.com>
2018-04-27 11:23:07 +0100

    Fixed particle update bug where a ParticleSystem wouldn't start when loaded during the frame loop due to the _last_frame value not being set.

--- src/osgParticle/ParticleSystem.cpp
+++ src/osgParticle/ParticleSystem.cpp
@@ -211,8 +211,6 @@ void osgParticle::ParticleSystem::update(double dt, osg::NodeVisitor& nv)
 
 void osgParticle::ParticleSystem::drawImplementation(osg::RenderInfo& renderInfo) const
 {
-    if (_particles.size() <= 0) return;
-
     ScopedReadLock lock(_readWriteMutex);
 
     osg::State& state = *renderInfo.getState();
@@ -220,6 +218,8 @@ void osgParticle::ParticleSystem::drawImplementation(osg::RenderInfo& renderInfo
     // update the frame count, so other objects can detect when
     // this particle system is culled
     _last_frame = state.getFrameStamp()->getFrameNumber();
+
+    if (_particles.size() <= 0) return;
 
     // update the dirty flag of delta time, so next time a new request for delta time
     // will automatically cause recomputing

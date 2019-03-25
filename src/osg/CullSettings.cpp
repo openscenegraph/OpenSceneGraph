e0d90fb4c22396eeaa62e5452886d49d070b26a3
Robert Osfield <robert@openscenegraph.com>
2019-03-25 12:40:41 +0000

    Added --NO_CULLING, --VIEW_FRSUTUM and --VIEW_FRUSTUM_SIDES/--vfs command line options for setting the Camera CullingMode value

--- src/osg/CullSettings.cpp
+++ src/osg/CullSettings.cpp
@@ -117,12 +117,19 @@ void CullSettings::readEnvironmentalVariables()
 
 void CullSettings::readCommandLine(ArgumentParser& arguments)
 {
+    OSG_INFO<<"CullSettings::readCommandLine(ArgumentParser& arguments)"<<std::endl;
+
     // report the usage options.
     if (arguments.getApplicationUsage())
     {
         arguments.getApplicationUsage()->addCommandLineOption("--COMPUTE_NEAR_FAR_MODE <mode>","DO_NOT_COMPUTE_NEAR_FAR | COMPUTE_NEAR_FAR_USING_BOUNDING_VOLUMES | COMPUTE_NEAR_FAR_USING_PRIMITIVES");
         arguments.getApplicationUsage()->addCommandLineOption("--NEAR_FAR_RATIO <float>","Set the ratio between near and far planes - must greater than 0.0 but less than 1.0.");
     }
+
+    while(arguments.read("--NO_CULLING")) setCullingMode(NO_CULLING);
+    while(arguments.read("--VIEW_FRUSTUM")) setCullingMode(VIEW_FRUSTUM_CULLING);
+    while(arguments.read("--VIEW_FRUSTUM_SIDES") || arguments.read("--vfs") ) setCullingMode(VIEW_FRUSTUM_SIDES_CULLING);
+
 
     std::string str;
     while(arguments.read("--COMPUTE_NEAR_FAR_MODE",str))

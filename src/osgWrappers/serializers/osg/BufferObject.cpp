22473d0f7f0efe6eed268664817205cc4e0774af
Julien Valentin <mp3butcher@hotmail.com>
2020-01-06 20:10:43 +0100

    add MappingBitField serializer and increment SO version

--- src/osgWrappers/serializers/osg/BufferObject.cpp
+++ src/osgWrappers/serializers/osg/BufferObject.cpp
@@ -8,7 +8,11 @@ REGISTER_OBJECT_WRAPPER( BufferObject,
                          osg::BufferObject,
                          "osg::Object osg::BufferObject" )
 {
-    ADD_GLENUM_SERIALIZER( Target,GLenum, GL_ARRAY_BUFFER_ARB);  // _type
-    ADD_GLENUM_SERIALIZER( Usage,GLenum, GL_STATIC_DRAW_ARB);  // _type   setTarget(GL_ARRAY_BUFFER_ARB);
+    ADD_GLENUM_SERIALIZER( Target, GLenum, GL_ARRAY_BUFFER_ARB);  // _type
+    ADD_GLENUM_SERIALIZER( Usage, GLenum, GL_STATIC_DRAW_ARB);  // _usage
     ADD_BOOL_SERIALIZER( CopyDataAndReleaseGLBufferObject, false);
+    {
+        UPDATE_TO_VERSION_SCOPED( 201 )
+        ADD_HEXINT_SERIALIZER( MappingBitfield, 0x0 );  // _mappingBitField
+    }
 }
